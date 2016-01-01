286a2ef2a2376a74b6aa58c11b4891f17f060e5f
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-01-02 01:26:57 +0900

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "searches#new"
   resources :searches
   # The priority is based upon order of creation: first created -> highest priority.
