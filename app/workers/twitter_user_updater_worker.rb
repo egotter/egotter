14877f363f0b872f9d0aecb9f1858a00c79a9edd
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-01-15 19:52:20 +0900

    add debug message

--- app/workers/twitter_user_updater_worker.rb
+++ app/workers/twitter_user_updater_worker.rb
@@ -4,24 +4,24 @@ class TwitterUserUpdaterWorker
 
   def perform(uid)
     u = client.user(uid.to_i) && client.user(uid.to_i)
-    puts "[#{Time.zone.now}] #{user_name(u)} start"
+    puts "[#{Time.zone.now}] TwitterUserUpdaterWorker #{user_name(u)} start"
 
     tu = TwitterUser.latest(u.id)
     if tu.blank?
-      puts "[#{Time.zone.now}] #{user_name(u)} something is wrong(TwitterUser doesn't exist)"
+      puts "[#{Time.zone.now}] TwitterUserUpdaterWorker #{user_name(u)} TwitterUser doesn't exist"
       return
     end
 
     if tu.recently_created? || tu.recently_updated?
-      puts "[#{Time.zone.now}] #{user_name(u)} skip"
+      puts "[#{Time.zone.now}] TwitterUserUpdaterWorker #{user_name(u)} skip(recently created or recently updated)"
       return
     end
 
     new_tu = TwitterUser.build_with_raw_twitter_data(client, u.id)
     if new_tu.save_raw_twitter_data
-      puts "[#{Time.zone.now}] #{user_name(u)} create(new TwitterUser)"
+      puts "[#{Time.zone.now}] TwitterUserUpdaterWorker #{user_name(u)} create new TwitterUser"
     else
-      puts "[#{Time.zone.now}] #{user_name(u)} do nothing"
+      puts "[#{Time.zone.now}] TwitterUserUpdaterWorker #{user_name(u)} do nothing(#{new_tu.errors.full_messages})"
     end
   end
 
