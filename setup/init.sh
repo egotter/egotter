0d97a2c1fe4e0cf357d82cb36980622205b24e5e
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-01-22 22:24:29 +0900

    create rsyslog conf

--- setup/init.sh
+++ setup/init.sh
@@ -33,4 +33,6 @@ bundle exec rails s --binding=0.0.0.0
 # 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * * /bin/bash -l -c 'cd /home/ec2-user/egotter && RAILS_ENV=development TWITTER_CONSUMER_SECRET=xxx TWITTER_CONSUMER_KEY=xxx bundle exec rake update_job_dispatcher:run --silent >> /home/ec2-user/egotter/log/crontab.log 2>&1'
 
 # https://app.datadoghq.com/account/settings#agent/aws
+# cp ./setup/etc/rsyslog.d/sidekiq /etc/rsyslog.d/sidekiq
+
 # https://papertrailapp.com/systems/setup
