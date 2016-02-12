91c4a2c2ad72ae0acd2158f8e0dc13f83e7d90c2
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-02-13 00:51:47 +0900

    update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -1,6 +1,8 @@
+sudo yum update -y
 sudo yum groupinstall -y "Development Tools"
 sudo yum install -y git tmux dstat monit tree dstat mysql-server mysql-devel ruby22-devel
 sudo update-alternatives --set ruby /usr/bin/ruby2.2
+
 gem install --no-ri --no-rdoc bundler
 git clone https://github.com/ts-3156/egotter.git
 cd egotter
@@ -10,10 +12,9 @@ sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo yum --enablerepo=remi,epel install -y redis
 sudo chkconfig redis on
 sudo service redis start
-# save ""
 
-sudo service mysqld start
-sudo chkconfig mysqld on
+# sudo chkconfig mysqld on
+# sudo service mysqld start
 
 # set httpd port 2812 and
 #     use address localhost
@@ -28,7 +29,7 @@ bundle exec rake db:create db:migrate
 bundle exec rails s --binding=0.0.0.0
 
 # https://gist.githubusercontent.com/ts-3156/5373957/raw/e179785a48f88e120285be6978f0ea578d308628/.bashrc
-# bot.json, cluster_bad_words.json, cluster_good_words.json
+# bot.json, cluster_bad_words.json, cluster_good_words.json, .env
 
 # 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * * /bin/bash -l -c 'cd /home/ec2-user/egotter && RAILS_ENV=development bundle exec rake update_job_dispatcher:run --silent >> /home/ec2-user/egotter/log/crontab.log 2>&1'
 
