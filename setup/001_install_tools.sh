abbbe19219cb1d540946bc47114c971460140dad
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-02-21 22:42:53 +0900

    install colordiff

--- setup/001_install_tools.sh
+++ setup/001_install_tools.sh
@@ -6,5 +6,9 @@ sudo yum install -y git tmux dstat monit tree dstat mysql-server mysql-devel rub
 sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo yum --enablerepo=remi,epel install -y redis
 
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/colordiff-1.0.6a-2.el5.noarch.rpm
+sudo rpm -Uvh colordiff-1.0.6a-2.el5.noarch.rpm
+rm colordiff-1.0.6a-2.el5.noarch.rpm
+
 sudo update-alternatives --set ruby /usr/bin/ruby2.2
 gem install --no-ri --no-rdoc bundler
