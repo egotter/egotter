d2a0cbc76e60de6f3da1302fdafe456c258b62e9
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-02-21 22:58:21 +0900

    set datetime when the notification is sent

--- app/workers/notification_worker.rb
+++ app/workers/notification_worker.rb
@@ -13,10 +13,12 @@ class NotificationWorker
     if options[:type] == BACKGROUND_SEARCH && user.notification.can_send_search?
       text = I18n.t('dictionary.your_account_is_searched', kaomoji: Kaomoji.unhappy, url: "http://egotter.com/searches/#{user.uid}?screen_name=#{user.screen_name}", menu_url: 'http://egotter.com/menu')
       client.create_direct_message(user.uid.to_i, text)
+      user.notification.update(last_search_at: Time.zone.now)
       logger.debug "send dm to #{user.uid},#{user.screen_name} #{options[:type]} #{text}"
     elsif options[:type] == BACKGROUND_UPDATE && user.notification.can_send_dm?
       text = I18n.t('dictionary.your_account_is_updated', kaomoji: Kaomoji.happy, url: "http://egotter.com/searches/#{user.uid}?screen_name=#{user.screen_name}", menu_url: 'http://egotter.com/menu')
       client.create_direct_message(user.uid.to_i, text)
+      user.notification.update(last_dm_at: Time.zone.now)
       logger.debug "send dm to #{user.uid},#{user.screen_name} #{options[:type]} #{text}"
     else
       logger.debug "do nothing #{user.uid},#{user.screen_name} #{options[:type]}"
