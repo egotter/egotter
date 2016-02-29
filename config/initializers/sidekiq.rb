8ee4e19f51b8bde8e5e97c2762d976c446e275d0
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-03-01 00:25:27 +0900

    use redis at another host

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,9 @@
 Sidekiq::Logging.logger.level = Logger::DEBUG
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379" }
+end
