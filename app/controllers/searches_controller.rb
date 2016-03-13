e058086aafd932a6f23c15a71451d362099c591f
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-03-13 21:43:54 +0900

    fix start date

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -420,7 +420,7 @@ class SearchesController < ApplicationController
     @debug_info = Hashie::Mash.new(JSON.parse(redis.get(Redis.debug_info_key) || '{}'))
     @last_1hour = 1.hour.ago..Time.now
     @last_1day = 1.day.ago..Time.now
-    @last_1week = 1.week.ago..Time.now
+    @last_1week = (1.week.ago + 1.day)..Time.now
     render layout: false
   end
 
