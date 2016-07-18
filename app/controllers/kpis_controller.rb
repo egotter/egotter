43eb466e3c976ca89c2a3ce14f5c831aa0c2e413
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-07-19 02:25:45 +0900

    add new user on kpis page

--- app/controllers/kpis_controller.rb
+++ app/controllers/kpis_controller.rb
@@ -4,6 +4,7 @@ class KpisController < ApplicationController
   def index
     @dau = dau
     @search_num = search_num
+    @new_user = new_user
   end
 
   private
@@ -45,6 +46,26 @@ class KpisController < ApplicationController
     result = SearchLog.find_by_sql([sql, {date: (Time.zone.now - 14.days).to_date.to_s}])
 
     %i(total guest login).map do |legend|
+      {
+        name: legend,
+        data: result.map{|r| [ActiveSupport::TimeZone['UTC'].parse(r.date.to_s).to_i * 1000, r.send(legend)] }
+      }
+    end
+  end
+
+  def new_user
+    sql = <<-'EOS'.strip_heredoc
+      SELECT
+        date(created_at) date,
+        count(*) count
+      FROM users
+      WHERE created_at >= :date
+      GROUP BY date(created_at)
+      ORDER BY date(created_at);
+    EOS
+    result = SearchLog.find_by_sql([sql, {date: (Time.zone.now - 14.days).to_date.to_s}])
+
+    %i(total).map do |legend|
       {
         name: legend,
         data: result.map{|r| [ActiveSupport::TimeZone['UTC'].parse(r.date.to_s).to_i * 1000, r.send(legend)] }
