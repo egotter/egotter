87454b617b8367d27f3a53981e5e2522b32c8503
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-07-23 13:56:47 +0900

    show action_name in debug message

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -388,7 +388,7 @@ class SearchesController < ApplicationController
   def set_twitter_user
     uid = params.has_key?(:id) ? params[:id].match(/\A\d+\z/)[0].to_i : -1
     if uid.in?([-1, 0])
-      logger.warn "#{self.class}##{__method__}: The uid is invalid #{params[:id]} #{current_user_id} #{request.device_type}."
+      logger.warn "#{self.class}##{__method__}: The uid is invalid #{params[:id]} #{current_user_id} #{action_name} #{request.device_type}."
       return redirect_to '/', alert: t('before_sign_in.that_page_doesnt_exist')
     end
 
@@ -397,7 +397,7 @@ class SearchesController < ApplicationController
       tu.assign_attributes(client: client, egotter_context: 'search')
       @searched_tw_user = tu
     else
-      logger.warn "#{self.class}##{__method__}: The TwitterUser doesn't exist #{uid} #{current_user_id} #{request.device_type}."
+      logger.warn "#{self.class}##{__method__}: The TwitterUser doesn't exist #{uid} #{current_user_id} #{action_name} #{request.device_type}."
       redirect_to '/', alert: t('before_sign_in.that_page_doesnt_exist')
     end
   end
