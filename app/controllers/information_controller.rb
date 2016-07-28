48e508216a0a6773caf2362bf22100b0c00f3272
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-07-28 23:20:06 +0900

    add today's information

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,6 +1,7 @@
 class InformationController < ApplicationController
   def show
     lists = [
+      '2016/07/28 検索を高速化しました',
       '2016/07/27 トップページのレイアウトを修正しました',
       '2016/07/26 トップページのレイアウトを修正しました',
       '2016/07/25 ページの読み込みを高速化しました',
