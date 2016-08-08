76704fc430550d5dd47975befcbb0f7b708b5397
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-08-09 00:43:20 +0900

    add new information

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -3,6 +3,7 @@ class InformationController < ApplicationController
 
   def index
     @lists = [
+      %w(2016/08/09 表示の高速化を行いました),
       %w(2016/08/06 表示の高速化を行いました),
       %w(2016/08/01 レイアウトの調整を行いました),
       %w(2016/07/31 通知ページを作成しました),
