d6cf7ec1c4e3d8211ed6c2d2a35abdaa3b4aad37
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-09-01 23:33:21 +0900

    implement toggle switches

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -2,9 +2,11 @@ class SearchHistoriesController < ApplicationController
   include SearchesHelper
   include SearchHistoriesHelper
 
+  layout false
+
   def index
     in_modal = params.has_key?(:in_modal) && params[:in_modal] == 'true' ? true : false
-    html = render_to_string(partial: 'search_histories/items', locals: {search_histories: build_search_histories, in_modal: in_modal})
+    html = render_to_string(locals: {search_histories: build_search_histories, in_modal: in_modal})
     render json: {html: html}, status: 200
   rescue => e
     logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message}"
