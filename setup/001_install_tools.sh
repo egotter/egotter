f672cec4521eb9d2caf8152c1debcc1f498caa42
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-09-03 00:32:05 +0900

    modify install script

--- setup/001_install_tools.sh
+++ setup/001_install_tools.sh
@@ -2,13 +2,10 @@
 
 sudo yum update -y
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y git tmux dstat monit tree dstat mysql-server mysql-devel ruby22-devel nginx
+sudo yum install -y git tmux dstat htop monit tree mysql-server mysql-devel ruby23-devel nginx
 sudo rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-sudo yum --enablerepo=remi,epel install -y redis
+sudo yum install -y redis --enablerepo=remi
+sudo yum install -y colordiff --enablerepo=epel
 
-wget http://dl.fedoraproject.org/pub/epel/5/x86_64/colordiff-1.0.6a-2.el5.noarch.rpm
-sudo rpm -Uvh colordiff-1.0.6a-2.el5.noarch.rpm
-rm colordiff-1.0.6a-2.el5.noarch.rpm
-
-sudo update-alternatives --set ruby /usr/bin/ruby2.2
-gem install --no-ri --no-rdoc bundler
+sudo update-alternatives --set ruby /usr/bin/ruby2.3
+gem install bundler --no-ri --no-rdoc
