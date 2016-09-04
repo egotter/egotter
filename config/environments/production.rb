5d966c8becbcb35636c3aed46985b1bb948a01f4
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-09-04 13:02:55 +0900

    set default protocol and host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:protocol] = 'https'
+  Rails.application.routes.default_url_options[:host] = 'egotter.com'
 end
