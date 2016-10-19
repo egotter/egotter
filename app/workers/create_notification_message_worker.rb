fd33c1383d56742ca8da56882825d364c14b352e
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-20 06:23:24 +0900

    implement notification by onesignal

--- app/workers/create_notification_message_worker.rb
+++ app/workers/create_notification_message_worker.rb
@@ -2,17 +2,51 @@ class CreateNotificationMessageWorker
   include Sidekiq::Worker
   sidekiq_options queue: :egotter, retry: false, backtrace: false
 
-  def perform(attrs)
-    notification = NotificationMessage.new(attrs)
-    user = User.find(notification.user_id)
+  def perform(user_id, uid, screen_name, options)
+    user_id = user_id.to_i
+    uid = uid.to_i
+    type = options['type']
+    mention_name = "@#{screen_name}"
+    medium = options['medium']
 
-    if user.notification_setting.can_send_search?
-      user.api_client.create_direct_message(user.uid.to_i, notification.message)
-      user.notification_setting.touch(:last_dm_at)
-      notification.save!
+    url = Rails.application.routes.url_helpers.search_url(screen_name: screen_name, id: uid, medium: medium)
+
+    if %w(search update).include?(type) && medium == 'dm'
+      message = I18n.t("dm.#{type}Notification.title", user: mention_name, url: url) +
+        I18n.t("dm.#{type}Notification.message", url: url)
+      notification = NotificationMessage.new(user_id: user_id, uid: uid, screen_name: screen_name, message: message, medium: medium)
+      user = User.find(user_id)
+
+      if user.notification_setting.can_send_search?
+        user.api_client.create_direct_message(user.uid.to_i, notification.message)
+        user.notification_setting.touch(:last_search_at)
+        notification.save!
+      end
+
+      return
     end
 
+    if %w(search update).include?(type) && medium == 'onesignal'
+      headings = %i(en ja).map do |locale|
+        [locale, I18n.t("onesignal.#{type}Notification.title", user: mention_name, locale: locale)]
+      end.to_h
+      contents = %i(en ja).map do |locale|
+        [locale, I18n.t("onesignal.#{type}Notification.message", url: url, locale: locale)]
+      end.to_h
+
+      message = contents[:ja]
+      notification = NotificationMessage.new(user_id: user_id, uid: uid, screen_name: screen_name, message: message, medium: medium)
+      user = User.find(user_id)
+
+      if user.notification_setting.can_send_search?
+        Onesignal.new(user.id, headings: headings, contents: contents, url: url).send
+        user.notification_setting.touch(:last_search_at)
+        notification.save!
+      end
+
+      return
+    end
   rescue => e
-    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{attrs.inspect}"
+    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{user_id} #{uid} #{screen_name} #{options.inspect}"
   end
 end
--- app/workers/create_twitter_user_worker.rb
+++ app/workers/create_twitter_user_worker.rb
@@ -5,8 +5,7 @@ class CreateTwitterUserWorker
   def perform(values)
     user_id      = values['user_id'].to_i
     uid          = values['uid'].to_i
-    url          = values['url']
-    log_attrs = {
+    log = BackgroundSearchLog.new(
       session_id:  values['session_id'],
       user_id:     user_id,
       uid:         uid,
@@ -19,106 +18,75 @@ class CreateTwitterUserWorker
       browser:     values['browser'],
       user_agent:  values['user_agent'],
       referer:     values['referer'],
-      channel:     values['channel'],
-    }
-    client = User.exists?(user_id) ? User.find(user_id).api_client : Bot.api_client
-    log_attrs.update(bot_uid: client.verify_credentials.id)
+      channel:     values['channel']
+    )
+    user = User.find_by(id: user_id)
+    client = user.nil? ? Bot.api_client : user.api_client
+    log.bot_uid = client.verify_credentials.id
 
     existing_tu = TwitterUser.latest(uid)
     if existing_tu.present? && existing_tu.fresh?
       existing_tu.increment(:search_count).save
-      create_log(true, log_attrs, call_count: client.call_count, message: 'Recently created record exists.')
-      send_notification_message(user_id, existing_tu, url)
+      log.update(status: true, call_count: client.call_count, message: "[#{existing_tu.id}] is recently updated.")
+      notify(user, existing_tu) unless user.nil?
       return
     end
 
-    new_tu = TwitterUser.build_with_relations(client.user(uid), client: client, login_user: User.find_by(id: user_id), context: :search)
-    new_tu.user_id = user_id
+    new_tu = TwitterUser.build_with_relations(client.user(uid), client: client, login_user: user, context: :search)
+    new_tu.user_id = user.id
     if new_tu.save
       new_tu.increment(:search_count).save
-      create_log(true, log_attrs, call_count: client.call_count, message: 'creates a new TwitterUser.')
-      send_notification_message(user_id, new_tu, url)
+      log.update(status: true, call_count: client.call_count, message: "[#{new_tu.id}] is created.")
+      notify(user, new_tu) unless user.nil?
       return
     end
 
     if existing_tu.present?
       existing_tu.increment(:search_count).save
-      create_log(true, log_attrs, call_count: client.call_count, message: 'Existing one is the same as new one.')
-      send_notification_message(user_id, existing_tu, url)
+      log.update(status: true, call_count: client.call_count, message: "[#{existing_tu.id}] is not changed.")
+      notify(user, existing_tu) unless user.nil?
       return
     end
 
-    create_log(
-      false,
-      log_attrs,
+    log.update(
+      status: false,
       call_count: client.call_count,
       reason: BackgroundSearchLog::SomethingError::MESSAGE,
       message: "#{new_tu.errors.full_messages.join(', ')}."
     )
   rescue Twitter::Error::TooManyRequests => e
-    create_log(
-      false,
-      log_attrs,
+    log.update(
+      status: false,
       call_count: client.call_count,
       reason: BackgroundSearchLog::TooManyRequests::MESSAGE
     )
   rescue Twitter::Error::Unauthorized => e
-    create_log(
-      false,
-      log_attrs,
+    log.update(
+      status: false,
       call_count: client.call_count,
       reason: BackgroundSearchLog::Unauthorized::MESSAGE
     )
   rescue => e
     logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message}"
     logger.info e.backtrace.slice(0, 10).join("\n")
-    create_log(
-      false,
-      log_attrs,
+    log.update(
+      status: false,
       call_count: client.call_count,
       reason: BackgroundSearchLog::SomethingError::MESSAGE,
       message: "#{e.class} #{e.message}"
     )
   end
 
-  def send_notification_message(login_user_id, tu, url)
+  def notify(login_user, tu)
     searched_user = User.find_by(uid: tu.uid)
     return if searched_user.nil?
 
-    unless login_user_id == searched_user.id
-      CreateNotificationMessageWorker.perform_async(
-        user_id: login_user_id,
-        uid: tu.uid,
-        screen_name: tu.screen_name,
-        message: I18n.t('dictionary.you_are_searched', kaomoji: Kaomoji.unhappy, url: url),
-        medium: 'dm'
-      )
+    if login_user.id != searched_user.id
+      %w(dm onesignal).each do |medium|
+        CreateNotificationMessageWorker.perform_async(login_user.id, tu.uid.to_i, tu.screen_name, type: 'search', medium: medium)
+      end
     end
   rescue => e
-    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{login_user_id} #{tu.inspect} #{url}"
-  end
-
-  def create_log(status, attrs, call_count: -1, reason: '', message: '')
-    BackgroundSearchLog.create!(
-      session_id:  attrs[:session_id],
-      user_id:     attrs[:user_id],
-      uid:         attrs[:uid],
-      screen_name: attrs[:screen_name],
-      bot_uid:     attrs[:bot_uid],
-      auto:        attrs[:auto],
-      status:      status,
-      reason:      reason,
-      message:     message,
-      call_count:  call_count,
-      via:         attrs[:via],
-      device_type: attrs[:device_type],
-      os:          attrs[:os],
-      browser:     attrs[:browser],
-      user_agent:  attrs[:user_agent],
-      referer:     attrs[:referer],
-      channel:     attrs[:channel],
-    )
-  rescue => e
-    logger.warn "#{self.class}##{__method__} #{e.class} #{e.message} #{status} #{attrs.inspect} #{reason} #{message}"
+    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{login_user.id} #{tu.inspect}"
   end
 end
