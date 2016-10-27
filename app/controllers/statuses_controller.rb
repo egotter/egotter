cb5cc6f43fdcdfbee1d2f80f81507e1fe0353486
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-27 20:53:51 +0900

    rename from statuses/_items.html.haml to statuses/_status.html.haml

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -14,7 +14,7 @@ class StatusesController < ApplicationController
 
   # GET /statuses/:id
   def show
-    @status_items = Kaminari.paginate_array(@searched_tw_user.statuses).page(params[:page]).per(100)
+    @statuses = Kaminari.paginate_array(@searched_tw_user.statuses).page(params[:page]).per(100)
     @title = t('.title', user: @searched_tw_user.mention_name)
   end
 
@@ -22,8 +22,8 @@ class StatusesController < ApplicationController
     json = redis.fetch('keyword_timeline', ttl: 1.day) do
       Bot.api_client.search(t('dictionary.app_name')).slice(0, 5).map { |t| t.to_hash }.to_json
     end
-    tweets = JSON.load(json).slice(0, 5).map { |t| Hashie::Mash.new(t) }.map { |t| t.tweeted_at = t.created_at; t }
-    html = render_to_string(partial: 'statuses/items', locals: {items: tweets})
+    tweets = JSON.load(json).take(5).map { |t| Hashie::Mash.new(t) }.map { |t| t.tweeted_at = t.created_at; t }
+    html = render_to_string(partial: 'statuses/status', collection: tweets, cached: true)
     render json: {html: html}, status: 200
   rescue => e
     logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message}"
