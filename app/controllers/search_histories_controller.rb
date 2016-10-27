209dfa718cbe097a1eadf8f898ad107b125abb4b
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-28 02:08:09 +0900

    refactoring of search_histories

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -5,9 +5,10 @@ class SearchHistoriesController < ApplicationController
   layout false
 
   def index
-    in_modal = params.has_key?(:in_modal) && params[:in_modal] == 'true' ? true : false
-    html = redis.fetch("search_histories:#{current_user_id}:#{in_modal}", ttl: 5.minutes) do
-      render_to_string(locals: {search_histories: build_search_histories(current_user_id), in_modal: in_modal})
+    @in_modal = params.has_key?(:in_modal) && params[:in_modal] == 'true' ? true : false
+    html = redis.fetch("search_histories:#{current_user_id}:#{@in_modal}", ttl: 5.minutes) do
+      @search_histories = build_search_histories(current_user_id)
+      render_to_string
     end
 
     render json: {html: html}, status: 200
