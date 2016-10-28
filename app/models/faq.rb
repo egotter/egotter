bfeaf7885905de34ae7545b50ab0f41bb314002b
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-28 19:43:28 +0900

    rename from misc/_faq_items.html.erb to misc/_faq.html.erb

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,6 +1,7 @@
 class Faq
   def self.init
     @@faq ||= JSON.parse(File.read(Rails.configuration.x.constants['faq_path'])).map { |b| Hashie::Mash.new(b) }
+    @@faq.each.with_index { |f, i| f[:i] = i }
     raise 'create Faq' if @@faq.empty?
   end
 
