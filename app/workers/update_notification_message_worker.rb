d8fe27c7f534f5749795294499d019c77277d8f5
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-10-29 13:28:04 +0900

    check `read` is true or not when updating NotificationMessage

--- app/workers/update_notification_message_worker.rb
+++ app/workers/update_notification_message_worker.rb
@@ -3,7 +3,7 @@ class UpdateNotificationMessageWorker
   sidekiq_options queue: :egotter, retry: false, backtrace: false
 
   def perform(attrs)
-    NotificationMessage.order(created_at: :desc).find_by(token: attrs['token']).update!(read: true, read_at: attrs['read_at'])
+    NotificationMessage.order(created_at: :desc).find_by(token: attrs['token'], read: false).update!(read: true, read_at: attrs['read_at'])
   rescue => e
     logger.warn "#{e.class}: #{e.message} #{attrs.inspect}"
   end
