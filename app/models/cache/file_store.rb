50d4e1c6e37c876fe4c37830c8faa6dbf7ddcfa3
Shinohara Teruki <ts_3156@yahoo.co.jp>
2016-11-21 22:27:22 +0900

    change page cache key

--- app/models/cache/file_store.rb
+++ app/models/cache/file_store.rb
@@ -10,7 +10,7 @@ module Cache
     end
 
     def key_prefix
-      'v9-searches-file-store'
+      'va-searches-file-store'
     end
 
     def key_suffix
@@ -51,7 +51,7 @@ module Cache
     end
 
     def cleanup
-      store.delete_matched(/^(v[2-8]-)?searches-file-store:/)
+      store.delete_matched(/^(v[2-9]-)?searches-file-store:/)
     end
 
     def ttl(uid)
