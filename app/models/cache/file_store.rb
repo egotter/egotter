e273fda66f2ebb967d28bb6ab3d775390236ce28
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-01-31 19:47:36 +0900

    show breadcrumb list

--- app/models/cache/file_store.rb
+++ app/models/cache/file_store.rb
@@ -10,7 +10,7 @@ module Cache
     end
 
     def key_prefix
-      'vg-searches-file-store'
+      'vh-searches-file-store'
     end
 
     def key_suffix
@@ -51,7 +51,7 @@ module Cache
     end
 
     def cleanup
-      store.delete_matched(/^(v[2-9a-f]-)?searches-file-store:/)
+      store.delete_matched(/^(v[2-9a-g]-)?searches-file-store:/)
     end
 
     def ttl(uid)
---
+++ config/breadcrumbs.rb
@@ -0,0 +1,28 @@
+crumb :root do
+  link t('searches.common.top'), root_path
+end
+
+crumb :search do |screen_name|
+  link t('searches.show.simple_title', user: mention_name(screen_name)), search_path(screen_name: screen_name)
+  parent :root
+end
+
+crumb :status do |uid, screen_name|
+  link t('searches.common.tweet'), status_path(uid: uid)
+  parent :search, screen_name
+end
+
+crumb :common do |screen_name, menu|
+  link t("searches.#{menu}.name"), send("#{menu}_search_path", screen_name: screen_name)
+  parent :search, screen_name
+end
+
+crumb :one_sided_friend do |screen_name|
+  link t('one_sided_friends.new.simple_title'), one_sided_friend_path(screen_name: screen_name)
+  parent :search, screen_name
+end
+
+crumb :unfriend do |screen_name|
+  link t('unfriends.new.simple_title'), unfriend_path(screen_name: screen_name)
+  parent :search, screen_name
+end
