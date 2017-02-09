4052f5b9deaf3c7af653b9054ff92783ee9671ef
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-09 23:21:23 +0900

    set lastmod in sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,52 +1,50 @@
 SitemapGenerator::Sitemap.default_host = 'https://egotter.com'
 
 SitemapGenerator::Sitemap.create do
-  add one_sided_friends_top_path, priority: 0.8, changefreq: 'daily'
-  add unfriends_top_path, priority: 0.8, changefreq: 'daily'
+  options = {priority: 0.8}
 
-  processed = []
+  add one_sided_friends_top_path, options
+  add unfriends_top_path, options
+  # add relationships_top_path, options
+
   Rails.logger.silence do
-    TwitterUser.with_friends.find_in_batches(start: 1, batch_size: 1000) do |tu_array|
-      uids = tu_array.select { |tu| processed.exclude?(tu.uid.to_i) }.map(&:uid).uniq
+    TwitterUser.pluck(:uid).uniq.each do |uid|
+      twitter_user = TwitterUser.latest(uid)
+      next if twitter_user.protected_account?
 
-      uids.each do |uid|
-        twitter_user = TwitterUser.order(created_at: :desc).find_by(uid: uid)
-        if twitter_user.protected_account?
-          processed << uid.to_i
-          next
-        end
+      options = {priority: 0.5, changefreq: 'weekly', lastmod: twitter_user.updated_at}
 
-        screen_name = twitter_user.screen_name
-        add search_path(screen_name: screen_name), changefreq: 'daily'
-        add status_path(uid: uid), changefreq: 'daily'
-        add one_sided_friend_path(screen_name: screen_name, type: 'one_sided_friends'), changefreq: 'daily'
-        add one_sided_friend_path(screen_name: screen_name, type: 'one_sided_followers'), changefreq: 'daily'
-        add one_sided_friend_path(screen_name: screen_name, type: 'mutual_friends'), changefreq: 'daily'
-        add unfriend_path(screen_name: screen_name, type: 'removing'), changefreq: 'daily'
-        add unfriend_path(screen_name: screen_name, type: 'removed'), changefreq: 'daily'
-        add unfriend_path(screen_name: screen_name, type: 'blocking_or_blocked'), changefreq: 'daily'
+      screen_name = twitter_user.screen_name
+      add search_path(screen_name: screen_name), options
+      add status_path(uid: uid), options
+      add one_sided_friend_path(screen_name: screen_name, type: 'one_sided_friends'), options
+      add one_sided_friend_path(screen_name: screen_name, type: 'one_sided_followers'), options
+      add one_sided_friend_path(screen_name: screen_name, type: 'mutual_friends'), options
+      add unfriend_path(screen_name: screen_name, type: 'removing'), options
+      add unfriend_path(screen_name: screen_name, type: 'removed'), options
+      add unfriend_path(screen_name: screen_name, type: 'blocking_or_blocked'), options
 
-        %i(
-          friends
-          followers
-          new_friends
-          new_followers
-          replying
-          replied
-          favoriting
-          inactive_friends
-          inactive_followers
-          clusters_belong_to
-          close_friends
-          usage_stats
-        ).each { |menu| add send("#{menu}_search_path", screen_name: screen_name), changefreq: 'daily' }
+      %i(
+        friends
+        followers
+        new_friends
+        new_followers
+        replying
+        replied
+        favoriting
+        inactive_friends
+        inactive_followers
+        clusters_belong_to
+        close_friends
+        usage_stats
+      ).each { |menu| add send("#{menu}_search_path", screen_name: screen_name), options }
 
-        add support_path
-        add terms_of_service_path
-        add privacy_policy_path
-
-        processed << uid.to_i
-      end
     end
+
+    options = {priority: 0.3}
+
+    add support_path, options
+    add terms_of_service_path, options
+    add privacy_policy_path, options
   end
 end
