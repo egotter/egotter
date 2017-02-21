97eefff1bc4903c3c2d87474f5351dadc5054cff
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-21 20:02:05 +0900

    rename from xxx_link to xxx_link_html

--- app/controllers/page_caches_controller.rb
+++ app/controllers/page_caches_controller.rb
@@ -15,11 +15,11 @@ class PageCachesController < ApplicationController
   before_action(only: %i(create destroy)) { create_page_cache_log(action_name) }
 
   def create
-    tu = @twitter_user
-    ::Cache::PageCache.new.write(tu.uid, render_to_string(template: 'search_results/show'))
+    @searched_tw_user = @twitter_user # for search_results/show
+    ::Cache::PageCache.new.write(@twitter_user.uid, render_to_string(template: 'search_results/show'))
     head :ok
   rescue => e
-    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{current_user_id} #{tu.uid} #{tu.screen_name} #{request.browser}"
+    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{current_user_id} #{@twitter_user.uid} #{@twitter_user.screen_name} #{request.browser}"
     head :internal_server_error
   end
 
