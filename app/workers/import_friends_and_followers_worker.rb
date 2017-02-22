fc8bfda8d32cc45b33d46c62c65cb0f94991d222
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-22 19:49:25 +0900

    retry in workers when a deadlock is found

--- app/workers/import_friends_and_followers_worker.rb
+++ app/workers/import_friends_and_followers_worker.rb
@@ -11,47 +11,70 @@ class ImportFriendsAndFollowersWorker
       {method: :followers, args: [uid]}
     ]
 
-    # This process takes a few seconds.
-    t_user, friends, followers = client._fetch_parallelly(signatures)
+    t_user = nil
+    friends = followers = []
+    ActiveRecord::Base.benchmark("[benchmark] #{self.class}#fetch friends and followers") do
+      t_user, friends, followers = client._fetch_parallelly(signatures)
+    end
+
     users = []
 
-    ActiveRecord::Base.benchmark("benchmark #{self.class}#build friends") do
-      users =
-        friends.map do |friend|
-          [friend.id, friend.screen_name, friend.slice(*TwitterUser::PROFILE_SAVE_KEYS).to_json, -1, -1]
-        end if friends&.any?
-    end
+    ActiveRecord::Base.benchmark("[benchmark] #{self.class}#build friends") do
+      users.concat(friends.map { |f| to_array(f) })
+    end if friends&.any?
 
-    ActiveRecord::Base.benchmark("benchmark #{self.class}#build followers") do
-      users +=
-        followers.map do |follower|
-          [follower.id, follower.screen_name, follower.slice(*TwitterUser::PROFILE_SAVE_KEYS).to_json, -1, -1]
-        end if followers&.any?
-    end
+    ActiveRecord::Base.benchmark("[benchmark] #{self.class}#build followers") do
+      users.concat(followers.map { |f| to_array(f) })
+    end if followers&.any?
 
-    user_info = t_user.slice(*TwitterUser::PROFILE_SAVE_KEYS).to_json
-    users << [uid, t_user.screen_name, user_info, -1, -1]
+    users << to_array(t_user)
+    users.uniq!(&:first).sort_by!(&:first)
 
-    ActiveRecord::Base.benchmark("benchmark #{self.class}#import TwitterDB::User") { Rails.logger.silence { ActiveRecord::Base.transaction {
-      users.uniq(&:first).sort_by(&:first).each_slice(1000) do |array|
-        TwitterDB::User.import(%i(uid screen_name user_info friends_size followers_size), array, on_duplicate_key_update: %i(uid screen_name user_info), validate: false)
+    create_columns = %i(uid screen_name user_info friends_size followers_size)
+    update_columns = %i(uid screen_name user_info)
+    retrying = false
+    begin
+      ActiveRecord::Base.benchmark("[benchmark] #{self.class}#import TwitterDB::User") { Rails.logger.silence { ActiveRecord::Base.transaction {
+        users.each_slice(1000) do |array|
+          TwitterDB::User.import(create_columns, array, on_duplicate_key_update: update_columns, validate: false)
+        end
+      }}}
+    rescue ActiveRecord::StatementInvalid => e
+      if !retrying && e.message.start_with?('Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction')
+        retrying = true
+        retry
       end
-    }}}
+      raise
+    end
 
-    ActiveRecord::Base.benchmark("benchmark #{self.class}#import TwitterDB::Friendship and TwitterDB::Followership") { Rails.logger.silence { ActiveRecord::Base.transaction {
-      TwitterDB::Friendship.import_from!(uid, friends.map(&:id)) if friends&.any?
-      TwitterDB::Followership.import_from!(uid, followers.map(&:id)) if followers&.any?
-
-      TwitterDB::User.find_by(uid: uid).tap { |me| me.update_columns(friends_size: me.friendships.size, followers_size: me.followerships.size) }
-    }}}
+    retrying = false
+    begin
+      ActiveRecord::Base.benchmark("[benchmark] #{self.class}#import TwitterDB::Friendship and TwitterDB::Followership") { Rails.logger.silence { ActiveRecord::Base.transaction {
+        TwitterDB::Friendship.import_from!(uid, friends.map(&:id)) if friends&.any?
+        TwitterDB::Followership.import_from!(uid, followers.map(&:id)) if followers&.any?
+        TwitterDB::User.find_by(uid: uid).tap { |me| me.update_columns(friends_size: me.friendships.size, followers_size: me.followerships.size) }
+      }}}
+    rescue ActiveRecord::StatementInvalid => e
+      if !retrying && e.message.start_with?('Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction')
+        retrying = true
+        retry
+      end
+      raise
+    end
 
     Rails.logger.info "[worker] #{self.class} finished. #{user_id} #{uid} #{t_user.screen_name}"
 
-  rescue ActiveRecord::StatementInvalid => e
-    logger.warn "#{self.class}: #{e.class} #{user_id} #{uid}"
+    # ImportInactiveFriendsAndInactiveFollowersWorker.perform_async(user_id, uid) if friends&.any? || followers&.any?
+
   rescue => e
     message = e.message.truncate(150)
     logger.warn "#{self.class}: #{e.class} #{message} #{user_id} #{uid}"
     logger.info e.backtrace.join "\n"
+  end
+
+  private
+
+  def to_array(user)
+    [user.id, user.screen_name, user.slice(*TwitterUser::PROFILE_SAVE_KEYS).to_json, -1, -1]
   end
 end
