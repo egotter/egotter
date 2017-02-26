36213068984a11e00a83d64c97427fe341ddada1
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-26 12:25:18 +0900

    add new information

--- app/models/cache/file_store.rb
+++ app/models/cache/file_store.rb
@@ -10,7 +10,7 @@ module Cache
     end
 
     def key_prefix
-      'vo-searches-file-store'
+      'vp-searches-file-store'
     end
 
     def key_suffix
@@ -51,7 +51,7 @@ module Cache
     end
 
     def cleanup
-      store.delete_matched(/^(v[2-9a-n]-)?searches-file-store:/)
+      store.delete_matched(/^(v[2-9a-o]-)?searches-file-store:/)
     end
 
     def ttl(uid)
