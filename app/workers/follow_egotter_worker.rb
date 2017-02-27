1818641f9ec16bfbb682f5efb5a101802644a967
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-27 19:44:31 +0900

    run FollowEgotterWorker on another sidekiq process

--- app/workers/follow_egotter_worker.rb
+++ app/workers/follow_egotter_worker.rb
@@ -1,6 +1,6 @@
 class FollowEgotterWorker
   include Sidekiq::Worker
-  sidekiq_options queue: self, retry: false, backtrace: false
+  sidekiq_options queue: self, retry: 1, backtrace: false
 
   def perform(user_id)
     user = User.find(user_id)
@@ -8,7 +8,13 @@ class FollowEgotterWorker
     unless client.friendship?(user.uid.to_i, User::EGOTTER_UID)
       client.follow!(User::EGOTTER_UID)
     end
+  rescue Twitter::Error::Unauthorized => e
+    logger.warn "#{e.class}: #{e.message} #{user_id}"
+    if e.message == 'Invalid or expired token.'
+      user.update(authorized: false)
+    end
   rescue => e
-    logger.warn "#{e.class}: #{e.message}"
+    logger.warn "#{e.class}: #{e.message} #{user_id}"
+    raise
   end
 end
