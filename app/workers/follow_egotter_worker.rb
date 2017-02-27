c8872d8ea934443a7bab834c4f62d09cedbaaf41
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-02-27 21:29:58 +0900

    sleep 1 hour in FollowEgotterWorker if rate limit is exceeded.

--- app/workers/follow_egotter_worker.rb
+++ app/workers/follow_egotter_worker.rb
@@ -1,6 +1,6 @@
 class FollowEgotterWorker
   include Sidekiq::Worker
-  sidekiq_options queue: self, retry: 1, backtrace: false
+  sidekiq_options queue: self, retry: false, backtrace: false
 
   def perform(user_id)
     user = User.find(user_id)
@@ -8,13 +8,22 @@ class FollowEgotterWorker
     unless client.friendship?(user.uid.to_i, User::EGOTTER_UID)
       client.follow!(User::EGOTTER_UID)
     end
+
   rescue Twitter::Error::Unauthorized => e
-    logger.warn "#{e.class}: #{e.message} #{user_id}"
+    message = "#{e.class}: #{e.message} #{user_id}"
     if e.message == 'Invalid or expired token.'
-      user.update(authorized: false)
+      message += user.update(authorized: false) ? ' set unauthorized.' : ' try to set unauthorized but failed.'
+    end
+    logger.warn message
+  rescue Twitter::Error::Forbidden => e
+    logger.warn "#{e.class}: #{e.message} #{user_id}"
+    if e.message.start_with? 'You are unable to follow more people at this time.'
+      logger.warn "I will sleep. Bye! #{user_id}"
+      sleep 1.hour
+      logger.warn "Good morning. I will retry. #{user_id}"
+      retry
     end
   rescue => e
     logger.warn "#{e.class}: #{e.message} #{user_id}"
-    raise
   end
 end
