c33090d9fd420bf8e4201eeee15b1cdb43301d7c
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-08 14:28:35 +0900

    sleep while workers are busy in DelayedCreateTwitterUserWorker

--- app/workers/delayed_create_twitter_user_worker.rb
+++ app/workers/delayed_create_twitter_user_worker.rb
@@ -5,6 +5,10 @@ class DelayedCreateTwitterUserWorker < CreateTwitterUserWorker
   private
 
   def before_perform(*args)
+    while (queue = Sidekiq::Queue.new('CreateTwitterUserWorker')).size > CreateTwitterUserWorker::BUSY_QUEUE_SIZE
+      logger.warn "I will sleep. Bye! #{queue.size}"
+      sleep 10.minutes
+    end
     true
   end
 end
