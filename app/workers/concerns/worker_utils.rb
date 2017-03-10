8d51c44a640c98ed9ef89c5c5506995ff4a93acf
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-11 03:31:18 +0900

    implement retry_timeout in worker_utils.rb

--- app/workers/concerns/worker_utils.rb
+++ app/workers/concerns/worker_utils.rb
@@ -10,7 +10,7 @@ module Concerns::WorkerUtils
   end
 
   def short_hour(time)
-    I18n.l(time, format: :short_hour)
+    time.nil? ? '' : I18n.l(time, format: :short_hour)
   end
 
   def _benchmark(message, &block)
@@ -21,20 +21,23 @@ module Concerns::WorkerUtils
     _benchmark(message) { Rails.logger.silence { ActiveRecord::Base.transaction(&block) } }
   end
 
+  RETRY_TIMEOUT = 10.seconds
 
-  def _retry_with_transaction!(message, retry_limit: 1, &block)
+  def _retry_with_transaction!(message, retry_limit: 1, retry_timeout: RETRY_TIMEOUT, &block)
     retry_count = 0
+    start_time = Time.zone.now
+
     begin
       _transaction(message, &block)
     rescue ActiveRecord::StatementInvalid => e
       if e.message.start_with?('Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction')
-        if retry_count < retry_limit
-          retry_count += 1
-          retry
-        else
+        if retry_count >= retry_limit || (Time.zone.now - start_time > retry_timeout)
           @retry_count = retry_count
           raise
         end
+
+        retry_count += 1
+        retry
       end
     end
   end
