23ddbd5904b77253a829dfdda9b6ca2cdfbc76ea
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-14 07:35:50 +0900

    change file cache prefix

--- app/models/cache/file_store.rb
+++ app/models/cache/file_store.rb
@@ -10,7 +10,7 @@ module Cache
     end
 
     def key_prefix
-      'vx-searches-file-store'
+      'vy-searches-file-store'
     end
 
     def key_suffix
@@ -51,7 +51,7 @@ module Cache
     end
 
     def cleanup
-      store.delete_matched(/^(v[2-9a-w]-)?searches-file-store:/)
+      store.delete_matched(/^(v[2-9a-x]-)?searches-file-store:/)
     end
 
     def ttl(uid)
