17a040b4c9c418f5da475716ce586033c0524d29
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-15 22:21:05 +0900

    show kind sign in and sign out message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,13 +12,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         FollowEgotterWorker.perform_async(user.id) if follow
       end
     rescue =>  e
+      logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message}"
       return redirect_to root_path, alert: t('before_sign_in.login_failed_html', sign_in_path: welcome_path(via: "#{controller_name}/#{action_name}/sign_in_failed"))
     end
 
-    delete_uid(user)
+    Util::SearchedUids.new(Redis.client).delete(user.uid)
+    user.update(authorized: true)
 
     sign_in_and_redirect user, event: :authentication
-    set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+    notification_status = t("dictionary.#{user.notification_setting.dm? ? 'enabled' : 'disabled'}")
+    flash[:notice] = t('devise.omniauth_callbacks.success_html', kind: 'Twitter', status: notification_status, menu_path: menu_path) if is_navigational_format?
   end
 
   def failure
@@ -28,13 +31,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
-
-  def delete_uid(user)
-    Util::SearchedUids.new(Redis.client).delete(user.uid)
-    user.update(authorized: true)
-  rescue => e
-    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message} #{user.inspect}"
-  end
 
   def user_params
     Hashie::Mash.new(request.env['omniauth.auth'].slice(:provider, :uid, :info, :credentials).to_h)
