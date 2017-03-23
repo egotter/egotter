2549f2d8bcfd4c8646170e909ccf7ecb335a7a73
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-23 21:35:17 +0900

    implement Bot.rate_limit

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -17,31 +17,54 @@
 #
 
 class Bot < ActiveRecord::Base
+
+  IDS = pluck(:id)
+
+  def self.api_client
+    find(IDS.sample).api_client
+  end
+
   def self.load(path)
     JSON.parse(File.read(path)).each do |bot|
       create!(uid: bot['uid'], screen_name: bot['screen_name'], secret: bot['secret'], token: bot['token'])
     end
   end
 
-  def self.config(screen_name = nil)
-    @@ids ||= pluck(:id)
-    bot = screen_name ? find_by(screen_name: screen_name) : find(@@ids.sample)
-    ApiClient.config(access_token: bot.token, access_token_secret: bot.secret)
-  end
-
-  def self.api_client(screen_name = nil)
-    ApiClient.instance(config(screen_name))
-  end
-
   def self.verify_credentials
     processed = Queue.new
-    Parallel.each_with_index(pluck(:screen_name), in_threads: 10) do |screen_name, i|
-      user = (api_client(screen_name).verify_credentials rescue nil)
-      processed << {i: i, screen_name: screen_name, status: !!user}
+    Parallel.each(all, in_threads: 10) do |bot|
+      processed << {id: bot.id, screen_name: bot.screen_name, status: !!bot.verify_credentials}
     end
 
-    processed.size.times.map { processed.pop }.sort_by { |p| p[:i] }.map do |p|
-      puts "#{p[:screen_name]} #{p[:status]}"
+    processed.size.times.map { processed.pop }.sort_by { |p| p[:id] }
+  end
+
+  def self.rate_limit
+    processed = Queue.new
+    Parallel.each(all, in_threads: 10) do |bot|
+      processed << {id: bot.id, rate_limit: bot.rate_limit}
     end
+
+    processed.size.times.map { processed.pop }.sort_by { |p| p[:id] }.map do |p|
+      resources = p[:rate_limit][:resources]
+      {
+        id: p[:id],
+        verify_credentials: resources[:account][:'/account/verify_credentials'][:remaining],
+        friend_ids: resources[:friends][:'/friends/ids'][:remaining],
+        follower_ids: resources[:followers][:'/followers/ids'][:remaining]
+      }
+    end
+  end
+
+  def verify_credentials
+    api_client.verify_credentials rescue nil
+  end
+
+  def rate_limit
+    api_client.send(:perform_get, '/1.1/application/rate_limit_status.json') rescue nil
+  end
+
+  def api_client
+    ApiClient.instance(ApiClient.config(access_token: token, access_token_secret: secret))
   end
 end
