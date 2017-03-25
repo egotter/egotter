99a7d1c493afc4ab8dec80f443039000c016c890
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-25 15:52:38 +0900

    send sidekiq metrics to cloudwatch

--- app/workers/send_stats_worker.rb
+++ app/workers/send_stats_worker.rb
@@ -5,23 +5,51 @@ class SendStatsWorker
   sidekiq_options queue: self, retry: 0, backtrace: false
 
   def perform
-    statsd = Datadog::Statsd.new('localhost', 8125)
-
-    [
+    values = [
       CreateTwitterUserWorker,
       CreateSignedInTwitterUserWorker,
       DelayedCreateTwitterUserWorker,
       ImportTwitterUserRelationsWorker,
       DelayedImportTwitterUserRelationsWorker
-    ].each do |klass|
+    ].map do |klass|
       name = klass.name
       queue = Sidekiq::Queue.new(name)
-      statsd.gauge("sidekiq.queues.#{name}.size", queue.size)
-      statsd.gauge("sidekiq.queues.#{name}.latency", queue.latency)
+      [name, queue.size, queue.latency]
     end
+
+    datadog(values)
+    cloudwatch(values)
   rescue => e
     logger.warn "#{e.class} #{e.name}"
   ensure
     SendStatsWorker.perform_in(1.minute.since)
+  end
+
+  def datadog(values)
+    statsd = Datadog::Statsd.new('localhost', 8125)
+    values.each do |name, size, latency|
+      statsd.gauge("sidekiq.queues.#{name}.size", size)
+      statsd.gauge("sidekiq.queues.#{name}.latency", latency)
+    end
+  end
+
+  def cloudwatch(values)
+    region = %x(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//')
+    instance_id=%x(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+    values.each do |name, size, latency|
+      options = {
+        namespace: 'egotter',
+        dimensions: "name=#{name}",
+        unit: 'Count',
+        region: region,
+      }
+
+      size_options = {'metric-name' => 'QueueSize', value: size}.merge(options).map { |n, v| "--#{n} #{v}" }.join ' '
+      latency_options = {'metric-name' => 'QueueLatency', value: latency}.merge(options).map { |n, v| "--#{n} #{v}" }.join ' '
+
+      %x(aws cloudwatch put-metric-data #{size_options})
+      %x(aws cloudwatch put-metric-data #{latency_options})
+    end
   end
 end
