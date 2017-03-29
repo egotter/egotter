db04422b410e9a6cefaabc819db0fca0bc0b434c
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-30 02:40:38 +0900

    show reload buttons on search_results/show

--- app/helpers/adsense_helper.rb
+++ app/helpers/adsense_helper.rb
@@ -13,11 +13,13 @@ module AdsenseHelper
         when [true,  'show',                   :bottom] then '4452673613'
         when [true,  'show',                   :slit]   then '9958552019'
         when [true,  'search_histories/index', :slit]   then '5378418414'
+        when [true,  'waiting',                 :top]   then '2340125210'
         when [false, 'new',                    :top]    then '4742208415'
         when [false, 'show',                   :top]    then '6218941612'
         when [false, 'show',                   :bottom] then '7695674812'
         when [false, 'show',                   :slit]   then '5528352415'
         when [false, 'search_histories/index', :slit]   then '3901685219'
+        when [false, 'waiting',                :top]    then '9863392014'
         else nil
       end
 
