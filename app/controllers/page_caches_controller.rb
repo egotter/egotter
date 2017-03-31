e1e854c9396a84e6f1df7a02163c7eff84174961
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-03-31 20:04:17 +0900

    skip verify_authenticity_token if the DELETE request is from same domain

--- app/controllers/page_caches_controller.rb
+++ app/controllers/page_caches_controller.rb
@@ -7,6 +7,7 @@ class PageCachesController < ApplicationController
   layout false
 
   skip_before_action :verify_authenticity_token, if: -> { action_name == 'create' && params[:token] == ENV['PAGE_CACHE_CREATE_TOKEN'] }
+  skip_before_action :verify_authenticity_token, if: -> { action_name == 'destroy' && request.referer.starts_with?('https://egotter.com') }
 
   before_action(only: %i(create destroy)) { valid_uid?(params[:uid].to_i) }
   before_action(only: %i(create destroy)) { existing_uid?(params[:uid].to_i) }
