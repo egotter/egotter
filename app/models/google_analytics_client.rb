2a3ab37f80ac5ca4f882b8396615514daf04a25b
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-04-05 23:15:50 +0900

    implement GoogleAnalyticsClient#realtime_data

--- app/models/google_analytics_client.rb
+++ app/models/google_analytics_client.rb
@@ -2,13 +2,39 @@ require 'google/apis/analytics_v3'
 require 'googleauth/stores/file_token_store'
 
 class GoogleAnalyticsClient
+
+  PROFILE_ID = ENV['GOOGLE_ANALYTICS_PROFILE_ID']
+
   def initialize
     @analytics = Google::Apis::AnalyticsV3::AnalyticsService.new
     @analytics.authorization = user_credentials_for(Google::Apis::AnalyticsV3::AUTH_ANALYTICS)
   end
 
   def active_users
-    @analytics.get_realtime_data("ga:#{ENV['GOOGLE_ANALYTICS_PROFILE_ID']}", 'rt:activeUsers').totals_for_all_results['rt:activeUsers']
+    @analytics.get_realtime_data("ga:#{PROFILE_ID}", 'rt:activeUsers').totals_for_all_results['rt:activeUsers']
+  end
+
+  # puts result.column_headers.map { |h| h.name }.inspect
+  # => ["rt:deviceCategory", "rt:medium", "rt:source", "rt:userType", "rt:pagePath", "rt:activeUsers"]
+  #
+  # result.rows.each { |row| puts row.inspect }
+  # => ["DESKTOP", "(none)", "(direct)", "NEW", "/searches/aaa?locale=ja", "100"]
+  #
+  # rt:deviceCategory -> DESKTOP, MOBILE, TABLET
+  # rt:medium         -> (none), ORGANIC, REFERRAL, SOCIAL
+  # rt:source         -> (direct), google, yahoo, Naver, Twitter, domain.com
+  # rt:userType       -> NEW, RETURNING
+  # rt:pagePath       -> /aaa/bbb?c=ddd
+  #
+  # https://developers.google.com/analytics/devguides/reporting/realtime/dimsmets/user?hl=ja
+  #
+  def realtime_data(metrics: %w(rt:activeUsers), dimensions: %w(rt:deviceCategory rt:medium rt:source rt:userType rt:pagePath))
+    @analytics.get_realtime_data(
+      "ga:#{PROFILE_ID}",
+      metrics.join(','),
+      dimensions: dimensions.join(','),
+      sort: dimensions.join(',')
+    )
   end
 
   private
