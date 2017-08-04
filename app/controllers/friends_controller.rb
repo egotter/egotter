aff45096ff928645a19d4706d047908b6f228353
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-08-04 19:54:32 +0900

    Bug fix. Create friends#new

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,7 @@ class FriendsController < ApplicationController
   include Concerns::Logging
   include SearchesHelper
 
-  before_action do
+  before_action(only: %i(show)) do
     if request.format.html?
       if valid_screen_name?(params[:screen_name])
         case params[:type]
@@ -17,18 +17,21 @@ class FriendsController < ApplicationController
     end
   end
 
-  before_action { valid_screen_name?(params[:screen_name]) }
-  before_action { not_found_screen_name?(params[:screen_name]) }
-  before_action { @tu = build_twitter_user(params[:screen_name]) }
-  before_action { authorized_search?(@tu) }
-  before_action { existing_uid?(@tu.uid.to_i) }
-  before_action  do
+  before_action(only: %i(show)) { valid_screen_name?(params[:screen_name]) }
+  before_action(only: %i(show)) { not_found_screen_name?(params[:screen_name]) }
+  before_action(only: %i(show)) { @tu = build_twitter_user(params[:screen_name]) }
+  before_action(only: %i(show)) { authorized_search?(@tu) }
+  before_action(only: %i(show)) { existing_uid?(@tu.uid.to_i) }
+  before_action(only: %i(show))  do
     @twitter_user = TwitterUser.latest(@tu.uid.to_i)
     remove_instance_variable(:@tu)
   end
   before_action do
     push_referer
     create_search_log
+  end
+
+  def new
   end
 
   def show
