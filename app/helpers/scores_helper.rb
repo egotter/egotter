2b21529120df0942ee9465a251435e58e224ddcb
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-08-07 00:05:30 +0900

    Fetch and create Score if not crawler

--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -3,11 +3,15 @@ module ScoresHelper
     score = Score.find_by(uid: uid)
 
     unless score
-      begin
-        score = Score.builder(uid).build
-        score.save! if score.valid? # It currently validates only klout_id.
-      rescue => e
-        logger.warn "Score of #{uid} is invalid. #{e.class} #{e.message.truncate(100)}"
+      if request.from_crawler?
+        score = Score.new(uid: uid)
+      else
+        begin
+          score = Score.builder(uid).build
+          score.save! if score.valid? # It currently validates only klout_id.
+        rescue => e
+          logger.warn "Score of #{uid} is invalid. #{e.class} #{e.message.truncate(100)}"
+        end
       end
     end
 
