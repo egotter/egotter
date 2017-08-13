2668995798a4496caf0b3f2d6fabafec5d9d5be9
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-08-13 22:32:41 +0900

    Do not sleep in DelayedCreateTwitterUserWorker#perform

--- app/workers/delayed_create_twitter_user_worker.rb
+++ app/workers/delayed_create_twitter_user_worker.rb
@@ -7,14 +7,14 @@ class DelayedCreateTwitterUserWorker < CreateTwitterUserWorker
   end
 
   def perform(values = {})
-    slept = false
-    while (queue = Sidekiq::Queue.new('CreateTwitterUserWorker')).size > BUSY_QUEUE_SIZE
-      logger.info "I will sleep. Bye! size: #{queue.size}"
-      slept = true
-      sleep 3.minutes
-    end
-
-    logger.info 'Good morning. I will retry.' if slept
+    # slept = false
+    # while (queue = Sidekiq::Queue.new('CreateTwitterUserWorker')).size > BUSY_QUEUE_SIZE
+    #   logger.info "I will sleep. Bye! size: #{queue.size}"
+    #   slept = true
+    #   sleep 3.minutes
+    # end
+    #
+    # logger.info 'Good morning. I will retry.' if slept
 
     super
   end
@@ -23,11 +23,11 @@ class DelayedCreateTwitterUserWorker < CreateTwitterUserWorker
 
   def handle_retryable_exception(values, ex)
     if ex.class == Twitter::Error::TooManyRequests
-      sleep_time = ex&.rate_limit&.reset_in.to_i + 1
-      logger.warn "recover #{ex.message} Reset in #{sleep_time} seconds #{values['user_id']} #{values['uid']}"
+      sleep_seconds = ex&.rate_limit&.reset_in.to_i + 1
+      logger.warn "recover #{ex.message} Reset in #{sleep_seconds} seconds #{values['user_id']} #{values['uid']}"
       logger.info ex.backtrace.grep_v(/\.bundle/).join "\n"
-      logger.warn 'I will sleep. Bye!'
-      sleep sleep_time
+      logger.warn "I will sleep for #{sleep_seconds} seconds. Bye!"
+      sleep sleep_seconds
       logger.warn 'Good morning. I will retry.'
     else
       logger.warn "recover #{ex.class.name.demodulize} #{values['user_id']} #{values['uid']}"
