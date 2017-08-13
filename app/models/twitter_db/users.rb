438d4bb4ca8083be5ca849bcc61eea9a43c2e26c
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-08-13 13:46:05 +0900

    Create TwitterDB::Users::Batch class

--- app/models/twitter_db/users.rb
+++ app/models/twitter_db/users.rb
@@ -4,37 +4,43 @@ module TwitterDB
     # `followers` does not include suspended uids.
     # `followers_count` is ambiguous.
     def self.fetch_and_import(uids, client:)
-      uids = uids.uniq.map(&:to_i)
-      users = client.users(uids)
-      imported = import(users)
-      import_suspended(uids - users.map(&:id)) if uids.size != imported.size
+      Batch.fetch_and_import(uids, client: client)
     end
 
-    def self.import(t_users)
-      users = t_users.map { |user| TwitterDB::User.to_import_format(user) }
-      users.sort_by!(&:first)
-      TwitterDB::User.import_in_batches(users)
-      users
-    end
-
-    def self.import_suspended(uids)
-      filtered = uids.uniq.map(&:to_i) - TwitterDB::User.where(uid: uids).pluck(:uid)
-      return [] if filtered.empty?
-
-      if filtered.size >= 10
-        logger "#{self}##{__method__}: Too many suspended uids #{filtered.size} #{filtered.inspect.truncate(100)}"
+    class Batch
+      def self.fetch_and_import(uids, client:)
+        uids = uids.uniq.map(&:to_i)
+        users = client.users(uids)
+        imported = import(users)
+        import_suspended(uids - users.map(&:id)) if uids.size != imported.size
       end
 
-      t_users =  filtered.map { |uid| Hashie::Mash.new(id: uid, screen_name: 'suspended', description: '') }
-      logger "#{self}##{__method__}: Import suspended uids #{filtered.inspect}" if filtered.any?
-      import(t_users)
-      filtered
-    end
+      def self.import(t_users)
+        users = t_users.map { |user| TwitterDB::User.to_import_format(user) }
+        users.sort_by!(&:first)
+        TwitterDB::User.import_in_batches(users)
+        users
+      end
 
-    private
+      def self.import_suspended(uids)
+        filtered = uids.uniq.map(&:to_i) - TwitterDB::User.where(uid: uids).pluck(:uid)
+        return [] if filtered.empty?
 
-    def self.logger(message)
-      File.basename($0) == 'rake' ? puts(message) : Rails.logger.warn(message)
+        if filtered.size >= 10
+          logger "#{self}##{__method__}: Too many suspended uids #{filtered.size} #{filtered.inspect.truncate(100)}"
+        end
+
+        t_users =  filtered.map { |uid| Hashie::Mash.new(id: uid, screen_name: 'suspended', description: '') }
+        # logger "#{self}##{__method__}: Import suspended uids #{filtered.inspect.truncate(100)}" if filtered.any?
+        import(t_users)
+        filtered
+      end
+
+      private
+
+      def self.logger(message)
+        File.basename($0) == 'rake' ? puts(message) : Rails.logger.warn(message)
+      end
     end
   end
 end
