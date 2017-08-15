2590cd77f3d7c317e332badc2b1293e11fc91cfd
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-08-15 22:44:06 +0900

    Refactoring of ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,80 +16,72 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from ActionController::InvalidAuthenticityToken do |e|
-    if request.referer.present? && !request.referer.match(%r{^https://egotter.com})
-      screen_name = params[:screen_name].strip.remove /^@/
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
 
-      if screen_name&.match(Validations::ScreenNameValidator::REGEXP) && controller_name == 'searches' && action_name == 'create'
-        logger.warn "cross domain post #{request.device_type} #{current_user_id} #{request.fullpath} #{request.referer} #{params['screen_name']}"
-        _search_path = search_path(screen_name: screen_name)
-        _sign_in_path = sign_in_path via: "#{controller_name}/#{action_name}/cross_domain_post_and_recover", redirect_path: _search_path
-        redirect_to root_path, alert: t('before_sign_in.cross_domain_post_and_recover_html', user: screen_name, search_path: _search_path, sign_in_path: _sign_in_path)
-      else
-        logger.warn "cross domain post #{request.device_type} #{current_user_id} #{request.fullpath} #{request.referer} #{params.inspect}"
-        _sign_in_path = sign_in_path via: "#{controller_name}/#{action_name}/cross_domain_post"
-        redirect_to root_path, alert: t('before_sign_in.cross_domain_post_html', sign_in_path: _sign_in_path)
-      end
-    else
-      recover = recover_invalid_token?
-      logger.warn "CSRF token error (#{recover}) #{session[:fingerprint]&.truncate(15)} #{request.method} #{!!request.xhr?} #{request.device_type} #{current_user_id} #{request.fullpath} #{request.user_agent} #{params.inspect}"
+  def invalid_token
+    logger.info "Invalid token: #{debug_str}"
 
-      if recover
-        _sign_in_path = sign_in_path via: "#{controller_name}/#{action_name}/invalid_token_and_recover", redirect_path: search_path(screen_name: params[:screen_name])
-        redirect_to root_path, alert: t('before_sign_in.session_expired_and_recover_html', user: params[:screen_name], sign_in_path: _sign_in_path)
-      else
-        if request.xhr?
-          head :internal_server_error
-        else
-          redirect_to root_path, alert: t('before_sign_in.session_expired_html', sign_in_path: welcome_path(via: "#{controller_name}/#{action_name}/invalid_token"))
-        end
-      end
+    if request.xhr?
+      return head(:bad_request)
     end
+
+    screen_name = params[:screen_name].to_s.strip.remove /^@/
+    if screen_name.blank? || !screen_name.match(Validations::ScreenNameValidator::REGEXP)
+      return redirect_to root_path, alert: t('application.invalid_token.session_expired_html', url: welcome_path(via: "#{controller_name}/#{action_name}/invalid_token"))
+    end
+
+    search = search_path(screen_name: screen_name)
+    sign_in = sign_in_path(via: "#{controller_name}/#{action_name}/invalid_token_and_recover", redirect_path: search)
+
+    if recoverable_request?
+      logger.warn "Recoverable CSRF token error #{session[:fingerprint]&.truncate(15)} #{debug_str}"
+      return redirect_to root_path, alert: t('application.invalid_token.ready_to_search_html', user: screen_name, url1: search, url2: sign_in)
+    end
+
+    redirect_to root_path, alert: t('application.invalid_token.session_expired_and_recover_html', user: screen_name, url1: search, url2: sign_in)
   end
 
   def not_found
-    if request.from_crawler? || from_minor_crawler?(request.user_agent)
-      create_crawler_log
+    logger.info "Not found: #{debug_str}"
+
+    if request.from_crawler? || from_minor_crawler?(request.user_agent) || request.method != 'GET' || request.xhr?
       return head :not_found
     end
 
-    if params['screen_name']&.match(Validations::ScreenNameValidator::REGEXP) && request.path == '/searches' && !request.xhr?
+    if params['screen_name'].to_s.match(Validations::ScreenNameValidator::REGEXP) && request.path == '/searches'
       @screen_name = params['screen_name']
       @redirect_path = search_path(screen_name: @screen_name)
       @via = params['via']
       return render template: 'searches/create', layout: false
     end
 
-    if request.fullpath.match(%r{^/https:/egotter\.com(.+)}) && !request.xhr?
-      redirect_url = "https://egotter.com#{$1}"
-      logger.info "redirect to #{redirect_url} #{current_user_id} #{request.device_type} #{request.browser} #{request.referer}"
-      return redirect_to redirect_url, status: 301
-    end
-
-    if params['from']&.match(Validations::ScreenNameValidator::REGEXP) && request.path == '/profile' && !request.xhr?
+    if params['from'].to_s.match(Validations::ScreenNameValidator::REGEXP) && request.path == '/profile'
       @screen_name = params['from']
       @redirect_path = search_path(screen_name: @screen_name)
       @via = params['via']
       return render template: 'searches/create', layout: false
     end
 
-    unless request.fullpath.match %r{^/search_results/}
-      logger.warn "#{request.method} #{request.fullpath} #{current_user_id} #{request.device_type} #{request.browser}"
-    end
-
-    if request.xhr?
-      return head(:not_found)
+    if request.fullpath.match(%r{^/https:/egotter\.com(.+)})
+      redirect = "https://egotter.com#{$1}"
+      logger.info "Redirect to: #{redirect}"
+      return redirect_to redirect, status: 301
     end
 
     flash.now[:alert] = t('before_sign_in.that_page_doesnt_exist')
     render template: 'searches/new', status: 404
   end
 
-  def recover_invalid_token?
+  def recoverable_request?
     %i(pc smartphone).include?(request.device_type) && !request.xhr? &&
-      !!params['screen_name']&.match(Validations::ScreenNameValidator::REGEXP) &&
-      controller_name == 'searches' && action_name == 'create' && session[:fingerprint].present? &&
-      SearchLog.exists?(created_at: 3.hours.ago..Time.zone.now, session_id: session[:fingerprint])
+      params['screen_name'].to_s.match(Validations::ScreenNameValidator::REGEXP) &&
+      controller_name == 'searches' && action_name == 'create'
+      # session[:fingerprint].present? &&
+      # SearchLog.exists?(created_at: 3.hours.ago..Time.zone.now, session_id: session[:fingerprint])
+  end
+
+  def debug_str
+    "#{request.method} #{current_user_id} #{request.device_type} #{request.browser} #{request.xhr?} #{request.referer} #{params.inspect}"
   end
 
   # https://github.com/plataformatec/devise/issues/1390
