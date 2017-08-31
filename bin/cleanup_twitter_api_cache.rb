df39be3b23e970df761db2b2b2f2489f6c3a3e03
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-09-01 02:22:35 +0900

    Update bin/cleanup_twitter_api_cache.rb

--- bin/cleanup_twitter_api_cache.rb
+++ bin/cleanup_twitter_api_cache.rb
@@ -1,11 +1,11 @@
 require 'twitter_with_auto_pagination'
 
 start = Time.now
-before = %x(du -sh tmp/api_cache | awk '{ print $1 }').chomp
+before = %x(du -sh tmp/twitter_cache | awk '{ print $1 }').chomp
 
 TwitterWithAutoPagination::Client.new.cache.cleanup
 
-after = %x(du -sh tmp/api_cache | awk '{ print $1 }').chomp
+after = %x(du -sh tmp/twitter_cache | awk '{ print $1 }').chomp
 elapsed = (Time.now - start).round(3)
 
 puts "#{Time.now.utc} #{$0} #{before} -> #{after} (#{elapsed}sec)"
