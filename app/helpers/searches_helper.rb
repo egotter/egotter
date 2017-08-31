22cc8a22ec0b82ecf692dbc5d3f27920155abc68
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-09-01 01:23:15 +0900

    Bug fix. Specify application in #search_path_for

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -39,9 +39,12 @@ module SearchesHelper
 
   def search_path_for(menu, screen_name)
     case menu.to_s
-      when *%w(searches notifications login) then timeline_path(screen_name: screen_name)
+      when *%w(searches notifications login application) then timeline_path(screen_name: screen_name)
       else send("#{menu.to_s.singularize}_path", screen_name: screen_name)
     end
+  rescue => e
+    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message}"
+    timeline_path(screen_name: screen_name)
   end
 
   def searches_path_for(screen_name:, via:)
