1a67b60f020d47b3eaceff44085421aef522d05d
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-09-02 01:31:03 +0900

    Bug fix of FollowEgotterWorker

--- app/workers/follow_egotter_worker.rb
+++ app/workers/follow_egotter_worker.rb
@@ -3,28 +3,29 @@ class FollowEgotterWorker
   include Concerns::WorkerUtils
   sidekiq_options queue: self, retry: 0, backtrace: false
 
-  def perform(user_id)
-    if user_id
-      follow(User.find(user_id))
-    else
-      request = FollowRequest.find_by(id: FollowRequest.pluck(:id).sample)
-      if request
-        follow(request.user)
-        request.destroy
-      end
+  def perform(*args)
+    raised = false
+    request = FollowRequest.find_by(id: FollowRequest.pluck(:id).sample)
+    if request
+      follow(request.user)
+      request.destroy
     end
   rescue Twitter::Error::Unauthorized => e
-    handle_unauthorized_exception(e, user_id: user_id)
+    handle_unauthorized_exception(e, user_id: request.user.id)
+    request.destroy
   rescue Twitter::Error::Forbidden => e
-    handle_forbidden_exception(e, user_id: user_id)
+    handle_forbidden_exception(e, user_id: request.user.id)
 
-    if e.message == FORBIDDEN_MESSAGES[2] # You are unable to follow more people ...
-      FollowRequest.create(user_id: user_id)
+    if e.message == FORBIDDEN_MESSAGES[2]
+      logger.warn "#{e.class} #{e.message} #{request.inspect}"
     end
+    raised = true
   rescue => e
-    logger.warn "#{e.class} #{e.message} #{user_id}"
+    logger.warn "#{e.class} #{e.message} #{request.inspect}"
+    raised = true
   ensure
-    FollowEgotterWorker.perform_in(1.minute.since, nil) unless user_id
+    interval = raised ? 30.minutes.since : 1.minutes.since
+    FollowEgotterWorker.perform_in(interval)
   end
 
   private
