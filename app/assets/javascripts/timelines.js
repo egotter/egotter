65a902ea0dc4bb95dcac21037b99699754fad237
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-09-01 21:46:52 +0900

    Refactoring of #loadSummary and do not show the protection result if the user is not signed in

--- app/assets/javascripts/timelines.js
+++ app/assets/javascripts/timelines.js
@@ -1,46 +1,32 @@
-function loadSummary (apiEndpoint, uid, menuName, progressMessages) {
-  var $summaryBox = $('.menu-items.' + menuName);
-  var $progressMsgBox = $summaryBox.find('.progress-msg');
+'use strict';
 
-  var $resultLinkButtons = {
-    reload: $summaryBox.find('.reload-btn'),
-    more: $summaryBox.find('.btn-view-more')
-  };
-
-  $.getJSON(apiEndpoint, {uid: uid}, function(data) {
+function loadSummary (apiEndpoint, summaryBox, progressMessages) {
+  $.getJSON(apiEndpoint, function(data) {
     console.log(data);
 
     if (!data || !data.users || data.users.length <= 0) {
-      $summaryBox.find('.result-link').attr('href', '#').attr('onclick', 'return false;');
-      $progressMsgBox.html(progressMessages.empty).show();
-      $resultLinkButtons.reload.show();
-      $("div[data-replaced-by='" + menuName + "']").remove();
-      $summaryBox.show();
+      summaryBox.link(false);
+      summaryBox.messageBox().html(progressMessages.empty).show();
+      summaryBox.reloadBtn().show();
+      summaryBox.show();
       return;
     }
 
-    var $userTemplate = $summaryBox.find('.user-template');
+    var template = $('#user-template').html();
+
     $.each(data.users, function () {
       var user = this;
-      var $cloned = $userTemplate.clone();
-      $cloned
-        .attr('href', $cloned.attr('href').replace('SCREEN_NAME', user.screen_name))
-        .removeClass('user-template')
-        .find('img')
-        .attr({src: user.profile_image_url_https, alt: user.screen_name})
-        .end()
-        .css('display', 'inline-block')
-        .insertBefore($userTemplate);
+      user.menu_name = summaryBox.menuName;
+      var rendered = Mustache.render(template, user);
+      summaryBox.appendUser(rendered);
     });
-    $resultLinkButtons.more.find('span').text(data.count).end().show();
+    summaryBox.viewMoreBtn(data.count).show();
 
     var graphOption = $.extend(true, {}, window.common_pie_chart_options);
     graphOption.series[0].data = data.chart;
 
-    $("div[data-replaced-by='" + menuName + "']").remove();
-    $summaryBox.show();
-
-    $summaryBox.find('.media-right').show().end().find('.common-chart').highcharts(graphOption);
+    summaryBox.show();
+    summaryBox.graph(graphOption);
   });
 }
 
@@ -70,3 +56,81 @@ function checkForUpdates (path, options, successCallback, stopCallback, failedCa
       failedCallback(xhr);
     });
 }
+
+window.SummaryBox = function (menuName) {
+  this.menuName = menuName;
+  this.$summaryBox = $('.menu-items.' + menuName);
+};
+
+SummaryBox.prototype.show = function () {
+  $("div[data-replaced-by='" + this.menuName + "']").remove();
+  this.$summaryBox.show();
+};
+
+SummaryBox.prototype.messageBox = function () {
+  return this.$summaryBox.find('.progress-msg');
+};
+
+SummaryBox.prototype.reloadBtn = function () {
+  return this.$summaryBox.find('.reload-btn');
+};
+
+SummaryBox.prototype.viewMoreBtn = function (count) {
+  return this.$summaryBox.find('.btn-view-more').find('span').text(count).end();
+};
+
+SummaryBox.prototype.signInBtn = function () {
+  return this.$summaryBox.find('.sign-in-btn');
+};
+
+SummaryBox.prototype.link = function (enable) {
+  if (!enable) {
+    this.$summaryBox.find('.result-link').attr('href', '#').attr('onclick', 'return false;');
+  }
+};
+
+SummaryBox.prototype.graph = function (options) {
+  return this.$summaryBox.find('.media-right').show().end().find('.common-chart').highcharts(options);
+};
+
+SummaryBox.prototype.appendUser = function (user) {
+  this.$summaryBox.find('.media-body').append(user);
+};
+
+SummaryBox.prototype.lazyload = function (callback) {
+  var menuName = this.menuName;
+  $('.placeholder-wrapper.' + this.menuName)
+    .lazyload()
+    .one('appear', function () {
+      console.log('appear', menuName);
+      callback();
+    });
+};
+
+window.Adsense = function (url, selector) {
+  this.url = url;
+  this.selector = selector;
+};
+
+Adsense.prototype.lazyload = function () {
+  var url = this.url;
+  var selector = this.selector;
+  var $wrapper = $(this.selector);
+  $wrapper
+    .lazyload()
+    .one('appear', function () {
+      console.log('appear', selector);
+      $.getJSON(url, function (data) { $wrapper.html(data.html); });
+    });
+};
+
+window.PublicTweets = function (url, selector) {
+  this.url = url;
+  this.selector = selector;
+};
+
+PublicTweets.prototype.load = function () {
+  var url = this.url;
+  var $wrapper = $(this.selector);
+  $.getJSON(url, function (data) { $wrapper.html(data.html); });
+};
