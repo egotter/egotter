5a2550bb6b17270cd58c7b78b4f2a1cfc4649216
Shinohara Teruki <ts_3156@yahoo.co.jp>
2017-09-05 01:29:38 +0900

    Bug fix. Include TwitterHelper in ScoresController

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,7 @@
 class ScoresController < ApplicationController
   include Concerns::Showable
   include ScoresHelper
+  include TwitterHelper
 
   def show
     @breadcrumb_name = controller_name.singularize.to_sym
