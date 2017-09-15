6607ec6a4008b5f037a649f634f6915dbf3ffaf8
ts_3156 <ts_3156@yahoo.co.jp>
2017-09-15 13:26:51 +0900

    Bug fix. Implement #mention_name in ScoresController

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -15,11 +15,21 @@ class ScoresController < ApplicationController
     @meta_description = t('.meta_description', user: @twitter_user.mention_name, score: (@score * 10000).round.to_s(:delimited))
     @page_description = t('.page_description', user: @twitter_user.mention_name)
 
-    tweet_text = t('.score_text_html', user: @twitter_user.mention_name, score: (@score * 10000).round.to_s(:delimited))
-    tweet_text += t('.will_win', users: will_win.map { |name| mention_name(name) }.join(' ')) if will_win.any?
-    tweet_text += t('.will_loose', users: will_loose.map { |name| mention_name(name) }.join(' ')) if will_loose.any?
-    @tweet_text =  tweet_text + "\n#egotter #{@canonical_url}"
+    @tweet_text =  scores_text(@twitter_user, will_win, will_loose, @score, @canonical_url)
 
     @screen_names = (will_win + will_loose).uniq
+  end
+
+  private
+
+  def scores_text(twitter_user, will_win, will_loose, score, canonical_url)
+    tweet_text = t('.score_text_html', user: twitter_user.mention_name, score: (score * 10000).round.to_s(:delimited))
+    tweet_text += t('.will_win', users: will_win.map { |name| mention_name(name) }.join(' ')) if will_win.any?
+    tweet_text += t('.will_loose', users: will_loose.map { |name| mention_name(name) }.join(' ')) if will_loose.any?
+    tweet_text + "\n#egotter #{canonical_url}"
+  end
+
+  def mention_name(screen_name)
+    view_context.mention_name(screen_name)
   end
 end
