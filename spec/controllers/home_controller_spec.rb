79671039282564763f2ffa2fc2be74b2c73bf3bc
ts-3156 <ts_3156@yahoo.co.jp>
2019-01-17 13:10:20 +0900

    Send SearchReport

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe SearchesController, type: :controller do
+RSpec.describe HomeController, type: :controller do
 
   describe 'GET #new' do
     it 'calls create_search_log' do
