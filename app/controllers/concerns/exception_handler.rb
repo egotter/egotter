62334cc6748f48b3d7414b3d7b94351709221c69
ts-3156 <ts_3156@yahoo.co.jp>
2019-01-20 21:44:51 +0900

    Remove debug print from twitter_exception_handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -9,8 +9,6 @@ module Concerns::ExceptionHandler
   end
 
   def twitter_exception_handler(ex, screen_name)
-    logger.warn "#{caller[0][/`([^']*)'/, 1] rescue ''}: #{ex.class} #{ex.message} #{current_user_id} #{screen_name} #{request.device_type} #{request.browser} #{params.inspect}"
-
     return head :bad_request if request.xhr?
 
     message =
