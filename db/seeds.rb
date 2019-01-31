53a130eb47025d922db1aed7a9a275d3a3cc0e6b
ts-3156 <ts_3156@yahoo.co.jp>
2019-01-31 14:33:11 +0900

    Remove reset_for_debug.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,7 @@ bots =
 
 Bot.transaction {Bot.import! bots}
 
-# user_info = {}.to_json
-#
-# ActiveRecord::Base.transaction do
-#   (1..10).map do |uid|
-#     TwitterUser.new(uid: uid, screen_name: "sn#{uid}", user_info: user_info, user_id: -1).tap { |u| u.save!(validate: false) }
-#   end.each do |tu|
-#     (1..10).each { |uid| tu.friends.create!(uid: uid, screen_name: "sn#{uid}", user_info: user_info) }
-#     (1..10).each { |uid| tu.followers.create!(uid: uid, screen_name: "sn#{uid}", user_info: user_info) }
-#     tu.update!(friends_size: tu.friends.size, followers_size: tu.followers.size)
-#   end
-# end
+dir = ENV['TWITTER_CACHE_DIR']
+ApiClient.instance(cache_dir: dir).cache.clear
+
+Redis.client.flushdb
