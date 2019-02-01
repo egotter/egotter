bbfbf73e5012ab74c536f84d6707dc52a3bf8642
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-01 18:19:18 +0900

    Format indentation in persistence.rb

--- app/models/concerns/twitter_user/persistence.rb
+++ app/models/concerns/twitter_user/persistence.rb
@@ -14,17 +14,17 @@ module Concerns::TwitterUser::Persistence
   private
 
   def put_relations_back
-    statuses.each { |r| r.from_id = id }
-    mentions.each { |r| r.from_id = id }
+    statuses.each       { |r| r.from_id = id }
+    mentions.each       { |r| r.from_id = id }
     search_results.each { |r| r.from_id = id }
-    favorites.each { |r| r.from_id = id }
+    favorites.each      { |r| r.from_id = id }
 
     options = {validate: false}
 
-    silent_transaction { statuses.each_slice(1000) { |ary| Status.import(ary, options) } }
-    silent_transaction { mentions.each_slice(1000) { |ary| Mention.import(ary, options) } }
+    silent_transaction { statuses.each_slice(1000)       { |ary| Status.import(ary, options) } }
+    silent_transaction { mentions.each_slice(1000)       { |ary| Mention.import(ary, options) } }
     silent_transaction { search_results.each_slice(1000) { |ary| SearchResult.import(ary, options) } }
-    silent_transaction { favorites.each_slice(1000) { |ary| Favorite.import(ary, options) } }
+    silent_transaction { favorites.each_slice(1000)      { |ary| Favorite.import(ary, options) } }
 
     # Set friends_size and followers_size in AssociationBuilder#build_friends_and_followers
 
@@ -35,7 +35,7 @@ module Concerns::TwitterUser::Persistence
   rescue => e
     # ActiveRecord::RecordNotFound Couldn't find TwitterUser with 'id'=00000
     # ActiveRecord::StatementInvalid Mysql2::Error: Deadlock found when trying to get lock;
-    logger.warn "#{self.class}##{__method__}: #{e.class} #{e.message.truncate(120)} #{id} #{uid} #{screen_name}"
+    logger.warn "#{__method__}: #{e.class} #{e.message.truncate(120)} #{self.inspect}"
     logger.info e.backtrace.join("\n")
     destroy
   end
