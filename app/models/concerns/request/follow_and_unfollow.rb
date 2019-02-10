401cdd96fa07b62a82c55309841003a2002ec724
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-11 00:01:58 +0900

    Include follow limit exceeded error to without_error

--- app/models/concerns/request/follow_and_unfollow.rb
+++ app/models/concerns/request/follow_and_unfollow.rb
@@ -4,13 +4,22 @@ module Concerns::Request::FollowAndUnfollow
   extend ActiveSupport::Concern
 
   included do
-    scope :without_error, -> {where("error_message is null or error_message = '' or error_message like 'You are unable to follow more people at this time.%'")}
+    scope :without_error, -> do
+      sql = <<~SQL
+        error_message is null
+        or error_message = ''
+        or error_message like 'You are unable to follow more people at this time.%'
+        or error_message like 'Follow or unfollow limit exceeded%'
+      SQL
+      where(sql)
+    end
 
     scope :with_limit_error, -> {where("error_message like 'You are unable to follow more people at this time.%'")}
 
     scope :unprocessed, -> user_id {
       where(user_id: user_id, finished_at: nil).
           where.not(uid: User::EGOTTER_UID).
+          where(created_at: 1.day.ago..Time.zone.now).
           without_error
     }
 
