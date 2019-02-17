e734d1c5428558555f644ebd4ada6be4d1c2c165
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-18 08:23:10 +0900

    Fix typo

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,7 @@
 module WelcomeHelper
   def sign_in_with_twitter_link(redirect_path, via, html_options = {}, &block)
     path_options = {via: build_via(via)}
-    path_options[redirect_path] = redirect_path if redirect_path
+    path_options[:redirect_path] = redirect_path if redirect_path
     link_to t('welcome.new.sign_in_with_twitter_html'), sign_in_path(path_options), html_options, &block
   end
 end
