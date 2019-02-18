78bd66a265f82759e45fec04866a693a7cdde496
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-18 11:34:16 +0900

    Refactoring of PublicTweetsController

--- app/controllers/public_tweets_controller.rb
+++ app/controllers/public_tweets_controller.rb
@@ -1,6 +1,20 @@
 class PublicTweetsController < ApplicationController
   def load
-    html = render_to_string partial: 'common/public_tweets'
+    html = render_to_string partial: 'twitter/tweet', collection: tweets_for('#egotter'), cached: true
     render json: {html: html}
+  end
+
+  private
+
+  def tweets_for(keyword)
+    if Util::TweetsCache.exists?(keyword)
+      JSON.parse(Util::TweetsCache.get(keyword)).map { |tweet| Hashie::Mash.new(tweet) }
+    else
+      CreateTweetsWorker.perform_async(keyword)
+      []
+    end
+  rescue => e
+    logger.warn "#{__method__}: #{e.class} #{e.message} #{keyword}"
+    []
   end
 end
