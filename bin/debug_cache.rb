ad80a881ab5ee25e34f4a38f7d17df3b121cdaef
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-27 00:53:44 +0900

    Add debug_cache.rb

---
+++ bin/debug_cache.rb
@@ -0,0 +1,18 @@
+require 'dotenv/load'
+require 'active_support'
+
+dir = ARGV[0]
+
+cache = ActiveSupport::Cache::FileStore.new(dir, expires_in: 1.second)
+key = 'a'
+
+puts %x(ls #{dir})
+cache.write(key, 1)
+puts cache.exist?(key)
+puts cache.read(key)
+puts %x(ls #{dir})
+
+sleep 2
+
+puts cache.read(key)
+puts %x(ls #{dir})
