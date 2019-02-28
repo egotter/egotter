cb73522e7278b35caceb561c78c09cd9d4ff6285
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-01 07:39:02 +0900

    Divide a job into small pieces

--- app/workers/import_twitter_db_s3_users_worker.rb
+++ app/workers/import_twitter_db_s3_users_worker.rb
@@ -6,9 +6,13 @@ class ImportTwitterDbS3UsersWorker
     persisted_uids = TwitterDB::User.where(uid: uids, updated_at: 1.hour.ago..Time.zone.now).pluck(:uid)
     logger.debug {"uids #{uids.size}, persisted_uids #{persisted_uids.size}"}
 
-    TwitterDB::User.where(uid: persisted_uids).select(:id, :uid, :screen_name, :user_info).find_each(batch_size: 100) do |user|
+    target_uids = persisted_uids.take(100)
+
+    TwitterDB::User.where(uid: target_uids).select(:id, :uid, :screen_name, :user_info).each do |user|
       TwitterDB::S3::Profile.import_from!(user.uid, user.screen_name, user.user_info)
     end
+
+    self.class.perform_async(persisted_uids - target_uids)
   rescue => e
     logger.warn "#{e.class}: #{e.message} #{uids.inspect.truncate(100)}"
     logger.info e.backtrace.join("\n")
