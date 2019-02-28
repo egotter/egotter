d80b0974fa9c396e49b3026678b8cde14acff4af
ts-3156 <ts_3156@yahoo.co.jp>
2019-02-28 17:25:31 +0900

    Implement TwitterDB::S3::Friendship

---
+++ app/models/twitter_db/s3/followership.rb
@@ -0,0 +1,12 @@
+# -*- SkipSchemaAnnotations
+module TwitterDB
+  module S3
+    class Followership
+      extend ::S3::Util
+      extend TwitterDB::S3::Api
+
+      self.bucket_name = "egotter.#{Rails.env}.twitter-db.followerships"
+      self.uids_key = :follower_uids
+    end
+  end
+end
