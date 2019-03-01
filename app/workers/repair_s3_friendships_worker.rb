7b8e8c803fb80360a6174c16fbafe71ab69e46e3
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-01 11:08:41 +0900

    Update RepairS3FriendshipsWorker

--- app/workers/repair_s3_friendships_worker.rb
+++ app/workers/repair_s3_friendships_worker.rb
@@ -8,33 +8,58 @@ class RepairS3FriendshipsWorker
     s3_status = [
         S3::Friendship.cache_disabled {S3::Friendship.exists?(twitter_user_id: user.id)},
         S3::Followership.cache_disabled {S3::Followership.exists?(twitter_user_id: user.id)},
-        S3::Profile.cache_disabled {!S3::Profile.exists?(twitter_user_id: user.id)}
+        S3::Profile.cache_disabled {S3::Profile.exists?(twitter_user_id: user.id)}
     ]
 
-    unless user.friends_size == 0 && user.followers_size == 0
-      logger.warn "Can't repair S3::Friendship, S3::Followership and S3::Profile #{s3_status.inspect} #{user.inspect}"
+    if s3_status[0] && s3_status[1] && s3_status[2]
+      logger.warn "There is not need to update #{twitter_user_id}"
       return false
     end
 
     updated = false
 
-    unless s3_status[0]
-      S3::Friendship.import_from!(user.id, user.uid, user.screen_name, [])
-      logger.warn "S3::Friendship is updated #{twitter_user_id}"
-      updated = true
-    end
-
-    unless s3_status[1]
-      S3::Followership.import_from!(user.id, user.uid, user.screen_name, [])
-      logger.warn "S3::Followership is updated #{twitter_user_id}"
-      updated = true
-    end
-
-    if s3_status[2] &&
+    if !s3_status[2] &&
         user.user_info.present? &&
         user.user_info != '{}'
+
       S3::Profile.import_from!(user.id, user.uid, user.screen_name, user.user_info)
+
       logger.warn "S3::Profile is updated #{twitter_user_id}"
+      updated = true
+    end
+
+    if user.friends_size == 0 &&
+        user.followers_size == 0 &&
+        !s3_status[0] &&
+        !s3_status[1]
+
+      S3::Friendship.import_from!(user.id, user.uid, user.screen_name, [])
+      S3::Followership.import_from!(user.id, user.uid, user.screen_name, [])
+
+      logger.warn "S3::Friendship and S3::Followership are updated with empty array #{twitter_user_id}"
+
+      return true
+    end
+
+    friendships = Friendship.where(from_id: user.id).order(sequence: :asc)
+
+    if user.friends_size != 0 &&
+        user.friends_size == friendships.size &&
+        !s3_status[0]
+
+      S3::Friendship.import_from!(user.id, user.uid, user.screen_name, friendships.pluck(:friend_uid))
+      logger.warn "S3::Friendship is updated by #{friendships.size} users #{twitter_user_id}"
+      updated = true
+    end
+
+    followerships = Followership.where(from_id: user.id).order(sequence: :asc)
+
+    if user.followers_size != 0 &&
+        user.followers_size == followerships.size &&
+        !s3_status[1]
+
+      S3::Followership.import_from!(user.id, user.uid, user.screen_name, followerships.pluck(:follower_uid))
+      logger.warn "S3::Followership is updated by #{followerships.size} users #{twitter_user_id}"
       updated = true
     end
 
@@ -42,8 +67,6 @@ class RepairS3FriendshipsWorker
       logger.warn "Nothing is updated #{s3_status.inspect} #{user.inspect}"
       return false
     end
-
-    logger.warn "Updated #{s3_status.inspect} #{user.inspect}"
 
     true
   rescue => e
