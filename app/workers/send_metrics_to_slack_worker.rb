f8cd53c60affd712cf89cdf52ca1b241dff3cf4f
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-02 05:11:22 +0900

    Create SendMetricsToSlackWorker

---
+++ app/workers/send_metrics_to_slack_worker.rb
@@ -0,0 +1,21 @@
+class SendMetricsToSlackWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'misc', retry: 0, backtrace: false
+
+  def perform
+    counts = [SearchLog, SignInLog, TwitterUser, SearchHistory, Job].map do |klass|
+      [klass.to_s, klass.where(created_at: 1.hour.ago..Time.zone.now).size]
+    end.to_h
+
+    send_message(counts.to_s)
+  rescue => e
+    logger.warn "#{e.class}: #{e.message}"
+    logger.info e.backtrace.join("\n")
+  end
+
+  URL = ENV['SLACK_METRICS_WEBHOOK_URL']
+
+  def send_message(text)
+    HTTParty.post(URL, body: {text: text}.to_json)
+  end
+end
