d4f1aa0014c44dbf883b637a9578a8ba0426f58b
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-02 00:10:27 +0900

    Update RepairS3FriendshipsWorker

--- app/workers/repair_s3_friendships_worker.rb
+++ app/workers/repair_s3_friendships_worker.rb
@@ -21,9 +21,9 @@ class RepairS3FriendshipsWorker
         S3::Followership.find_by(twitter_user_id: user.id)[:follower_uids].size == user.followers_size &&
         S3::Profile.find_by(twitter_user_id: user.id)[:user_info].present?
 
-        logger.info "There is no need to update #{s3_status.inspect} #{twitter_user_id}"
+        logger.info "There is no need to update #{to_s(s3_status, user)}"
       else
-        logger.warn "File is found but there is mismatch  #{s3_status.inspect} #{twitter_user_id}"
+        logger.warn "File is found but there is mismatch  #{to_s(s3_status, user)}"
       end
 
       return false
@@ -31,13 +31,37 @@ class RepairS3FriendshipsWorker
 
     updated = false
 
+    if user.friends_size == 0 &&
+        user.followers_size == 0 &&
+        !s3_status[0] &&
+        s3_status[1] &&
+        s3_status[1][:follower_uids].size == 0
+
+      S3::Followership.import_from!(user.id, user.uid, user.screen_name, [])
+
+      logger.warn "S3::Followership are updated with empty array #{to_s(s3_status, user)}"
+      updated = true
+    end
+
+    if user.friends_size == 0 &&
+        user.followers_size == 0 &&
+        !s3_status[1] &&
+        s3_status[0] &&
+        s3_status[0][:friend_uids].size == 0
+
+      S3::Friendship.import_from!(user.id, user.uid, user.screen_name, [])
+
+      logger.warn "S3::Friendship are updated with empty array #{to_s(s3_status, user)}"
+      updated = true
+    end
+
     if !s3_status[2] &&
         user.user_info.present? &&
         user.user_info != '{}'
 
       S3::Profile.import_from!(user.id, user.uid, user.screen_name, user.user_info)
 
-      logger.warn "S3::Profile is updated #{twitter_user_id}"
+      logger.warn "S3::Profile is updated #{to_s(s3_status, user)}"
       updated = true
     end
 
@@ -49,7 +73,7 @@ class RepairS3FriendshipsWorker
       S3::Friendship.import_from!(user.id, user.uid, user.screen_name, [])
       S3::Followership.import_from!(user.id, user.uid, user.screen_name, [])
 
-      logger.warn "S3::Friendship and S3::Followership are updated with empty array #{twitter_user_id}"
+      logger.warn "S3::Friendship and S3::Followership are updated with empty array #{to_s(s3_status, user)}"
 
       return true
     end
@@ -61,7 +85,7 @@ class RepairS3FriendshipsWorker
         !s3_status[0]
 
       S3::Friendship.import_from!(user.id, user.uid, user.screen_name, friendships.pluck(:friend_uid))
-      logger.warn "S3::Friendship is updated by #{friendships.size} users #{twitter_user_id}"
+      logger.warn "S3::Friendship is updated by #{friendships.size} users #{to_s(s3_status, user)}"
       updated = true
     end
 
@@ -72,18 +96,22 @@ class RepairS3FriendshipsWorker
         !s3_status[1]
 
       S3::Followership.import_from!(user.id, user.uid, user.screen_name, followerships.pluck(:follower_uid))
-      logger.warn "S3::Followership is updated by #{followerships.size} users #{twitter_user_id}"
+      logger.warn "S3::Followership is updated by #{followerships.size} users #{to_s(s3_status, user)}"
       updated = true
     end
 
     unless updated
-      logger.warn "Nothing is updated #{s3_status.inspect} #{user.inspect}"
+      logger.warn "Nothing is updated #{to_s(s3_status, user)}"
       return false
     end
 
     true
   rescue => e
-    logger.warn "#{e.class}: #{e.message} #{twitter_user_id}"
+    logger.warn "#{e.class}: #{e.message} #{to_s(s3_status, user) rescue nil}"
     logger.info e.backtrace.join("\n")
+  end
+
+  def to_s(s3_status, user)
+    "#{s3_status} #{user.slice(:id, :uid, :screen_name, :friends_size, :followers_size, :user_info).tap{|h| h[:user_info].truncate(10) }}"
   end
 end
