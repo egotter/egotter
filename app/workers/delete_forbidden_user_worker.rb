6c8746143d8bbbe7f47ff3de281666739cd6a151
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-01 16:50:31 +0900

    Use RunningQueue in DeleteNotFoundUserWorker and DeleteForbiddenUserWorker

--- app/workers/delete_forbidden_user_worker.rb
+++ app/workers/delete_forbidden_user_worker.rb
@@ -3,6 +3,10 @@ class DeleteForbiddenUserWorker
   sidekiq_options queue: 'misc', retry: 0, backtrace: false
 
   def perform(screen_name)
+    queue = RunningQueue.new(self.class)
+    return if queue.exists?(screen_name)
+    queue.add(screen_name)
+
     ForbiddenUser.find_by(screen_name: screen_name)&.delete
   rescue ActiveRecord::RecordNotUnique => e
     logger.info e.message.truncate(100)
