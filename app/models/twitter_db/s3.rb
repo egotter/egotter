c887917fc99bce3fc1b8554dec401668728010ec
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-05 14:44:22 +0900

    Implement S3::Querying

--- app/models/twitter_db/s3.rb
+++ app/models/twitter_db/s3.rb
@@ -2,32 +2,8 @@
 module TwitterDB
   module S3
     module Api
-      def find_by!(uid:)
-        text = fetch(uid)
-        item = parse_json(text)
-        uids = item.has_key?('compress') ? unpack(item[uids_key.to_s]) : item[uids_key.to_s]
-        {
-            uid: item['uid'],
-            screen_name: item['screen_name'],
-            uids_key => uids
-        }
-      end
-
       def find_by(uid:)
-        tries ||= 3
-        find_by!(uid: uid)
-      rescue Aws::S3::Errors::NoSuchKey => e
-        message = "#{self}##{__method__} #{e.class} #{e.message} #{uid}"
-
-        if (tries -= 1) < 0
-          Rails.logger.warn "RETRY EXHAUSTED #{message}"
-          Rails.logger.info {e.backtrace.join("\n")}
-          {}
-        else
-          Rails.logger.warn "RETRY #{tries} #{message}"
-          sleep 0.1
-          retry
-        end
+        find_by_current_scope(uids_key, :uid, uid)
       end
 
       def import_by!(user:)
@@ -63,32 +39,8 @@ module TwitterDB
         :user_info
       end
 
-      def find_by!(uid:)
-        text = fetch(uid)
-        item = parse_json(text)
-        profile = item.has_key?('compress') ? unpack(item[profile_key.to_s]) : item[profile_key.to_s]
-        {
-            uid: item['uid'],
-            screen_name: item['screen_name'],
-            profile_key => profile
-        }
-      end
-
       def find_by(uid:)
-        tries ||= 5
-        find_by!(uid: uid)
-      rescue Aws::S3::Errors::NoSuchKey => e
-        message = "#{self}##{__method__} #{e.class} #{e.message} #{uid}"
-
-        if (tries -= 1) < 0
-          Rails.logger.warn "RETRY EXHAUSTED #{message}"
-          Rails.logger.info {e.backtrace.join("\n")}
-          {}
-        else
-          Rails.logger.info "RETRY #{tries} #{message}"
-          sleep 0.1 * (5 - tries)
-          retry
-        end
+        find_by_current_scope(profile_key, :uid, uid)
       end
 
       def import_by!(twitter_db_user:)
