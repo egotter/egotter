cfca3680f458fd102b61e349e94d88fa9e7eb284
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-07 06:26:58 +0900

    Add benchmark code to WriteProfilesToS3Worker

--- app/workers/write_profiles_to_s3_worker.rb
+++ app/workers/write_profiles_to_s3_worker.rb
@@ -23,15 +23,26 @@ class WriteProfilesToS3Worker
     client = user ? user.api_client : Bot.api_client
 
     target_uids = uids.take(100)
-
-    client.users(target_uids).each do |user|
-      TwitterDB::S3::Profile.import_from!(user[:id], user[:screen_name], TwitterUser.collect_user_info(user))
-    end
+    import(fetch_users(client, target_uids))
 
     next_uids = uids - target_uids
     self.class.perform_in(retry_in, next_uids) if next_uids.any?
   rescue => e
     logger.warn "#{e.class}: #{e.message} #{uids.inspect.truncate(100)}"
     logger.info e.backtrace.join("\n")
+  end
+
+  def fetch_users(client, uids)
+    ApplicationRecord.benchmark("#{self.class}##{__method__} #{uids.size}", level: :info) do
+      client.users(uids)
+    end
+  end
+
+  def import(users)
+    ApplicationRecord.benchmark("#{self.class}##{__method__} #{users.size}", level: :info) do
+      users.each do |user|
+        TwitterDB::S3::Profile.import_from!(user[:id], user[:screen_name], TwitterUser.collect_user_info(user))
+      end
+    end
   end
 end
