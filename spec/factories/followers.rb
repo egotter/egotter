fee2056d3ea95391d3e42460836452c55bfba453
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-07 10:24:00 +0900

    Remove Friendship and Followership

--- spec/factories/followers.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :follower do
-    sequence(:uid, 200) { |n| n }
-    screen_name {'friend_sn'}
-    user_info { {id: uid, screen_name: screen_name, friends_count: 1, followers_count: 1, protected: true}.to_json }
-  end
-end
--- spec/factories/friends.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :friend do
-    sequence(:uid, 100) { |n| n }
-    screen_name {'friend_sn'}
-    user_info { {id: uid, screen_name: screen_name, friends_count: 1, followers_count: 1, protected: true}.to_json }
-  end
-end
