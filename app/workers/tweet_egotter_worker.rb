b21e48790b17d686b0b91c27f754b82a203fdf15
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-09 06:02:21 +0900

    Handle 'Could not authenticate you.' error

--- app/workers/tweet_egotter_worker.rb
+++ app/workers/tweet_egotter_worker.rb
@@ -1,18 +1,33 @@
 class TweetEgotterWorker
   include Sidekiq::Worker
-  include Concerns::WorkerUtils
   sidekiq_options queue: 'misc', retry: 0, backtrace: false
 
-  def perform(user_id, text)
-    User.find(user_id).api_client.twitter.update(text)
+  def perform(user_id, text, options = {})
+    user = User.find(user_id)
+    user.api_client.twitter.update(text)
   rescue Twitter::Error::Unauthorized => e
-    unless e.message == 'Invalid or expired token.'
-      logger.warn "#{e.class}: #{e.message} #{user_id}"
+    if e.message == 'Invalid or expired token.'
+    elsif e.message == 'Could not authenticate you.'
+      logger.warn "#{e.class} #{e.message} #{slice_user(user)} #{options}"
+
+      retry_count = options[:retry_count] || 0
+      if retry_count < 5
+        self.class.perform_in(5.seconds, user_id, text, options.merge(retry_count: retry_count + 1))
+      end
+    else
+      logger.warn "#{e.class} #{e.message} #{slice_user(user)} #{options}"
       logger.info e.backtrace.join("\n")
     end
   rescue Twitter::Error::Forbidden => e
-    handle_forbidden_exception(e, user_id: user_id)
+    logger.warn "#{e.class} #{e.message} #{slice_user(user)} #{options}"
+    logger.info e.backtrace.join("\n")
   rescue => e
-    logger.warn "#{e.class} #{e.message} #{user_id} #{text}"
+    logger.warn "#{e.class} #{e.message} #{slice_user(user)} #{options}"
+    logger.info e.backtrace.join("\n")
+  end
+
+  def slice_user(user)
+    # Don't log access_token and access_secret
+    user.slice(:id, :uid, :screen_name, :created_at)
   end
 end
