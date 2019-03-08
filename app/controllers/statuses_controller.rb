af258324fceeeb3d6e0a503dafb116d8100a801a
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-09 05:05:17 +0900

    Reject statuses that doesn't have user

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,6 +2,11 @@ class StatusesController < ApplicationController
   include Concerns::Showable
 
   def show
-    @statuses = @twitter_user.statuses.limit(20)
+    statuses = @twitter_user.statuses.limit(20)
+    @statuses = statuses.select(&:user)
+
+    if statuses.size != @statuses.size
+      logger.warn "Status doesn't have user. #{@twitter_user.inspect}"
+    end
   end
 end
