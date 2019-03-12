b95bc622888d7f0b69448394d754a02bb0408f29
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-12 14:18:57 +0900

    Benchmark Persistence module

--- app/models/concerns/twitter_user/persistence.rb
+++ app/models/concerns/twitter_user/persistence.rb
@@ -8,23 +8,23 @@ module Concerns::TwitterUser::Persistence
   included do
     # Relations are created on `after_commit` in order to avoid long transaction.
     after_commit(on: :create) do
-      start = Time.zone.now
-
-      [TwitterDB::Status, TwitterDB::Mention, TwitterDB::Favorite].each do |klass|
-        begin
-          klass.import_by!(twitter_user: self)
-        rescue => e
-          logger.warn "#{__method__} #{klass}: Continue to saving #{e.class} #{e.message.truncate(100)} #{self.inspect}"
+      ApplicationRecord.benchmark("Persistence##{__method__} first half #{id} #{screen_name}", level: :info) do
+        [TwitterDB::Status, TwitterDB::Mention, TwitterDB::Favorite].each do |klass|
+          begin
+            klass.import_by!(twitter_user: self)
+          rescue => e
+            logger.warn "Persistence##{__method__} #{klass}: Continue to saving #{e.class} #{e.message.truncate(100)} #{self.inspect}"
+          end
         end
       end
 
       # Set friends_size and followers_size in AssociationBuilder#build_friends_and_followers
 
-      S3::Friendship.import_from!(id, uid, screen_name, @friend_uids)
-      S3::Followership.import_from!(id, uid, screen_name, @follower_uids)
-      S3::Profile.import_from!(id, uid, screen_name, raw_attrs_text)
-
-      logger.debug {"Persistence##{__method__} #{id} #{screen_name} #{Time.zone.now - start}"}
+      ApplicationRecord.benchmark("Persistence##{__method__} second half #{id} #{screen_name}", level: :info) do
+        S3::Friendship.import_from!(id, uid, screen_name, @friend_uids)
+        S3::Followership.import_from!(id, uid, screen_name, @follower_uids)
+        S3::Profile.import_from!(id, uid, screen_name, raw_attrs_text)
+      end
 
     rescue => e
       # ActiveRecord::RecordNotFound Couldn't find TwitterUser with 'id'=00000
