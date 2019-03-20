41fb5b18981538f153394d53c59964c34c178081
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-20 15:37:31 +0900

    Update ads

--- app/helpers/adsense_helper.rb
+++ app/helpers/adsense_helper.rb
@@ -150,7 +150,31 @@ module AdsenseHelper
 
   GUEST_HOME_RESP                 = 6274092442
   GUEST_TIMELINES_TOP_RESP        = 6629315666
+  GUEST_TIMELINES_MMIDDLE_RESP    = 6046113305
   GUEST_TIMELINES_BOTTOM_RESP     = 1185417297
+
+  GUEST_AUDIENCE_INSIGHTS_RESP    = 3303210523
+  GUEST_BLOCKING_OR_BLOCKED_RESP  = 8863848337
+  GUEST_CLOSE_FRIENDS_RESP        = 5886331208
+  GUEST_CLUSTERS_RESP             = 1747775648
+  GUEST_FAVORITE_FRIENDS_RESP     = 7135833699
+  GUEST_FOLLOWERS_RESP            = 9378853659
+  GUEST_FRIENDS_RESP              = 5056465261
+  GUEST_INACTIVE_FOLLOWERS_RESP   = 1387493623
+  GUEST_INACTIVE_FRIENDS_RESP     = 7685308668
+  GUEST_INACTIVE_MUTUAL_FRIENDS_RESP = 5359717591
+  GUEST_MUTUAL_FRIENDS_RESP       = 9377664280
+  GUEST_ONE_SIDED_FOLLOWERS_RESP  = 1755514503
+  GUEST_ONE_SIDED_FRIENDS_RESP    = 9554113474
+  GUEST_REPLIED_RESP              = 4125337604
+  GUEST_REPLYING_AND_REPLIED_RESP = 5169335276
+  GUEST_REPLYING_RESP             = 9046491792
+  GUEST_SCORES_RESP               = 1372371120
+  GUEST_STATUSES_RESP             = 7746207785
+  GUEST_UNFOLLOWERS_RESP          = 4730351572
+  GUEST_UNFRIENDS_RESP            = 2302309413
+  GUEST_UPDATE_HISTORIES_RESP     = 2898395159
+  GUEST_USAGE_STATS_RESP          = 5985214186
 
   USER_TOP_TOP_RESP               = 6872899613
   USER_CLOSE_FRIENDS_TOP_RESP     = 8210032018
@@ -164,7 +188,9 @@ module AdsenseHelper
 
   USER_HOME_RESP                  = 2721860243
   USER_TIMELINES_TOP_RESP         = 3564168622
+  USER_TIMELINES_MIDDLE_RESP      = 4253330115
   USER_TIMELINES_BOTTOM_RESP      = 9147011257
+  USER_AUDIENCE_INSIGHTS_RESP     = 8987971417
   USER_BLOCKING_OR_BLOCKED_RESP   = 4128464773
   USER_CLOSE_FRIENDS_RESP         = 1523672104
   USER_CLUSTERS_RESP              = 8444303677
@@ -176,10 +202,14 @@ module AdsenseHelper
   USER_FRIENDS_RESP               = 7585328204
   USER_INACTIVE_FOLLOWERS_RESP    = 7202184827
   USER_INACTIVE_FRIENDS_RESP      = 5466786547
+  USER_INACTIVE_MUTUAL_FRIENDS_RESP = 8552857148
+  USER_MUTUAL_FRIENDS_RESP        = 7809431740
   USER_ONE_SIDED_FOLLOWERS_RESP   = 2425690232
   USER_ONE_SIDED_FRIENDS_RESP     = 9346321801
   USER_REPLIED_RESP               = 5230627600
   USER_REPLYING_RESP              = 3725974245
+  USER_REPLYING_AND_REPLIED_RESP  = 8305445130
+  USER_SCORES_RESP                = 1216103231
   USER_STATUSES_RESP              = 8403585859
   USER_UNFOLLOWERS_RESP           = 2768115790
   USER_UNFRIENDS_RESP             = 5925951541
@@ -190,9 +220,11 @@ module AdsenseHelper
     slot =
         case [user_signed_in?, controller_name.to_s, action_name.to_s, vertical_position.to_sym]
         when [true,  'home',                  'new',  :top]    then USER_HOME_RESP
-        when [true,  'timelines',             'show', :top]    then USER_TIMELINES_TOP_RESP
         when [true,  'timelines',             'show', :bottom] then USER_TIMELINES_BOTTOM_RESP
+        when [true,  'timelines',             'show', :middle] then USER_TIMELINES_MIDDLE_RESP
+        when [true,  'timelines',             'show', :top]    then USER_TIMELINES_TOP_RESP
         when [true,  'waiting',               'new',  :top]    then USER_WAITING_RESP
+        when [true,  'audience_insights',     'show', :bottom] then USER_AUDIENCE_INSIGHTS_RESP
         when [true,  'blocking_or_blocked',   'list', :slit]   then USER_BLOCKING_OR_BLOCKED_RESP
         when [true,  'blocking_or_blocked',   'show', :bottom] then USER_BLOCKING_OR_BLOCKED_RESP
         when [true,  'blocking_or_blocked',   'show', :top]    then USER_BLOCKING_OR_BLOCKED_RESP
@@ -200,6 +232,7 @@ module AdsenseHelper
         when [true,  'close_friends',         'show', :bottom] then USER_CLOSE_FRIENDS_RESP
         when [true,  'close_friends',         'show', :middle] then USER_CLOSE_FRIENDS_RESP
         when [true,  'close_friends',         'show', :top]    then USER_CLOSE_FRIENDS_RESP
+        when [true,  'clusters',              'new',  :top]    then USER_CLUSTERS_RESP
         when [true,  'clusters',              'show', :top]    then USER_CLUSTERS_RESP
         when [true,  'common_followers',      'list', :slit]   then USER_COMMON_FOLLOWERS_RESP
         when [true,  'common_followers',      'show', :bottom] then USER_COMMON_FOLLOWERS_RESP
@@ -212,12 +245,15 @@ module AdsenseHelper
         when [true,  'common_mutual_friends', 'show', :bottom] then USER_COMMON_MUTUAL_FRIENDS_RESP
         when [true,  'common_mutual_friends', 'show', :middle] then USER_COMMON_MUTUAL_FRIENDS_RESP
         when [true,  'common_mutual_friends', 'show', :top]    then USER_COMMON_MUTUAL_FRIENDS_RESP
+        when [true,  'favorite_friends',      'list', :slit]   then USER_FAVORITE_FRIENDS_RESP
         when [true,  'favorite_friends',      'show', :bottom] then USER_FAVORITE_FRIENDS_RESP
         when [true,  'favorite_friends',      'show', :middle] then USER_FAVORITE_FRIENDS_RESP
         when [true,  'favorite_friends',      'show', :top]    then USER_FAVORITE_FRIENDS_RESP
+        when [true,  'followers',             'list', :slit]   then USER_FOLLOWERS_RESP
         when [true,  'followers',             'show', :bottom] then USER_FOLLOWERS_RESP
         when [true,  'followers',             'show', :middle] then USER_FOLLOWERS_RESP
         when [true,  'followers',             'show', :top]    then USER_FOLLOWERS_RESP
+        when [true,  'friends',               'list',  :slit]  then USER_FRIENDS_RESP
         when [true,  'friends',               'new',  :top]    then USER_FRIENDS_RESP
         when [true,  'friends',               'show', :bottom] then USER_FRIENDS_RESP
         when [true,  'friends',               'show', :middle] then USER_FRIENDS_RESP
@@ -225,23 +261,41 @@ module AdsenseHelper
         when [true,  'inactive_followers',    'show', :bottom] then USER_INACTIVE_FOLLOWERS_RESP
         when [true,  'inactive_followers',    'show', :middle] then USER_INACTIVE_FOLLOWERS_RESP
         when [true,  'inactive_followers',    'show', :top]    then USER_INACTIVE_FOLLOWERS_RESP
+        when [true,  'inactive_friends',      'list',  :list]  then USER_INACTIVE_FRIENDS_RESP
         when [true,  'inactive_friends',      'new',  :top]    then USER_INACTIVE_FRIENDS_RESP
         when [true,  'inactive_friends',      'show', :bottom] then USER_INACTIVE_FRIENDS_RESP
         when [true,  'inactive_friends',      'show', :middle] then USER_INACTIVE_FRIENDS_RESP
         when [true,  'inactive_friends',      'show', :top]    then USER_INACTIVE_FRIENDS_RESP
+        when [true,  'inactive_mutual_friends','show',:bottom] then USER_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [true,  'inactive_mutual_friends','show',:middle] then USER_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [true,  'inactive_mutual_friends','show',:top]    then USER_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [true,  'mutual_friends',        'list', :slit]   then USER_MUTUAL_FRIENDS_RESP
+        when [true,  'mutual_friends',        'show', :bottom] then USER_MUTUAL_FRIENDS_RESP
+        when [true,  'mutual_friends',        'show', :middle] then USER_MUTUAL_FRIENDS_RESP
+        when [true,  'mutual_friends',        'show', :top]    then USER_MUTUAL_FRIENDS_RESP
+        when [true,  'one_sided_followers',   'list', :slit]   then USER_ONE_SIDED_FOLLOWERS_RESP
         when [true,  'one_sided_followers',   'show', :bottom] then USER_ONE_SIDED_FOLLOWERS_RESP
         when [true,  'one_sided_followers',   'show', :middle] then USER_ONE_SIDED_FOLLOWERS_RESP
         when [true,  'one_sided_followers',   'show', :top]    then USER_ONE_SIDED_FOLLOWERS_RESP
+        when [true,  'one_sided_friends',     'list', :slit]   then USER_ONE_SIDED_FRIENDS_RESP
         when [true,  'one_sided_friends',     'new',  :top]    then USER_ONE_SIDED_FRIENDS_RESP
         when [true,  'one_sided_friends',     'show', :bottom] then USER_ONE_SIDED_FRIENDS_RESP
         when [true,  'one_sided_friends',     'show', :middle] then USER_ONE_SIDED_FRIENDS_RESP
         when [true,  'one_sided_friends',     'show', :top]    then USER_ONE_SIDED_FRIENDS_RESP
+        when [true,  'replied',               'list', :slit]   then USER_REPLIED_RESP
         when [true,  'replied',               'show', :bottom] then USER_REPLIED_RESP
         when [true,  'replied',               'show', :middle] then USER_REPLIED_RESP
         when [true,  'replied',               'show', :top]    then USER_REPLIED_RESP
+        when [true,  'replying',              'list', :slit]   then USER_REPLYING_RESP
         when [true,  'replying',              'show', :bottom] then USER_REPLYING_RESP
         when [true,  'replying',              'show', :middle] then USER_REPLYING_RESP
         when [true,  'replying',              'show', :top]    then USER_REPLYING_RESP
+        when [true,  'replying_and_replied',  'show', :bottom] then USER_REPLYING_AND_REPLIED_RESP
+        when [true,  'replying_and_replied',  'show', :middle] then USER_REPLYING_AND_REPLIED_RESP
+        when [true,  'replying_and_replied',  'show', :top]    then USER_REPLYING_AND_REPLIED_RESP
+        when [true,  'scores',                'show', :bottom] then USER_SCORES_RESP
+        when [true,  'scores',                'show', :middle] then USER_SCORES_RESP
+        when [true,  'scores',                'show', :top]    then USER_SCORES_RESP
         when [true,  'statuses',              'show', :top]    then USER_STATUSES_RESP
         when [true,  'unfollowers',           'list', :slit]   then USER_UNFOLLOWERS_RESP
         when [true,  'unfollowers',           'show', :bottom] then USER_UNFOLLOWERS_RESP
@@ -255,10 +309,87 @@ module AdsenseHelper
         when [true,  'usage_stats',           'show', :bottom] then USER_USAGE_STATS_RESP
         when [true,  'usage_stats',           'show', :middle] then USER_USAGE_STATS_RESP
         when [true,  'usage_stats',           'show', :top]    then USER_USAGE_STATS_RESP
+
         when [false, 'home',                  'new',  :top]    then GUEST_HOME_RESP
-        when [false, 'timelines',             'show', :top]    then GUEST_TIMELINES_TOP_RESP
         when [false, 'timelines',             'show', :bottom] then GUEST_TIMELINES_BOTTOM_RESP
+        when [false, 'timelines',             'show', :middle] then GUEST_TIMELINES_MIDDLE_RESP
+        when [false, 'timelines',             'show', :top]    then GUEST_TIMELINES_TOP_RESP
         when [false, 'waiting',               'new',  :top]    then GUEST_WAITING_RESP
+        when [false, 'audience_insights',     'show', :bottom] then GUEST_AUDIENCE_INSIGHTS_RESP
+        when [false, 'blocking_or_blocked',   'list', :slit]   then GUEST_BLOCKING_OR_BLOCKED_RESP
+        when [false, 'blocking_or_blocked',   'show', :bottom] then GUEST_BLOCKING_OR_BLOCKED_RESP
+        when [false, 'blocking_or_blocked',   'show', :top]    then GUEST_BLOCKING_OR_BLOCKED_RESP
+        when [false, 'close_friends',         'list', :slit]   then GUEST_CLOSE_FRIENDS_RESP
+        when [false, 'close_friends',         'show', :bottom] then GUEST_CLOSE_FRIENDS_RESP
+        when [false, 'close_friends',         'show', :middle] then GUEST_CLOSE_FRIENDS_RESP
+        when [false, 'close_friends',         'show', :top]    then GUEST_CLOSE_FRIENDS_RESP
+        when [false, 'clusters',              'new',  :top]    then GUEST_CLUSTERS_RESP
+        when [false, 'clusters',              'show', :top]    then GUEST_CLUSTERS_RESP
+        when [false, 'favorite_friends',      'list', :slit]   then GUEST_FAVORITE_FRIENDS_RESP
+        when [false, 'favorite_friends',      'show', :bottom] then GUEST_FAVORITE_FRIENDS_RESP
+        when [false, 'favorite_friends',      'show', :middle] then GUEST_FAVORITE_FRIENDS_RESP
+        when [false, 'favorite_friends',      'show', :top]    then GUEST_FAVORITE_FRIENDS_RESP
+        when [false, 'followers',             'list', :slit]   then GUEST_FOLLOWERS_RESP
+        when [false, 'followers',             'show', :bottom] then GUEST_FOLLOWERS_RESP
+        when [false, 'followers',             'show', :middle] then GUEST_FOLLOWERS_RESP
+        when [false, 'followers',             'show', :top]    then GUEST_FOLLOWERS_RESP
+        when [false, 'friends',               'list', :slit]   then GUEST_FRIENDS_RESP
+        when [false, 'friends',               'new',  :top]    then GUEST_FRIENDS_RESP
+        when [false, 'friends',               'show', :bottom] then GUEST_FRIENDS_RESP
+        when [false, 'friends',               'show', :middle] then GUEST_FRIENDS_RESP
+        when [false, 'friends',               'show', :top]    then GUEST_FRIENDS_RESP
+        when [false, 'inactive_followers',    'list', :slit]   then GUEST_INACTIVE_FOLLOWERS_RESP
+        when [false, 'inactive_followers',    'show', :bottom] then GUEST_INACTIVE_FOLLOWERS_RESP
+        when [false, 'inactive_followers',    'show', :middle] then GUEST_INACTIVE_FOLLOWERS_RESP
+        when [false, 'inactive_followers',    'show', :top]    then GUEST_INACTIVE_FOLLOWERS_RESP
+        when [false, 'inactive_friends',      'list', :slit]   then GUEST_INACTIVE_FRIENDS_RESP
+        when [false, 'inactive_friends',      'new',  :top]    then GUEST_INACTIVE_FRIENDS_RESP
+        when [false, 'inactive_friends',      'show', :bottom] then GUEST_INACTIVE_FRIENDS_RESP
+        when [false, 'inactive_friends',      'show', :middle] then GUEST_INACTIVE_FRIENDS_RESP
+        when [false, 'inactive_friends',      'show', :top]    then GUEST_INACTIVE_FRIENDS_RESP
+        when [false, 'inactive_mutual_friends','show',:bottom] then GUEST_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [false, 'inactive_mutual_friends','show',:middle] then GUEST_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [false, 'inactive_mutual_friends','show',:top]    then GUEST_INACTIVE_MUTUAL_FRIENDS_RESP
+        when [false, 'mutual_friends',        'list', :slit]   then GUEST_MUTUAL_FRIENDS_RESP
+        when [false, 'mutual_friends',        'show', :bottom] then GUEST_MUTUAL_FRIENDS_RESP
+        when [false, 'mutual_friends',        'show', :middle] then GUEST_MUTUAL_FRIENDS_RESP
+        when [false, 'mutual_friends',        'show', :top]    then GUEST_MUTUAL_FRIENDS_RESP
+        when [false, 'one_sided_followers',   'list', :slit]   then GUEST_ONE_SIDED_FOLLOWERS_RESP
+        when [false, 'one_sided_followers',   'show', :bottom] then GUEST_ONE_SIDED_FOLLOWERS_RESP
+        when [false, 'one_sided_followers',   'show', :middle] then GUEST_ONE_SIDED_FOLLOWERS_RESP
+        when [false, 'one_sided_followers',   'show', :top]    then GUEST_ONE_SIDED_FOLLOWERS_RESP
+        when [false, 'one_sided_friends',     'list', :slit]   then GUEST_ONE_SIDED_FRIENDS_RESP
+        when [false, 'one_sided_friends',     'new',  :top]    then GUEST_ONE_SIDED_FRIENDS_RESP
+        when [false, 'one_sided_friends',     'show', :bottom] then GUEST_ONE_SIDED_FRIENDS_RESP
+        when [false, 'one_sided_friends',     'show', :middle] then GUEST_ONE_SIDED_FRIENDS_RESP
+        when [false, 'one_sided_friends',     'show', :top]    then GUEST_ONE_SIDED_FRIENDS_RESP
+        when [false, 'replied',               'list', :slit]   then GUEST_REPLIED_RESP
+        when [false, 'replied',               'show', :bottom] then GUEST_REPLIED_RESP
+        when [false, 'replied',               'show', :middle] then GUEST_REPLIED_RESP
+        when [false, 'replied',               'show', :top]    then GUEST_REPLIED_RESP
+        when [false, 'replying',              'list', :slit]   then GUEST_REPLYING_RESP
+        when [false, 'replying',              'show', :bottom] then GUEST_REPLYING_RESP
+        when [false, 'replying',              'show', :middle] then GUEST_REPLYING_RESP
+        when [false, 'replying',              'show', :top]    then GUEST_REPLYING_RESP
+        when [false, 'replying_and_replied',  'show', :bottom] then GUEST_REPLYING_AND_REPLIED_RESP
+        when [false, 'replying_and_replied',  'show', :middle] then GUEST_REPLYING_AND_REPLIED_RESP
+        when [false, 'replying_and_replied',  'show', :top]    then GUEST_REPLYING_AND_REPLIED_RESP
+        when [false, 'scores',                'show', :bottom] then GUEST_SCORES_RESP
+        when [false, 'scores',                'show', :middle] then GUEST_SCORES_RESP
+        when [false, 'scores',                'show', :top]    then GUEST_SCORES_RESP
+        when [false, 'statuses',              'show', :top]    then GUEST_STATUSES_RESP
+        when [false, 'unfollowers',           'list', :slit]   then GUEST_UNFOLLOWERS_RESP
+        when [false, 'unfollowers',           'show', :bottom] then GUEST_UNFOLLOWERS_RESP
+        when [false, 'unfollowers',           'show', :top]    then GUEST_UNFOLLOWERS_RESP
+        when [false, 'unfriends',             'list', :slit]   then GUEST_UNFRIENDS_RESP
+        when [false, 'unfriends',             'new',  :top]    then GUEST_UNFRIENDS_RESP
+        when [false, 'unfriends',             'show', :bottom] then GUEST_UNFRIENDS_RESP
+        when [false, 'unfriends',             'show', :top]    then GUEST_UNFRIENDS_RESP
+        when [false, 'update_histories',      'show', :bottom] then GUEST_UPDATE_HISTORIES_RESP
+        when [false, 'update_histories',      'show', :top]    then GUEST_UPDATE_HISTORIES_RESP
+        when [false, 'usage_stats',           'show', :bottom] then GUEST_USAGE_STATS_RESP
+        when [false, 'usage_stats',           'show', :middle] then GUEST_USAGE_STATS_RESP
+        when [false, 'usage_stats',           'show', :top]    then GUEST_USAGE_STATS_RESP
         else nil
         end
 
