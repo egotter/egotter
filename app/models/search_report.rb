3a308c6f796f8d4800c77932fad76993b6830754
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-22 01:45:19 +0900

    Refactoring of SearchReport

--- app/models/search_report.rb
+++ app/models/search_report.rb
@@ -18,6 +18,10 @@
 #
 
 class SearchReport < ApplicationRecord
+  include Concerns::Report::HasToken
+  include Concerns::Report::HasDirectMessage
+  include Concerns::Report::Readable
+
   belongs_to :user
 
   def self.you_are_searched(user_id)
@@ -25,9 +29,9 @@ class SearchReport < ApplicationRecord
   end
 
   def deliver
-    DirectMessageRequest.new(user.api_client.twitter, User::EGOTTER_UID, I18n.t('dm.searchNotification.whats_happening', screen_name: screen_name)).perform
+    DirectMessageRequest.new(user_client, User::EGOTTER_UID, I18n.t('dm.searchNotification.whats_happening', screen_name: screen_name)).perform
     button = {label: I18n.t('dm.searchNotification.timeline_page', screen_name: screen_name), url: timeline_url}
-    resp = DirectMessageRequest.new(User.find_by(uid: User::EGOTTER_UID).api_client.twitter, user.uid.to_i, build_message, [button]).perform
+    resp = DirectMessageRequest.new(egotter_client, user.uid, build_message, [button]).perform
     dm = DirectMessage.new(resp)
 
     transaction do
@@ -38,34 +42,26 @@ class SearchReport < ApplicationRecord
     dm
   end
 
-  def read?
-    !read_at.nil?
+  private
+
+  def user_client
+    @user_client ||= user.api_client.twitter
   end
 
-  private
+  def egotter_client
+    @egotter_client ||= User.find_by(uid: User::EGOTTER_UID).api_client.twitter
+  end
 
   def screen_name
     user.screen_name
   end
 
   def build_message
-    ERB.new(Rails.root.join(template_path).read).result_with_hash(screen_name: screen_name, url: timeline_url)
+    template = Rails.root.join('app/views/search_reports/you_are_searched.ja.text.erb')
+    ERB.new(template.read).result_with_hash(screen_name: screen_name, url: timeline_url)
   end
 
   def timeline_url
-    'https://egotter.com' + Rails.application.routes.url_helpers.timeline_path(screen_name: screen_name, token: token, medium: 'dm', type: 'search')
-  end
-
-  def template_path
-    "app/views/#{self.class.name.underscore.pluralize}/you_are_searched.ja.text.erb"
-  end
-
-  class << self
-    def generate_token
-      begin
-        t = SecureRandom.urlsafe_base64(10)
-      end while exists?(token: t)
-      t
-    end
+    Rails.application.routes.url_helpers.timeline_url(screen_name: screen_name, token: token, medium: 'dm', type: 'search')
   end
 end
