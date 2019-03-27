10337949860040bfeb0a267e813241043465af46
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-27 17:14:47 +0900

    Emphasize words in follow-modal

--- app/workers/delayed_import_twitter_user_relations_worker.rb
+++ app/workers/delayed_import_twitter_user_relations_worker.rb
@@ -12,5 +12,11 @@ class DelayedImportTwitterUserRelationsWorker
       jobs = Sidekiq::Queue.new(name).select {|job| job.klass == name}
       jobs.map {|job| job.args.extract_options!['twitter_user_id']}
     end
+
+    def consume_delayed_twitter_user_id(id)
+      user = TwitterUser.find(id)
+      uids = user.friend_uids + user.follower_uids
+      CreateTwitterDBUserWorker.new.perform(uids) if uids.any?
+    end
   end
 end
