f397b608ab6f6647b44090a91e6897685e846445
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-31 07:05:24 +0900

    Remove cached: true from search_histories

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -1,6 +1,8 @@
 class SearchHistoriesController < ApplicationController
   def new
     users = current_search_histories.map(&:twitter_db_user)
-    render partial: 'twitter/user', collection: users, cached: true, locals: {grid_class: 'col-xs-12', ad: false, via: 'search_histories'}
+
+    # Don't specify cached: true because wrong data (duplicate records are removed?) is cached.
+    render partial: 'twitter/user', collection: users, locals: {grid_class: 'col-xs-12', ad: false, via: 'search_histories'}
   end
 end
