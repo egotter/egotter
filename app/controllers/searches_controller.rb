362a48897b402af954135dd20ef04a2534aa8337
ts-3156 <ts_3156@yahoo.co.jp>
2019-03-31 17:19:13 +0900

    log if current_visit is nil

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -20,7 +20,7 @@ class SearchesController < ApplicationController
   end
 
   def create
-    CreateSearchHistoryWorker.perform_async(fingerprint, current_user_id, @twitter_user.uid, current_visit&.id)
+    CreateSearchHistoryWorker.perform_async(fingerprint, current_user_id, @twitter_user.uid, current_visit_id)
 
     uid, screen_name = @twitter_user.uid, @twitter_user.screen_name
     redirect_path = sanitized_redirect_path(params[:redirect_path].presence || timeline_path(@twitter_user))
@@ -31,6 +31,17 @@ class SearchesController < ApplicationController
       save_twitter_user_to_cache(uid, screen_name, @twitter_user.raw_attrs_text)
       jid = enqueue_create_twitter_user_job_if_needed(uid, user_id: current_user_id, screen_name: screen_name)
       redirect_to waiting_search_path(uid: uid, redirect_path: redirect_path, jid: jid)
+    end
+  end
+
+  private
+
+  def current_visit_id
+    if current_visit.nil?
+      logger.info "current_visit is nil. #{fingerprint} #{current_user_id}"
+      nil
+    else
+      current_visit.id
     end
   end
 end
