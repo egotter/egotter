f13a24a11d816ed5b9a8ee53f7379adb6bc42049
ts-3156 <ts_3156@yahoo.co.jp>
2019-04-01 19:04:40 +0900

    Create SendExceptionToRollbarWorker

---
+++ app/workers/send_exception_to_rollbar_worker.rb
@@ -0,0 +1,16 @@
+require 'rollbar/delay/sidekiq'
+
+class SendExceptionToRollbarWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'rollbar', retry: 0, backtrace: false
+
+  def perform(payload)
+    data = payload['data']
+    logger.warn "exception=#{data['body']['trace']['exception']} context=#{data['context']} person=#{data['person']}"
+
+    Rollbar.process_from_async_handler(payload)
+  rescue => e
+    logger.warn "#{e.class} #{e.message} #{payload.truncate(100)}"
+    logger.info e.backtrace.join("\n")
+  end
+end
