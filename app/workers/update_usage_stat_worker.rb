7bb5d33e8ab1028486bb7a2afd661acb5a864e06
ts-3156 <ts_3156@yahoo.co.jp>
2019-04-01 17:12:07 +0900

    Process UpdateUsageStatWorker in misc queue

--- app/workers/update_usage_stat_worker.rb
+++ app/workers/update_usage_stat_worker.rb
@@ -1,6 +1,6 @@
 class UpdateUsageStatWorker
   include Sidekiq::Worker
-  sidekiq_options queue: self, retry: 0, backtrace: false
+  sidekiq_options queue: 'misc', retry: 0, backtrace: false
 
   def unique_key(uid, options = {})
     uid
