156cc38fbebad3a158cfd5407ff97d198e9d7565
ts-3156 <ts_3156@yahoo.co.jp>
2019-04-05 05:57:41 +0900

    Update regexp of crawlers

--- app/helpers/crawlers_helper.rb
+++ app/helpers/crawlers_helper.rb
@@ -9,10 +9,21 @@ module CrawlersHelper
 
   private
 
-  CRAWLERS_REGEXP = /Applebot|Jooblebot|SBooksNet|AdsBot-Google-Mobile|FlipboardProxy|HeartRails_Capture|Mail\.RU_Bot|360Spider/
+  CRAWLERS_REGEXP = /Applebot|Jooblebot|SBooksNet|AdsBot-Google-Mobile|FlipboardProxy|HeartRails_Capture|Mail\.RU_Bot|360Spider|Yahoo Ad monitoring|KZ BRAIN Mobile|Researchscan\/t13rl|Y!J-BRW\/1.0|NetcraftSurveyAgent|https?:\/\//
+  CRAWLERS_FULL_UA = [
+      'WWWC/1.13',
+      'Chrome',
+      'Hatena::UserAgent/0.02',
+      'Hatena-Favicon2 (http://www.hatena.ne.jp/faq/)',
+      'Mozilla/5.0 (compatible; TrendsmapResolver/0.1)',
+      'Mozilla/5.0 (compatible; evc-batch/2.0)',
+      'Mozilla/5.0 zgrab/0.x',
+      'Ruby',
+  ]
 
   def from_minor_crawler?(user_agent)
-    user_agent.to_s.match? CRAWLERS_REGEXP
+    ua = user_agent.to_s
+    CRAWLERS_FULL_UA.include?(ua) || ua.match?(CRAWLERS_REGEXP)
   end
 
   def maybe_bot?
