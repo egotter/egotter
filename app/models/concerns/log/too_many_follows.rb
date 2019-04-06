0e162fb1a600cb5aa681bdf8cb49c6f285162cb4
ts-3156 <ts_3156@yahoo.co.jp>
2019-04-06 16:18:33 +0900

    Fix global_last_too_many_follows_time

--- app/models/concerns/log/too_many_follows.rb
+++ app/models/concerns/log/too_many_follows.rb
@@ -7,14 +7,14 @@ module Concerns::Log::TooManyFollows
     def global_last_too_many_follows_time
       order(created_at: :desc).
           where(status: false).
-          find_by('error_class like ?', "%#{sanitize_sql_like('TooManyFollows')}")
+          find_by('error_class like ?', "%#{sanitize_sql_like('TooManyFollows')}")&.created_at
     end
 
     def user_last_too_many_follows_time(user_id)
       order(created_at: :desc).
           where(status: false).
           where(user_id: user_id).
-          find_by('error_class like ?', "%#{sanitize_sql_like('TooManyFollows')}")
+          find_by('error_class like ?', "%#{sanitize_sql_like('TooManyFollows')}")&.created_at
     end
   end
 
