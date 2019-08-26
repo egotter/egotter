5cd4bcbb30fb323b06b168fa2ff2ea236f8dcb88
ts-3156 <ts_3156@yahoo.co.jp>
2019-08-26 16:05:19 +0900

    SearchCount means User.all.size

--- app/workers/set_search_count_worker.rb
+++ app/workers/set_search_count_worker.rb
@@ -7,10 +7,7 @@ class SetSearchCountWorker
   end
 
   def perform
-    count =
-        SearchLog.where(created_at: 1.day.ago..Time.zone.now)
-            .where(controller: 'timelines', action: 'show')
-            .size
+    count = User.all.size
     ::Util::SearchCountCache.set(count)
   rescue => e
     logger.warn "#{e.class} #{e.message}"
