c7f9dff074549a796539a817f698fee8abdd8116
ts-3156 <ts_3156@yahoo.co.jp>
2019-08-27 16:18:27 +0900

    Add debug code to StartSendingPromptReportsWorker to measure the time of queueing

--- app/workers/start_sending_prompt_reports_worker.rb
+++ app/workers/start_sending_prompt_reports_worker.rb
@@ -5,19 +5,24 @@ class StartSendingPromptReportsWorker
   def perform(*args)
     task = PromptReportTask.start(user_ids_str: nil, deadline_str: nil)
     start_time = Time.zone.now
+    users_size = task.users.size
+    Rails.logger.warn "Start queueing #{users_size}users #{Time.zone.now}"
 
     task.users.each.with_index do |user, i|
       request = CreatePromptReportRequest.create(user_id: user.id)
 
       options = {user_id: user.id, index: i}
 
-      if task.users.size - 1 == i
+      if users_size - 1 == i
         options[:start_next_loop] = true
         options[:queueing_started_at] = start_time
       end
 
       CreatePromptReportWorker.perform_async(request.id, options)
     end
+
+    Rails.logger.warn "Finish queueing #{users_size}users #{Time.zone.now}"
+
   rescue => e
     logger.warn "#{e.class} #{e.message}"
     logger.info e.backtrace.join("\n")
