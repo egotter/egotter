513a52dce19e5a5225d5a5290cf59d1f51a462d2
ts-3156 <ts_3156@yahoo.co.jp>
2019-08-27 15:43:08 +0900

    Fix DM write permission change

--- app/models/create_prompt_report_request.rb
+++ app/models/create_prompt_report_request.rb
@@ -102,6 +102,8 @@ class CreatePromptReportRequest < ApplicationRecord
       if blocked_exception?(e)
         CreateBlockedUserWorker.perform_async(user.uid, user.screen_name)
       end
+    elsif not_allowed_to_access_or_delete_dm_exception?(e)
+      # https://twittercommunity.com/t/updates-to-app-permissions-direct-message-write-permission-change/128221
     else
       logger.warn "#{self.class}##{__method__} #{e.class} #{e.message} #{self.inspect} #{changes.inspect}"
       logger.info e.backtrace.join("\n")
@@ -190,6 +192,12 @@ class CreatePromptReportRequest < ApplicationRecord
           ex.message == 'You cannot send messages to users who are not following you.' ||
           ex.message == 'You are sending a Direct Message to users that do not follow you.' ||
           ex.message == "This request looks like it might be automated. To protect our users from spam and other malicious activity, we can't complete this action right now. Please try again later."
+    end
+  end
+
+  def not_allowed_to_access_or_delete_dm_exception?(ex)
+    if ex.class == Twitter::Error::Forbidden
+      ex.message == 'This application is not allowed to access or delete your direct messages.'
     end
   end
 
