533ca7220c56d18c3353278d4a76057ed43aeba8
ts-3156 <ts_3156@yahoo.co.jp>
2019-08-29 01:38:58 +0900

    Update SendExceptionToRollbarWorker to debug TypeError

--- app/workers/send_exception_to_rollbar_worker.rb
+++ app/workers/send_exception_to_rollbar_worker.rb
@@ -18,10 +18,13 @@ class SendExceptionToRollbarWorker
 
   def extract_exception(data)
     message = data.dig('body', 'trace', 'exception')
-    message = data.dig('body', 'trace_chain', 'exception') if message.blank?
+    begin
+      message = data.dig('body', 'trace_chain', 'exception') if message.blank?
+    rescue => e
+    end
 
     if message.blank?
-      logger.info "exception is blank #{data.inspect.truncate(100)}}"
+      logger.warn "exception is blank #{data.inspect.truncate(100)}}"
       logger.info "#{JSON.pretty_generate(data)}"
     end
 
