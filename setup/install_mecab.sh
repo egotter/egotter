afdcef52b566deecd951e65bc10429d22daeae2b
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-10 23:54:28 +0900

    Update install_mecab.sh

--- setup/install_mecab.sh
+++ setup/install_mecab.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 rpm -ivh http://packages.groonga.org/centos/groonga-release-1.2.0-1.noarch.rpm
+# or
+# rpm -ivh http://packages.groonga.org/centos/groonga-release-latest.noarch.rpm
+
 yum install -y mecab mecab-devel mecab-ipadic
+# or
+# Add --nogpgcheck
