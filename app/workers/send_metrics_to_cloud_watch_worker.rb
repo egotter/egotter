1622d4493dab900c06034e95edceae8bf8969659
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-15 12:28:12 +0900

    Send GA total users to cloudwatch

--- app/workers/send_metrics_to_cloud_watch_worker.rb
+++ app/workers/send_metrics_to_cloud_watch_worker.rb
@@ -14,29 +14,38 @@ class SendMetricsToCloudWatchWorker
 
   def perform
     client = CloudWatchClient.new
-    send_sidekiq_metrics(client)
-    send_google_analytics_metrics(client)
 
-  rescue => e
-    logger.warn "#{e.class} #{e.message}"
-    logger.info e.backtrace.join("\n")
+    begin
+      send_sidekiq_metrics(client)
+    rescue => e
+      logger.warn "#{e.class} #{e.message}"
+      logger.info e.backtrace.join("\n")
+    end
+
+    begin
+      send_google_analytics_metrics(client)
+    rescue => e
+      logger.warn "#{e.class} #{e.message}"
+      logger.info e.backtrace.join("\n")
+    end
+
   end
 
-  def datadog(values, ga_active_users, rate_limits)
-    statsd = Datadog::Statsd.new('localhost', 8125)
-
-    values.each do |name, size, latency|
-      statsd.gauge("sidekiq.queues.#{name}.size", size)
-      statsd.gauge("sidekiq.queues.#{name}.latency", latency)
-    end
-    statsd.gauge('google.analytics.active_users', ga_active_users)
-
-    rate_limits.each do |rl|
-      %i(verify_credentials friend_ids follower_ids).each do |endpoint|
-        statsd.gauge("twitter.rate_limits.#{endpoint}.remaining", rl[endpoint][:remaining], tags: ["bot_id:#{rl[:id]}"])
-      end
-    end
-  end
+  # def datadog(values, ga_active_users, rate_limits)
+  #   statsd = Datadog::Statsd.new('localhost', 8125)
+  #
+  #   values.each do |name, size, latency|
+  #     statsd.gauge("sidekiq.queues.#{name}.size", size)
+  #     statsd.gauge("sidekiq.queues.#{name}.latency", latency)
+  #   end
+  #   statsd.gauge('google.analytics.active_users', ga_active_users)
+  #
+  #   rate_limits.each do |rl|
+  #     %i(verify_credentials friend_ids follower_ids).each do |endpoint|
+  #       statsd.gauge("twitter.rate_limits.#{endpoint}.remaining", rl[endpoint][:remaining], tags: ["bot_id:#{rl[:id]}"])
+  #     end
+  #   end
+  # end
 
   private
 
@@ -53,6 +62,17 @@ class SendMetricsToCloudWatchWorker
   end
 
   def send_google_analytics_metrics(client)
+    dimensions = [{name: 'rt:total', value: 'total'}]
+    options = {namespace: "Google Analytics/#{Rails.env}", dimensions: dimensions}
+    client.put_metric_data('rt:activeUsers', GoogleAnalyticsClient.new.active_users, options)
+
+    # There are many kinds of sources.
+    # [["DESKTOP", "(none)", "(direct)", "NEW", "0"],
+    #  ["DESKTOP", "ORGANIC", "google", "NEW", "0"],
+    #  ["DESKTOP", "SOCIAL", "Twitter", "NEW", "0"],
+    #  ["MOBILE", "(none)", "(direct)", "NEW", "0"],
+    #  ["MOBILE", "ORGANIC", "google", "NEW", "0"],
+    #  ["MOBILE", "SOCIAL", "Twitter", "NEW", "0"]]
     GoogleAnalyticsClient.new.realtime_data(
         metrics: %w(rt:activeUsers),
         dimensions: %w(rt:deviceCategory rt:medium rt:source rt:userType)
