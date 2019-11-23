008bef46744d7481c2196f978b0b1a6c0929dda4
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-23 22:41:31 +0900

    Write the sender of initialization_message to CreatePromptReportLog

--- app/models/create_prompt_report_log.rb
+++ app/models/create_prompt_report_log.rb
@@ -24,7 +24,7 @@
 class CreatePromptReportLog < ApplicationRecord
   before_validation do
     if self.error_message
-      self.error_message = self.error_message.truncate(100)
+      self.error_message = self.error_message.truncate(150)
     end
   end
 
--- app/workers/create_prompt_report_initialization_message_worker.rb
+++ app/workers/create_prompt_report_initialization_message_worker.rb
@@ -10,36 +10,65 @@ class CreatePromptReportInitializationMessageWorker
   #   create_prompt_report_request_id
   def perform(user_id, options = {})
     user = User.find(user_id)
-    return unless user.authorized?
+    unless user.authorized?
+      log(options).update(status: false, error_class: CreatePromptReportRequest::Unauthorized, error_message: '')
+      return
+    end
 
-    message1 = I18n.t('dm.promptReportNotification.initialization_start')
+    ex = nil
+    message1 = I18n.t('dm.promptReportNotification.initialization_start', url: Rails.application.routes.url_helpers.settings_url(via: 'prompt_report_initialization', og_tag: 'false'))
     message2 = I18n.t('dm.promptReportNotification.search_yourself', screen_name: user.screen_name, url: timeline_url(user.screen_name))
 
-    DirectMessageRequest.new(user.api_client.twitter, User::EGOTTER_UID, message1).perform
-    DirectMessageRequest.new(User.egotter.api_client.twitter, user.uid, message2).perform
+    begin
+      DirectMessageRequest.new(user.api_client.twitter, User::EGOTTER_UID, message1).perform
+    rescue => e
+      if TemporaryDmLimitation.temporarily_locked?(e)
+      elsif TemporaryDmLimitation.you_have_blocked?(e)
+        CreateBlockedUserWorker.perform_async(user.uid, user.screen_name)
+      elsif TemporaryDmLimitation.not_allowed_to_access_or_delete_dm?(e)
+      else
+        logger.warn "#{e.class} #{e.message} #{user_id} #{options.inspect}"
+        logger.info e.backtrace.join("\n")
+      end
+
+      ex = CreatePromptReportRequest::DirectMessageNotSent.new("Initialization message1 from user #{e.class}: #{e.message}")
+      log(options).update(status: false, error_class: ex.class, error_message: ex.message)
+    end
+
+    return if ex
+
+    begin
+      DirectMessageRequest.new(User.egotter.api_client.twitter, user.uid, message2).perform
+    rescue => e
+      if TemporaryDmLimitation.temporarily_locked?(e)
+      elsif TemporaryDmLimitation.you_have_blocked?(e)
+        CreateBlockedUserWorker.perform_async(user.uid, user.screen_name)
+      elsif TemporaryDmLimitation.not_allowed_to_access_or_delete_dm?(e)
+      else
+        logger.warn "#{e.class} #{e.message} #{user_id} #{options.inspect}"
+        logger.info e.backtrace.join("\n")
+      end
+
+      ex = CreatePromptReportRequest::DirectMessageNotSent.new("Initialization message2 from egotter #{e.class}: #{e.message}")
+      log(options).update(status: false, error_class: ex.class, error_message: ex.message)
+    end
+
+    return if ex
 
     log(options).update(status: false, error_class: CreatePromptReportRequest::InitializationStarted)
 
   rescue => e
-    if TemporaryDmLimitation.temporarily_locked?(e)
-      if TemporaryDmLimitation.you_have_blocked?(e)
-        CreateBlockedUserWorker.perform_async(user.uid, user.screen_name)
-      end
-    elsif TemporaryDmLimitation.not_allowed_to_access_or_delete_dm?(e)
-    else
-      logger.warn "#{e.class} #{e.message} #{user_id} #{options.inspect}"
-      logger.info e.backtrace.join("\n")
-    end
-
-    ex = CreatePromptReportRequest::DirectMessageNotSent.new("#{e.class}: #{e.message}")
-    log(options).update(status: false, error_class: ex.class, error_message: ex.message)
+    logger.warn "#{e.class} #{e.message} #{user_id} #{options.inspect}"
+    logger.info e.backtrace.join("\n")
   end
 
+  private
+
   def timeline_url(screen_name)
-    Rails.application.routes.url_helpers.timeline_url(screen_name: screen_name, via: 'prompt_report_search_yourself')
+    Rails.application.routes.url_helpers.timeline_url(screen_name: screen_name, via: 'prompt_report_search_yourself', og_tag: 'false')
   end
 
   def log(options)
-    CreatePromptReportLog.find_by(request_id: options['create_prompt_report_request_id'])
+    CreatePromptReportLog.find_or_initialize_by(request_id: options['create_prompt_report_request_id'])
   end
 end
