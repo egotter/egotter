b11642bc9fdc53a9ff003db7f6e44ba78b66a625
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-23 16:51:30 +0900

    Set unique_key and unique_in to CreateNotFoundUserWorker and CreateForbiddenUserWorker

--- app/workers/create_not_found_user_worker.rb
+++ app/workers/create_not_found_user_worker.rb
@@ -2,7 +2,17 @@ class CreateNotFoundUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'creating_low', retry: 0, backtrace: false
 
-  def perform(screen_name)
+  def unique_key(screen_name, options = {})
+    options['uid'] ? options['uid'] : screen_name
+  end
+
+  def unique_in
+    1.minute
+  end
+
+  # options:
+  #   uid
+  def perform(screen_name, options = {})
     NotFoundUser.create!(screen_name: screen_name)
   rescue ActiveRecord::RecordNotUnique => e
     logger.info e.message.truncate(100)
