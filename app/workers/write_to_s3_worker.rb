42e8b2fadbf048c8d063b414412deece0e5b9c2f
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-25 09:01:25 +0900

    Retry WriteToS3Worker after a timeout error occurs

--- app/workers/write_to_s3_worker.rb
+++ app/workers/write_to_s3_worker.rb
@@ -6,8 +6,15 @@ class WriteToS3Worker
     10.seconds
   end
 
-  def after_timeout(*args)
-    logger.warn "Timeout #{timeout_in} #{args.inspect.truncate(100)}"
+  def after_timeout(params, options = {})
+    options = Hashie::Mash.new(options)
+    options['retry_count'] = 0 unless options['retry_count']
+    if (options['retry_count'] += 1) < 3
+      logger.warn "Retry timeout #{timeout_in} seconds #{params.inspect.truncate(50)} #{options.to_h.inspect}"
+      WriteToS3Worker.perform_in(30.seconds, params, options.to_h)
+    else
+      logger.warn "Retry exhausted: Timeout #{timeout_in} seconds #{params.inspect.truncate(50)} #{options.to_h.inspect}"
+    end
   end
 
   # params:
@@ -15,10 +22,12 @@ class WriteToS3Worker
   #   bucket
   #   key
   #   body
+  # options:
+  #   retry_count
   def perform(params, options = {})
     params['klass'].constantize.client.put_object(bucket: params['bucket'], key: params['key'], body: params['body'])
   rescue => e
-    logger.warn "#{e.class}: #{e.message.truncate(100)} #{params.inspect} #{options.inspect}"
+    logger.warn "#{e.class}: #{e.message.truncate(100)} #{params.inspect.truncate(50)} #{options.inspect}"
     logger.info e.backtrace.join("\n")
   end
 end
