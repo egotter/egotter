42233726a9fc9902f9c1fec4bc0b079aca02cd4d
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-27 06:12:57 +0900

    Print hostname in deploy.rb

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -1,8 +1,11 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/env ruby
 
 CMD="cd /var/egotter && git pull origin master && bundle install --path .bundle --without test development && RAILS_ENV=production bundle exec rake assets:precompile && sudo service puma restart"
 
-[3, 5, 8].each do |id|
-  puts %x(ssh egotter_web#{id} "#{CMD}")
+[3, 5, 7].each do |id|
+  host = "egotter_web#{id}"
+  %x(ssh #{host} "#{CMD}").each_line do |line|
+    puts "#{host} #{line}"
+  end
   sleep 30
 end
