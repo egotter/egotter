6a6f4294b8bb097524cc3fd82cc9ee79db09f773
ts-3156 <ts_3156@yahoo.co.jp>
2019-11-28 15:25:28 +0900

    Create a tag after deployment

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -1,11 +1,27 @@
 #!/usr/bin/env ruby
 
-CMD="cd /var/egotter && git pull origin master && bundle install --path .bundle --without test development && RAILS_ENV=production bundle exec rake assets:precompile && sudo service puma restart"
+CMD = [
+    'cd /var/egotter',
+    'git pull origin master',
+    'bundle install --path .bundle --without test development',
+    'RAILS_ENV=production bundle exec rake assets:precompile',
+    'sudo service puma restart',
+].join(' && ')
 
 [3, 5, 7].each do |id|
   host = "egotter_web#{id}"
+  puts "#{host} #{CMD}"
+
   %x(ssh #{host} "#{CMD}").each_line do |line|
     puts "#{host} #{line}"
   end
-  sleep 30
+
+  3.times do
+    seconds = 10
+    puts "Sleep in #{seconds} seconds."
+    sleep seconds
+  end
 end
+
+%x(git tag deploy-#{Time.now.to_i})
+%x(git push origin --tags)
