68d567152762faccd06a20ddb99665a92450f904
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-01 09:51:20 +0900

    Check too_many_searches? and too_many_requests? in SearchRequestConcern

--- app/controllers/concerns/search_request_concern.rb
+++ app/controllers/concerns/search_request_concern.rb
@@ -10,10 +10,11 @@ module Concerns::SearchRequestConcern
     before_action(only: %i(show all)) { valid_screen_name? && !not_found_screen_name? && !forbidden_screen_name? }
     before_action(only: %i(show all)) { @twitter_user = build_twitter_user_by(screen_name: params[:screen_name]) }
     before_action(only: %i(show all)) { !protected_search?(@twitter_user) && !blocked_search?(@twitter_user) }
+    before_action(only: %i(show all)) { !too_many_searches?(@twitter_user) && !too_many_requests?(@twitter_user) }
     before_action(only: %i(show all)) { twitter_user_persisted?(@twitter_user.uid) }
-    before_action(only: %i(show all)) { twitter_db_user_persisted?(@twitter_user.uid) }
+    before_action(only: %i(show all)) { twitter_db_user_persisted?(@twitter_user.uid) } # Not redirected
 
-    before_action(only: %i(show all))  do
+    before_action(only: %i(show all)) do
       if screen_name_changed?(@twitter_user)
         flash.now[:notice] = screen_name_changed_message(@twitter_user.screen_name)
         @new_screen_name = @twitter_user.screen_name
