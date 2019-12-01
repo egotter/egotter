15201b3669f9fe3a698f7c31877a059edde332de
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-01 09:37:47 +0900

    Check signed_in_user_authorized? and enough_permission_level? in SearchesController

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
   include PathsHelper
 
   before_action :reject_crawler
+  before_action { signed_in_user_authorized? }
+  before_action { enough_permission_level? }
   before_action { valid_screen_name? && !not_found_screen_name? && !forbidden_screen_name? }
   before_action { @twitter_user = build_twitter_user_by(screen_name: params[:screen_name]) }
   before_action { !protected_search?(@twitter_user) && !blocked_search?(@twitter_user) }
