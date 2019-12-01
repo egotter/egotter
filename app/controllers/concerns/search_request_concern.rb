478f702a4554d041bfe6419feb545bb6cdd2ed0b
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-01 10:07:49 +0900

    Call #too_many_searches? after calling #twitter_user_persisted?

--- app/controllers/concerns/search_request_concern.rb
+++ app/controllers/concerns/search_request_concern.rb
@@ -10,9 +10,9 @@ module Concerns::SearchRequestConcern
     before_action(only: %i(show all)) { valid_screen_name? && !not_found_screen_name? && !forbidden_screen_name? }
     before_action(only: %i(show all)) { @twitter_user = build_twitter_user_by(screen_name: params[:screen_name]) }
     before_action(only: %i(show all)) { !protected_search?(@twitter_user) && !blocked_search?(@twitter_user) }
-    before_action(only: %i(show all)) { !too_many_searches?(@twitter_user) && !too_many_requests?(@twitter_user) }
     before_action(only: %i(show all)) { twitter_user_persisted?(@twitter_user.uid) }
     before_action(only: %i(show all)) { twitter_db_user_persisted?(@twitter_user.uid) } # Not redirected
+    before_action(only: %i(show all)) { !too_many_searches?(@twitter_user) && !too_many_requests?(@twitter_user) } # Call after #twitter_user_persisted?
 
     before_action(only: %i(show all)) do
       if screen_name_changed?(@twitter_user)
