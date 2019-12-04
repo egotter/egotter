4d175000518ce1f39b94f1a886766416d01d049d
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-04 12:09:55 +0900

    Refactoring of directory/profiles

--- app/controllers/directory/profiles_controller.rb
+++ app/controllers/directory/profiles_controller.rb
@@ -5,9 +5,9 @@ module Directory
       id1 = params[:id1]
       id2 = params[:id2]
 
-      if id1.present? && id2.present?
+      if id1.to_s.match?(/\A\d{1,2}\z/) && id2.to_s.match?(/\A\d{1,2}\z/)
         @screen_names = TwitterUser.distinct.
-            where(created_at: (1.days.ago)..(Time.now)).
+            where(created_at: 1.days.ago..Time.zone.now).
             where('uid % ? = 0', 10 * id1.to_i + id2.to_i).
             pluck(:screen_name)
         render 'directory/profiles/second_layer'
