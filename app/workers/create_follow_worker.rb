c4f0027d6e39396673762bf9e065157c5d882cbd
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-08 16:47:27 +0900

    CreateFollowWorker sleeps every second to restart gracefully

--- app/workers/create_follow_worker.rb
+++ app/workers/create_follow_worker.rb
@@ -18,7 +18,7 @@ class CreateFollowWorker
 
   rescue FollowRequest::TooManyFollows => e
     logger.warn "#{e.class} Sleep for 1 hour"
-    (1.hour / 10).times { sleep 10 }
+    1.hour.times { sleep 1 }
     CreateFollowWorker.perform_async(request_id, options)
 
   rescue FollowRequest::RetryableError => e
