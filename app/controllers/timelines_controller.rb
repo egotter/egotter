1b211f65a8d42cf587e95f11e0bf240947674072
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-09 03:42:31 +0900

    Add debug print of #profile_not_found? to Timelines#show

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -14,6 +14,7 @@ class TimelinesController < ApplicationController
     @chart_builder = find_or_create_chart_builder(@twitter_user)
 
     if @twitter_user.profile_not_found?
+      logger.warn "#{controller_name}##{action_name} Profile not found in EFS and S3. #{@twitter_user.id} #{sprintf("%.3f sec", Time.zone.now - @twitter_user.created_at)}"
       flash.now[:alert] = profile_not_found_message(@twitter_user.screen_name, request.path)
     end
   end
