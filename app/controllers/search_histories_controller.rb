db3c944b432ab2ab154a31357b3ceb2f6c1475d4
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-13 03:08:09 +0900

    Reject nil from search_histories

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -1,6 +1,6 @@
 class SearchHistoriesController < ApplicationController
   def new
-    users = current_search_histories.map(&:twitter_db_user)
+    users = current_search_histories.map(&:twitter_db_user).compact
 
     # Don't specify cached: true because wrong data (duplicate records are removed?) is cached.
     render partial: 'twitter/user', collection: users, locals: {grid_class: 'col-xs-12', ad: false, via: 'search_histories'}
