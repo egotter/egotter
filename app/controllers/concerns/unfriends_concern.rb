9fdd51366ba8d3e9429f8bf5d91ff784991394fd
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-15 00:23:47 +0900

    Add new tweet text to unfriends pages

--- app/controllers/concerns/unfriends_concern.rb
+++ app/controllers/concerns/unfriends_concern.rb
@@ -26,11 +26,12 @@ module Concerns::UnfriendsConcern
     mention_names = @twitter_user.users_by(controller_name: controller_name).
         select(:screen_name).limit(3).map(&:mention_name)
 
+    @tweet_text_for_empty_users = t('unfriends.show.tweet_empty_text', url: unfriends_top_url(via: build_via('tweet_for_empty')))
+
     if mention_names.empty?
-      @tweet_text = t('unfriends.show.tweet_empty_text', url: unfriends_top_url(via: build_via('tweet_for_empty')))
+      @tweet_text = nil
     else
       names = '.' + mention_names.map { |name| honorific_name(name) }.join("\n") + "\n\n"
-
       @tweet_text = t('.tweet_text', users: names, url: unfriends_top_url(via: build_via('tweet_for_users')))
     end
 
