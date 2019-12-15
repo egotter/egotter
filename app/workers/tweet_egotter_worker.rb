d700a0a45a35d1a95185bad42c15f31818795a2d
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-16 01:26:56 +0900

    Write a url of the shared tweet to a message of slack

--- app/workers/tweet_egotter_worker.rb
+++ app/workers/tweet_egotter_worker.rb
@@ -9,16 +9,17 @@ class TweetEgotterWorker
   # options:
   def perform(request_id, options = {})
     request = TweetRequest.find(request_id)
-    request.perform!
+    tweet = request.perform!
     request.finished!
-    send_message_to_slack(request)
+    send_message_to_slack(request, tweet)
   rescue => e
     logger.warn "#{e.class} #{e.message} #{request.inspect} #{options}"
     logger.info e.backtrace.join("\n")
   end
 
-  def send_message_to_slack(request)
-    SlackClient.tweet.send_message("`#{request.id}` `#{request.user_id}` `#{request.text}`")
+  def send_message_to_slack(request, tweet)
+    tweet_url = "https://twitter.com/#{request.user.screen_name}/status/#{tweet[:id]}"
+    SlackClient.tweet.send_message("`#{request.id}` `#{request.user_id}` `#{request.text}` #{tweet_url}")
   rescue => e
     logger.warn "Sending a message to slack is failed #{e.inspect}"
   end
