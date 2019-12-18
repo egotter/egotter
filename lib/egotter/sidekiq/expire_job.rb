8687467260deac9a5069e1d6edb38ab6ad77c1c2
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-18 19:02:40 +0900

    Add test cases for ExpireJob

--- lib/egotter/sidekiq/expire_job.rb
+++ lib/egotter/sidekiq/expire_job.rb
@@ -1,25 +1,54 @@
 module Egotter
   module Sidekiq
     class ExpireJob
-      include JobCallbackUtil
-
       def call(worker, msg, queue)
         if worker.respond_to?(:expire_in)
-          options = msg['args'].dup.extract_options!
-
-          if options['enqueued_at'].blank?
-            worker.logger.warn {"Can not expire this job because enqueued_at is not found. #{msg['args'].inspect.truncate(100)}"}
-          else
-            if Time.zone.parse(options['enqueued_at']) < Time.zone.now - worker.expire_in
-              worker.logger.info {"Skip expired job. #{msg['args'].inspect.truncate(100)}"}
-
-              send_callback(worker, :after_expire, msg['args'])
-
-              return false
-            end
+          if perform_expire_check(worker, msg['args'], worker.expire_in, extract_enqueued_at(msg))
+            yield
           end
+        else
+          yield
         end
-        yield
+      end
+
+      def perform_expire_check(worker, args, expire_in, enqueued_at)
+        if enqueued_at.blank?
+          worker.logger.warn { "Can not expire this job because enqueued_at is not found. #{args.inspect.truncate(100)}" }
+          return true
+        end
+
+        if enqueued_at < Time.zone.now - expire_in
+          worker.logger.info { "Skip expired job. #{args.inspect.truncate(100)}" }
+
+          if worker.respond_to?(:after_expire)
+            worker.after_expire(*args)
+          end
+
+          false
+        else
+          true
+        end
+      end
+
+      def extract_enqueued_at(msg)
+        args = msg['args']
+        enqueued_at = nil
+
+        if args.is_a?(Array) && args.size >= 1 && args.last.is_a?(Hash)
+          enqueued_at = args.last['enqueued_at']
+        end
+        enqueued_at = msg['enqueued_at'] if enqueued_at.blank?
+        parse_time(enqueued_at)
+      end
+
+      def parse_time(value)
+        if value.to_s.match?(/\d+\.\d+/)
+          Time.zone.at(value)
+        else
+          Time.zone.parse(value)
+        end
+      rescue => e
+        nil
       end
     end
   end
--- spec/lib/egotter/sidekiq/expire_job_spec.rb
+++ spec/lib/egotter/sidekiq/expire_job_spec.rb
@@ -1,65 +1,153 @@
 require 'rails_helper'
 
 RSpec.describe Egotter::Sidekiq::ExpireJob do
-  let(:middleware_class) do
-    Class.new(Egotter::Sidekiq::ExpireJob) do
+  let(:middleware) { described_class.new }
+
+  describe '#call' do
+    let(:worker) { double('Worker') }
+    let(:enqueued_at) { 'ok' }
+    let(:msg) { {'args' => [1, 'enqueued_at' => 'time']} }
+    let(:expire_in) { 1.minute }
+
+    before { allow(middleware).to receive(:extract_enqueued_at).with(msg).and_return(enqueued_at) }
+
+    context 'The worker implements #expire_in' do
+      before do
+        allow(worker).to receive(:respond_to?).with(:expire_in).and_return(true)
+        allow(worker).to receive(:expire_in).and_return(expire_in)
+      end
+
+      it do
+        expect(middleware).to receive(:perform_expire_check).with(worker, msg['args'], expire_in, enqueued_at)
+        middleware.call(worker, msg, nil) {}
+      end
+
+      context '#perform_expire_check returns true' do
+        before { expect(middleware).to receive(:perform_expire_check).with(any_args).and_return(true) }
+        it { expect { |b| middleware.call(worker, msg, nil, &b) }.to yield_control }
+      end
+
+      context '#perform_expire_check returns false' do
+        before { expect(middleware).to receive(:perform_expire_check).with(any_args).and_return(false) }
+        it { expect { |b| middleware.call(worker, msg, nil, &b) }.not_to yield_control }
+      end
+    end
+
+    context "The worker doesn't implement #expire_in" do
+      before { allow(worker).to receive(:respond_to?).with(:expire_in).and_return(false) }
+      it do
+        expect(middleware).not_to receive(:perform_expire_check)
+        expect { |b| middleware.call(worker, msg, nil, &b) }.to yield_control
+      end
     end
   end
 
-  let(:middleware) do
-    middleware_class.new
+  describe '#perform_expire_check' do
+    let(:worker) { double('Worker') }
+    let(:args) { [1, 'enqueued_at' => enqueued_at] }
+    let(:expire_in) { 1.minute }
+    subject { middleware.perform_expire_check(worker, args, expire_in, enqueued_at) }
+
+    before { allow(worker).to receive(:logger).and_return(Logger.new(nil)) }
+
+    context 'enqueued_at is nil' do
+      let(:enqueued_at) { nil }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'enqueued_at < Time.zone.now - expire_in' do
+      let(:enqueued_at) { Time.zone.now - expire_in - 1.second }
+      it do
+        expect(worker).to receive(:after_expire).with(*args)
+        is_expected.to be_falsey
+      end
+    end
   end
 
-  let(:worker_class) do
-    Class.new do
+  describe '#extract_enqueued_at' do
+    let(:msg) { {'args' => args, 'enqueued_at' => time} }
+    subject { middleware.extract_enqueued_at(msg) }
+
+    before { allow(middleware).to receive(:parse_time).with(time).and_return('parsed') }
+
+    context 'args has enqueued_at' do
+      let(:time) { 'at' }
+      let(:args) { [1, 'enqueued_at' => time] }
+      it { is_expected.to eq('parsed') }
+    end
+
+    context "args doesn't have enqueued_at" do
+      let(:time) { 'at' }
+      let(:args) { [1] }
+      it { is_expected.to eq('parsed') }
+    end
+  end
+
+  describe '#parse_time' do
+    let(:time) { Time.zone.now }
+    subject { middleware.parse_time(value) }
+
+    context 'The value is a floating point number' do
+      let(:value) { time.to_f }
+      it { is_expected.to be_within(1.second).of(time) }
+    end
+
+    context 'The value is a string' do
+      let(:value) { time.to_s }
+      it { is_expected.to be_within(1.second).of(time) }
+    end
+
+    context 'The value is something invalid' do
+      let(:value) { nil }
+      it { is_expected.to be_nil }
+    end
+  end
+
+  context 'Server side job running' do
+    class TestExpireWorker
+      include Sidekiq::Worker
+      sidekiq_options queue: 'test', retry: 0, backtrace: false
+
+      @@count = 0
+
       def expire_in
         1.minute
       end
 
-      def after_expire(*args)
-        'hello'
-      end
-
-      def logger
-        Logger.new('/dev/null')
+      def perform(*args)
+        @@count += 1
       end
     end
-  end
 
-  let(:worker) do
-    worker_class.new
-  end
-
-  let(:enqueued_at) { Time.zone.now.to_s }
-
-  let(:msg) do
-    {'args' => [1, {'enqueued_at' => enqueued_at}]}
-  end
-
-  subject do
-    middleware.call(worker, msg, nil) {'block result'}
-  end
-
-  it 'yields' do
-    is_expected.to eq('block result')
-  end
-
-  context "Worker doesn't respond to #expire_in" do
-    before { worker_class.class_eval { remove_method :expire_in } }
-    it 'yields' do
-      is_expected.to eq('block result')
-    end
-  end
-
-  context 'With expired job' do
-    let(:enqueued_at) { 1.hour.ago.to_s }
-    it 'returns false' do
-      is_expected.to be_falsey
+    before do
+      Redis.client.flushdb
+      TestExpireWorker.clear
+      TestExpireWorker.class_variable_set(:@@count, 0)
     end
 
-    it 'calls callback' do
-      expect(worker).to receive(:after_expire).with(*msg['args'])
-      subject
+    specify "Sidekiq server doesn't run jobs that have a expired enqueued_at" do
+      expect(TestExpireWorker.jobs.size).to eq(0)
+
+      TestExpireWorker.perform_async(1, enqueued_at: Time.zone.now)
+      expect(TestExpireWorker.jobs.size).to eq(1)
+
+      TestExpireWorker.perform_async(1, enqueued_at: 1.hour.ago)
+      expect(TestExpireWorker.jobs.size).to eq(2)
+
+      TestExpireWorker.drain
+      expect(TestExpireWorker.class_variable_get(:@@count)).to eq(1)
+    end
+
+    specify 'Sidekiq server expires jobs with no enqueued_at specified' do
+      expect(TestExpireWorker.jobs.size).to eq(0)
+
+      TestExpireWorker.perform_async(1)
+      expect(TestExpireWorker.jobs.size).to eq(1)
+
+      travel 1.hour
+
+      TestExpireWorker.drain
+      expect(TestExpireWorker.class_variable_get(:@@count)).to eq(0)
     end
   end
 end
