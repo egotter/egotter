12271ce35690e249bd75115d64f1f5bcb5afe6aa
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-21 23:31:23 +0900

    Update deploy.rb

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -3,108 +3,114 @@
 require 'optparse'
 
 module Deploy
-  class Web
+  module Util
+    def green(str)
+      puts "\e[32m#{str}\e[0m"
+    end
+  end
+
+  class Server
+    include Util
+
+    attr_reader :host
+
+    def initialize(host)
+      @host = host
+    end
+
+    def current_dir
+      '/var/egotter'
+    end
+
+    def execute(*cmd)
+      green(cmd.join(' '))
+      puts system(*cmd, exception: true)
+    end
+
+    def frontend(cmd)
+      execute(cmd)
+    end
+
+    def backend(cmd)
+      execute('ssh', host, "cd #{current_dir} && #{cmd}")
+    end
+  end
+
+  class Web < Server
     CMD = [
+        'git fetch origin',
         'git pull origin master',
         'bundle check || bundle install --path .bundle --without test development',
         'RAILS_ENV=production bundle exec rake assets:precompile',
         'sudo service puma restart',
     ]
 
-    attr_reader :hosts
-
-    def initialize(hosts)
-      @hosts = hosts.split(',')
+    def before_deploy
+      backend('echo "ssh connection test"')
     end
 
-    def current_dir
-      '/var/egotter'
+    def deploy
+      before_deploy
+
+      CMD.each do |cmd|
+        backend(cmd)
+      end
+
+      after_deploy
     end
 
-    def run
-      hosts.each do |host|
-        cmd = "ssh -q #{host} exit"
-        puts "\e[32m#{cmd}\e[0m" # Green
-        puts system(cmd, exception: true)
-      end
-
-      hosts.each do |host|
-        CMD.each do |cmd|
-          puts "\e[32m#{host} #{cmd}\e[0m" # Green
-          puts system('ssh', host, "cd #{current_dir} && #{cmd}", exception: true)
-        end
-
-        unless hosts.last == host
-          3.times do
-            seconds = 10
-            puts "Sleep in #{seconds} seconds."
-            sleep seconds
-          end
-        end
-      end
-
-      %x(git tag deploy-web-all-#{Time.now.to_i})
-      %x(git push origin --tags)
+    def after_deploy
+      frontend("git tag deploy-web-all-#{Time.now.to_i}")
+      frontend('git push origin --tags')
     end
   end
 
-  class Sidekiq
+  class Sidekiq < Server
     CMD = [
+        'git fetch origin',
         'git pull origin master',
         'bundle check || bundle install --path .bundle --without test development',
-        ['sudo service sidekiq_misc status'          , 'sudo service sidekiq_misc restart'],
-        ['sudo service sidekiq_prompt_reports status', 'sudo service sidekiq_prompt_reports restart'],
-        ['sudo service sidekiq status'               , 'sudo service sidekiq restart'],
-        ['sudo service sidekiq_import status'        , 'sudo service sidekiq_import restart'],
-        ['sudo service sidekiq_follow status'        , 'sudo service sidekiq_follow force-restart'],
-        ['sudo service sidekiq_unfollow status'      , 'sudo service sidekiq_unfollow restart'],
+        'sudo service sidekiq_misc status           && sudo service sidekiq_misc restart || :',
+        'sudo service sidekiq_prompt_reports status && sudo service sidekiq_prompt_reports restart || :',
+        'sudo service sidekiq status                && sudo service sidekiq restart || :',
+        'sudo service sidekiq_import status         && sudo service sidekiq_import restart || :',
+        'sudo service sidekiq_follow status         && sudo service sidekiq_follow force-restart || :',
+        'sudo service sidekiq_unfollow status       && sudo service sidekiq_unfollow restart || :',
     ]
 
-    attr_reader :hosts
-
-    def initialize(hosts)
-      @hosts = hosts.split(',')
+    def before_deploy
+      backend('echo "ssh connection test"')
     end
 
-    def current_dir
-      '/var/egotter'
+    def deploy
+      before_deploy
+
+      CMD.each do |cmd|
+        backend(cmd)
+      end
+
+      after_deploy
     end
 
-    def run
-      hosts.each do |host|
-        cmd = "ssh -q #{host} exit"
-        puts "\e[32m#{cmd}\e[0m" # Green
-        puts system(cmd, exception: true)
-      end
-
-      hosts.each do |host|
-        CMD.each do |cmd|
-          if cmd.class == Array
-            cmd.each do |c|
-              puts "\e[32m#{host} #{c}\e[0m" # Green
-              break unless (system('ssh', host, "cd #{current_dir} && #{c}", exception: true) rescue false)
-            end
-          else
-            puts "\e[32m#{host} #{cmd}\e[0m" # Green
-            puts system('ssh', host, "cd #{current_dir} && #{cmd}", exception: true)
-          end
-        end
-      end
-
-      %x(git tag deploy-sidekiq-#{Time.now.to_i})
-      %x(git push origin --tags)
+    def after_deploy
+      frontend("git tag deploy-sidekiq-all-#{Time.now.to_i}")
+      frontend('git push origin --tags')
     end
   end
 end
 
-STDOUT.sync = true
+if __FILE__ == $0
+  STDOUT.sync = true
 
-params = ARGV.getopts('r:', 'role:', 'hosts:')
+  params = ARGV.getopts('r:', 'role:', 'hosts:')
+  hosts = params['hosts'].split(',')
 
-if params['role'] == 'web'
-  Deploy::Web.new(params['hosts']).run
-elsif params['role'] == 'sidekiq'
-  Deploy::Sidekiq.new(params['hosts']).run
-else
-  puts "Invalid #{params.inspect}"
+  case params['role']
+  when 'web'
+    hosts.each { |host| Deploy::Web.new(host).deploy }
+  when 'sidekiq'
+    hosts.each { |host| Deploy::Sidekiq.new(host).deploy }
+  else
+    puts "Invalid #{params.inspect}"
+  end
 end
