5e7325331602d362e69045e4344201750dde9c42
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-23 22:21:50 +0900

    Cleanup ./bin/*

--- bin/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CMD="cd /var/egotter && git pull origin master && bundle install --path .bundle --without test development && RAILS_ENV=production bundle exec rake assets:precompile && sudo service puma restart"
-
-for i in $(seq 3 8); do
-  ssh egotter_web${i} "$CMD"
-
-  sleep 30
-done
--- bin/tail_warn.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-tail -f log/sidekiq_all.log log/development.log | grep -i warn
