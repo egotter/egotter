e8fd087ccd15287be28b6325c0608ea91ba45d09
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-25 14:24:55 +0900

    Implement Launcher module

--- bin/aws.rb
+++ bin/aws.rb
@@ -59,13 +59,13 @@ if __FILE__ == $0
 
   if params['h'] || params['help']
     puts <<~'TEXT'
-        Usage:
-          aws.rb --launch --role web
-          aws.rb --launch --role web --rotate
-          aws.rb --launch --role sidekiq --instance-type m5.large
-          aws.rb --list
-          aws.rb --terminate --role web
-          aws.rb --terminate --role sidekiq --instance-id i-0000
+      Usage:
+        aws.rb --launch --role web
+        aws.rb --launch --role web --rotate
+        aws.rb --launch --role sidekiq --instance-type m5.large
+        aws.rb --list
+        aws.rb --terminate --role web
+        aws.rb --terminate --role sidekiq --instance-id i-0000
     TEXT
 
     exit
@@ -79,57 +79,129 @@ if __FILE__ == $0
   Uninstall = ::Egotter::Uninstall
   Instance = ::Egotter::Aws::Instance
 
-  if params['launch']
-    role = params['role']
+  module Launcher
+    def build(params)
+      role = params['role']
 
-    if role == 'web'
-      az = target_group.availability_zone_with_fewest_instances
-      server = Launch::Web.new(Launch::Params.new(params.merge('availability-zone' => az))).launch
-      append_to_ssh_config(server.id, server.host, server.public_ip)
-      Install::Web.new(server.id).install
-
-      target_group.register(server.id)
-
-      if params['rotate']
-        instance = target_group.oldest_instance
-        if instance && target_group.deregister(instance.id)
-          instance.terminate
-        end
+      if role == 'web'
+        Web.new(params)
+      elsif role == 'sidekiq'
+        Sidekiq.new(params)
+      else
+        raise "Invalid role #{role}"
       end
-    elsif role == 'sidekiq'
-      az = 'ap-northeast-1b'
-      server = Launch::Sidekiq.new(Launch::Params.new(params.merge('availability-zone' => az))).launch
-      append_to_ssh_config(server.id, server.host, server.public_ip)
-      Install::Sidekiq.new(server.id).install
-    else
-      raise "Invalid role #{role}"
     end
 
-    CloudWatchClient::Dashboard.new('egotter-linux-system').
-        append_cpu_utilization(role, server.id).
-        append_memory_utilization(role, server.id).
-        append_cpu_credit_balance(role, server.id).
-        append_disk_space_utilization(role, server.id).
-        update
+    module_function :build
 
-    %x(git tag deploy-#{server.name})
+    class Base
+      def initialize
+        @role = nil
+        @launched = nil
+        @terminated = nil
+      end
+
+      def launch
+        after_launch if @launched
+        after_terminate if @terminated
+      end
+
+      def after_launch
+        CloudWatchClient::Dashboard.new('egotter-linux-system').
+            append_cpu_utilization(@role, @launched.id).
+            append_memory_utilization(@role, @launched.id).
+            append_cpu_credit_balance(@role, @launched.id).
+            append_disk_space_utilization(@role, @launched.id).
+            update
+      end
+
+      def after_terminate
+        CloudWatchClient::Dashboard.new('egotter-linux-system').
+            remove_cpu_utilization(@role, @terminated.id).
+            remove_memory_utilization(@role, @terminated.id).
+            remove_cpu_credit_balance(@role, @terminated.id).
+            remove_disk_space_utilization(@role, @terminated.id).
+            update
+      end
+    end
+
+    class Web < Base
+      def initialize(params)
+        super()
+        @params = params
+        @role = params['role']
+
+        target_group_arn = params['target-group'] || ENV['AWS_TARGET_GROUP']
+        @target_group = ::Egotter::Aws::TargetGroup.new(target_group_arn)
+      end
+
+      def launch
+        az = @target_group.availability_zone_with_fewest_instances
+        server = Launch::Web.new(Launch::Params.new(@params.merge('availability-zone' => az))).launch
+        append_to_ssh_config(server.id, server.host, server.public_ip)
+        Install::Web.new(server.id).install
+
+        @target_group.register(server.id)
+        @launched = server
+
+        if @params['rotate']
+          instance = @target_group.oldest_instance
+          if instance && @target_group.deregister(instance.id)
+            instance.terminate
+            @terminated = instance
+          end
+        end
+
+        super
+
+        @launched
+      end
+    end
+
+    class Sidekiq < Base
+      def initialize(params)
+        super()
+        @params = params
+        @role = params['role']
+      end
+
+      def launch
+        az = 'ap-northeast-1b'
+        server = Launch::Sidekiq.new(Launch::Params.new(params.merge('availability-zone' => az))).launch
+        append_to_ssh_config(server.id, server.host, server.public_ip)
+        Install::Sidekiq.new(server.id).install
+
+        @launched = server
+
+        super
+
+        @launched
+      end
+    end
+  end
+
+  if params['launch']
+    launcher = Launcher.build(params)
+    instance = launcher.launch
+
+    %x(git tag deploy-#{instance.name})
     %x(git push origin --tags)
   elsif params['terminate']
     role = params['role']
-    terminated = false
+    terminated = nil
 
     if role == 'web'
       instance = target_group.oldest_instance
       if instance && target_group.deregister(instance.id)
         instance.terminate
-        terminated = true
+        terminated = instance
       end
     elsif role == 'sidekiq'
       instance = Instance.retrieve_by(id: params['instance-id'], name: params['instance-name'])
       if instance
         Uninstall::Sidekiq.new(instance.id).uninstall
         instance.terminate
-        terminated = true
+        terminated = instance
       end
     else
       raise "Invalid role #{role}"
@@ -137,10 +209,10 @@ if __FILE__ == $0
 
     if terminated
       CloudWatchClient::Dashboard.new('egotter-linux-system').
-          remove_cpu_utilization(role, instance.id).
-          remove_memory_utilization(role, instance.id).
-          remove_cpu_credit_balance(role, instance.id).
-          remove_disk_space_utilization(role, instance.id).
+          remove_cpu_utilization(role, terminated.id).
+          remove_memory_utilization(role, terminated.id).
+          remove_cpu_credit_balance(role, terminated.id).
+          remove_disk_space_utilization(role, terminated.id).
           update
     end
   elsif params['list']
