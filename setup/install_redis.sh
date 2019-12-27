38564f96b6ead6363234c6a650948c972d52d850
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-28 03:48:28 +0900

    Start redis-server on a separate instance

---
+++ setup/install_redis.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Amazon Linux 2
+
+yum update -y
+amazon-linux-extras install -y epel
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum --enablerepo=remi install -y redis
+
+cat << EOS >>/etc/security/limits.conf
+root soft nofile 65536
+root hard nofile 65536
+* soft nofile 65536
+* hard nofile 65536
+EOS
+
+echo "net.ipv4.tcp_max_syn_backlog = 512" >>/etc/sysctl.conf
+echo "net.core.somaxconn = 512" >>/etc/sysctl.conf
+echo "vm.overcommit_memory = 1" >>/etc/sysctl.conf
+sysctl -p
+
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >>/etc/rc.local
+
+sed -i -e 's/LimitNOFILE=10240/LimitNOFILE=65536/g' /etc/systemd/system/redis.service.d/limit.conf
+
+[ ! -f "/etc/redis.conf.bak" ] && cp /etc/redis.conf /etc/redis.conf.bak
+\cp -f ./setup/etc/redis.conf /etc
+
+systemctl daemon-reload
+systemctl enable redis.service
+systemctl restart redis
+
+# redis-cli
+# > replicaof xxx.xxx.xxx.xxx 6379
+# > replicaof no one
+
+dd if=/dev/zero of=/swapfile bs=1M count=1024
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile swap swap defaults 0 0' >>/etc/fstab
+
+# Install datadog
