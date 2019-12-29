486b3b95e6fcacae8893ff345ab2eb3ed1b650b0
ts-3156 <ts_3156@yahoo.co.jp>
2019-12-29 19:59:05 +0900

    Add deploy/bin/egotter.rb

---
+++ deploy/bin/egotter.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+require 'dotenv/load'
+
+require 'optparse'
+
+Dir['./deploy/taskbooks/*.rb'].each { |file| require file }
+
+STDOUT.sync = true
+
+params = ARGV.getopts(
+    'h',
+    'help',
+    'deploy',
+    'role:',
+    'hosts:',
+    'git-tag',
+    'launch-template:',
+    'name-tag:',
+    'security-group:',
+    'subnet:',
+    'target-group:',
+    'availability-zone:',
+    'instance-type:',
+    'instance-id:',
+    'instance-name:',
+    'delim:',
+    'state:',
+    'launch',
+    'count:',
+    'terminate',
+    'sync',
+    'rotate',
+    'list',
+    'debug',
+)
+
+if params['h'] || params['help']
+  puts <<~'TEXT'
+    Usage:
+      egotter.rb --deploy --role web --hosts aaa,bbb,ccc
+      egotter.rb --deploy --role web --hosts aaa,bbb,ccc --git-tag
+      egotter.rb --deploy --role sidekiq --hosts aaa,bbb,ccc
+      egotter.rb --deploy --role sidekiq --hosts aaa,bbb,ccc --git-tag
+
+      egotter.rb --launch --role web
+      egotter.rb --launch --role web --rotate
+      egotter.rb --launch --role web --rotate --count 3
+      egotter.rb --launch --role sidekiq --instance-type m5.large
+
+      egotter.rb --sync --role web --instance-id i-0000
+      egotter.rb --sync --role sidekiq --instance-id i-0000
+
+      egotter.rb --list --role web
+      egotter.rb --list --role sidekiq
+
+      egotter.rb --terminate --role web
+      egotter.rb --terminate --role web --count 3
+      egotter.rb --terminate --role sidekiq --instance-id i-0000
+  TEXT
+
+  exit
+end
+
+if params['deploy']
+  tasks = TaskBooks::DeployTask.build(params)
+  tasks.each(&:run)
+
+  if params['git-tag']
+    system("git tag deploy-#{params['role']}-all-#{Time.now.to_i}")
+    system('git push origin --tags')
+  end
+else
+  task = Taskbooks::AwsTask.build(params)
+  task.run
+
+  if %i(launch terminate sync).include?(task.kind)
+    %x(git tag #{task.kind}-#{task.instance.name})
+    %x(git push origin --tags)
+  end
+end
