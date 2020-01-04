2a3c213ec541cc25f538e6814eb5b5bfc51f1f93
ts-3156 <ts_3156@yahoo.co.jp>
2020-01-05 03:05:01 +0900

    Update secret_mode_detected message

--- app/assets/javascripts/detectors.js
+++ app/assets/javascripts/detectors.js
@@ -10,11 +10,27 @@ Detectors.secretMode = function (detected) {
 
       if (quota < 120000000) {
         console.log('Incognito');
-        detected();
+        detected(quota);
       } else {
         console.log('Not Incognito')
       }
     });
+  } else {
+    console.log('Can not detect');
+  }
+};
+
+Detectors.secretMode_old = function () {
+  var fs = window.RequestFileSystem || window.webkitRequestFileSystem;
+  if (fs) {
+    fs(window.TEMPORARY,
+        100,
+        function (fs) {
+          console.log('Not Incognito')
+        },
+        function (fe) {
+          console.log('Incognito');
+        });
   } else {
     console.log('Can not detect');
   }
