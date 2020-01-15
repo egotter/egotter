73948b00c17d6dd9abc24182e3f5a9102532e839
ts-3156 <ts_3156@yahoo.co.jp>
2020-01-15 17:03:59 +0900

    Fix ArgumentError in JobCallbackUtil

--- lib/egotter/sidekiq/job_callback_util.rb
+++ lib/egotter/sidekiq/job_callback_util.rb
@@ -5,15 +5,15 @@ module Egotter
         if worker.respond_to?(callback_name)
           parameters = worker.method(callback_name).parameters
 
-          if parameters.empty?
-            worker.send(callback_name)
-          elsif parameters.size == args.size
-            worker.send(callback_name, *args)
-          elsif parameters.size == 1 && parameters[0][0] == :rest
-              worker.send(callback_name, args)
-          else
+          begin
+            if parameters.empty?
+              worker.send(callback_name)
+            else
+              worker.send(callback_name, *args)
+            end
+          rescue ArgumentError => e
             message = "The number of parameters of the callback method (#{parameters.size}) is not the same as the number of arguments (#{args.size})"
-            raise ArgumentError.new("#{self.class}:#{worker.class} #{message}")
+            raise ArgumentError.new("#{self.class}:#{worker.class} #{message} callback_name=#{callback_name} args=#{args.inspect} parameters=#{parameters.inspect}")
           end
         end
       end
