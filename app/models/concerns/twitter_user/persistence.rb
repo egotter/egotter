f7bfbe610ba24c20fa94ee0ffec346ffd1ef4cd4
ts-3156 <ts_3156@yahoo.co.jp>
2020-01-15 14:37:28 +0900

    Update benchmark code in Persistence

--- app/models/concerns/twitter_user/persistence.rb
+++ app/models/concerns/twitter_user/persistence.rb
@@ -9,38 +9,38 @@ module Concerns::TwitterUser::Persistence
   included do
     # This method is processed on `after_commit` to avoid long transaction.
     after_commit(on: :create) do
-      Util.bm("Efs::TwitterUser.import_from! #{id}") do
+      Util.bm('Efs::TwitterUser.import_from!', id, uid) do
         Efs::TwitterUser.import_from!(id, uid, screen_name, raw_attrs_text, @friend_uids, @follower_uids)
       end
 
-      Util.bm("S3::Friendship.import_from! #{id}") do
+      Util.bm('S3::Friendship.import_from!', id, uid) do
         S3::Friendship.import_from!(id, uid, screen_name, @friend_uids, async: true)
       end
 
-      Util.bm("S3::Followership.import_from! #{id}") do
+      Util.bm('S3::Followership.import_from!', id, uid) do
         S3::Followership.import_from!(id, uid, screen_name, @follower_uids, async: true)
       end
 
-      Util.bm("S3::Profile.import_from! #{id}") do
+      Util.bm('S3::Profile.import_from!', id, uid) do
         S3::Profile.import_from!(id, uid, screen_name, raw_attrs_text, async: true)
       end
 
-      Util.bm("S3::StatusTweet.import_from! #{id} #{uid}") do
+      Util.bm('S3::StatusTweet.import_from!', id, uid) do
         status_tweets = statuses.select(&:new_record?).map { |t| t.slice(:uid, :screen_name, :raw_attrs_text) }
         S3::StatusTweet.import_from!(uid, screen_name, status_tweets)
       end
 
-      Util.bm("S3::FavoriteTweet.import_from! #{id} #{uid}") do
+      Util.bm('S3::FavoriteTweet.import_from!', id, uid) do
         favorite_tweets = favorites.select(&:new_record?).map { |t| t.slice(:uid, :screen_name, :raw_attrs_text) }
         S3::FavoriteTweet.import_from!(uid, screen_name, favorite_tweets)
       end
 
-      Util.bm("S3::MentionTweet.import_from! #{id} #{uid}") do
+      Util.bm('S3::MentionTweet.import_from!', id, uid) do
         mention_tweets = mentions.select(&:new_record?).map { |t| t.slice(:uid, :screen_name, :raw_attrs_text) }
         S3::MentionTweet.import_from!(uid, screen_name, mention_tweets)
       end
 
-      Util.bm("Efs::FavoriteTweet.import_from! #{id} #{uid}") do
+      Util.bm('Efs::FavoriteTweet.import_from!', id, uid) do
         favorite_tweets = favorites.select(&:new_record?).map { |t| t.slice(:uid, :screen_name, :raw_attrs_text) }
         Efs::FavoriteTweet.import_from!(uid, screen_name, favorite_tweets)
       end
@@ -59,8 +59,8 @@ module Concerns::TwitterUser::Persistence
   module Util
     module_function
 
-    def bm(message, &block)
-      ApplicationRecord.benchmark("Benchmark Persistence #{message}", level: :info, &block)
+    def bm(message, id, uid, &block)
+      ApplicationRecord.benchmark("Benchmark Persistence #{id} #{message} #{uid}", level: :info, &block)
     end
   end
 end
