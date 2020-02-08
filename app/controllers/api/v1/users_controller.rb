06fc43dbfba711f185aab4e1d2e27cd480d598ab
ts-3156 <ts_3156@yahoo.co.jp>
2020-02-09 02:46:39 +0900

    Enqueue CreatePromptReportWorker in UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,12 @@ module Api
       def update_instance_id
         if params[:uid].present? && params[:access_token].present? && params[:instance_id].present? &&
             (user = User.find_by(uid: params[:uid], token: params[:access_token]))
+
           user.credential_token.update!(instance_id: params[:instance_id])
+
+          request = CreatePromptReportRequest.new(user_id: user.id)
+          CreatePromptReportWorker.perform_async(request.id, requested_by: 'android_app')
+
           render json: {uid: user.uid, found: true}
         else
           render json: {found: false}, status: :not_found
