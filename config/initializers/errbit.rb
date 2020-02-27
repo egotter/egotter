f7862f6278022e535642864db63dd79f0060ade6
ts-3156 <ts_3156@yahoo.co.jp>
2020-02-28 04:23:33 +0900

    Create a new errbit instance

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,6 +2,7 @@ Airbrake.configure do |config|
   config.host = ENV['AIRBRAKE_HOST']
   config.project_id = 1 # required, but any positive integer works
   config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+  config.performance_stats = false
 
   config.environment = Rails.env
   config.ignore_environments = %w(test)
