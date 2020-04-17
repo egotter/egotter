6c796fcc50a9b517227fb0b27cca25c89f8f0799
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-17 22:58:37 +0900

    The timeout of UpdateAudienceInsightWorker is 20 seconds (was 10 seconds)

--- app/workers/update_audience_insight_worker.rb
+++ app/workers/update_audience_insight_worker.rb
@@ -11,11 +11,11 @@ class UpdateAudienceInsightWorker
   end
 
   def timeout_in
-    10.seconds
+    20.seconds
   end
 
   def after_timeout(uid, options = {})
-    logger.info "Timeout #{timeout_in} #{uid} #{options}"
+    logger.warn "Timeout #{timeout_in} #{uid} #{options}"
     UpdateAudienceInsightWorker.perform_in(retry_in, uid, options)
   end
 
@@ -31,20 +31,6 @@ class UpdateAudienceInsightWorker
   #   location
   #   twitter_user_id
   def perform(uid, options = {})
-    Timeout.timeout(self.timeout_in.seconds) do
-      do_perform(uid)
-    end
-  rescue Timeout::Error => e
-    logger.warn "#{e.class}: #{e.message} #{uid} #{options}"
-    logger.info e.backtrace.join("\n")
-  rescue ActiveRecord::RecordNotUnique => e
-    logger.info "#{e.class}: #{e.message} #{uid} #{options}"
-  rescue => e
-    logger.warn "#{e.class}: #{e.message} #{uid} #{options}"
-    logger.info e.backtrace.join("\n")
-  end
-
-  def do_perform(uid, dry_run: false)
     insight = AudienceInsight.find_or_initialize_by(uid: uid)
     return if insight.fresh?
 
@@ -54,6 +40,12 @@ class UpdateAudienceInsightWorker
       insight.send("#{chart_name}_text=", chart_builder.send(chart_name).to_json)
     end
 
-    insight.save! unless dry_run
+    insight.save!
+
+  rescue ActiveRecord::RecordNotUnique => e
+    logger.info "#{e.class}: #{e.message} #{uid} #{options}"
+  rescue => e
+    logger.warn "#{e.class}: #{e.message} #{uid} #{options}"
+    logger.info e.backtrace.join("\n")
   end
 end
