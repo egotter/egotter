1597acb0e5c0f11f4bc8f071931dd4aa573f8ef9
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-27 03:21:20 +0900

    Update deploy script

--- deploy/tasks/uninstall_task.rb
+++ deploy/tasks/uninstall_task.rb
@@ -63,7 +63,7 @@ module Tasks
             'sudo stop sidekiq && tail -n 6 log/sidekiq.log || :',
             'sudo stop sidekiq_import && tail -n 6 log/sidekiq_import.log || :',
             'sudo stop sidekiq_misc && tail -n 6 log/sidekiq_misc.log || :',
-            'sudo stop sidekiq_prompt_reports && tail -n 6 log/sidekiq_prompt_reports.log || :',
+            'sudo stop sidekiq_prompt_reports_workers && tail -n 6 log/sidekiq_prompt_reports.log || :',
         ].each do |cmd|
           run_command(cmd)
         end
