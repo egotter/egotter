3d3dfeec4c7d055ab2110149dfbef6624f0bfedc
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-28 20:32:52 +0900

    Load jquery3 from separated cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require jquery_ujs
 //= require_tree .
