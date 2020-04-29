378465c4723205acdb59980485afc5faed7eed63
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-29 17:09:18 +0900

    Update debug code in #remaining_search_count

--- app/models/search_count_limitation.rb
+++ app/models/search_count_limitation.rb
@@ -30,7 +30,7 @@ class SearchCountLimitation
     def remaining_search_count(user: nil, session_id: nil)
       [0, max_search_count(user) - current_search_count(user: user, session_id: session_id)].max
     rescue => e
-      Rails.logger.warn "##{__method__} Maybe invalid session_id inspect=#{session_id.inspect}"
+      Rails.logger.warn "##{__method__} Maybe invalid session_id class=#{session_id.class} inspect=#{session_id.inspect}"
       raise
     end
     alias remaining_count remaining_search_count
