a3e53769ce71208967a5939546901da9573f4e71
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-29 18:54:53 +0900

    Dont use session.id

--- app/controllers/concerns/sessions_concern.rb
+++ app/controllers/concerns/sessions_concern.rb
@@ -11,15 +11,14 @@ module Concerns::SessionsConcern
       return -1
     end
 
-    if session[:egotter_visit_id].nil? || session[:egotter_visit_id].to_s == '-1'
-      session[:egotter_visit_id] = session.id.nil? ? '-1' : session.id
-    end
-
-    if session[:egotter_visit_id] == '-1' || session[:egotter_visit_id].class == Hash
-      digest = Digest::MD5.hexdigest("#{Time.zone.now.to_i + rand(1000)}")
-      session[:egotter_visit_id] = "digest-#{digest}"
+    if session[:egotter_visit_id].class != String
+      session[:egotter_visit_id] = gen_egotter_visit_id
     end
 
     session[:egotter_visit_id]
+  end
+
+  def gen_egotter_visit_id
+    "sess-v2-#{Digest::MD5.hexdigest("#{Time.zone.now.to_i + rand(10000)}")}"
   end
 end
