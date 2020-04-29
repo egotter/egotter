690f214046785a5dcfd41d42b4ec174ea2e9aae6
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-29 22:32:30 +0900

    Refactor support page

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,12 +1,7 @@
 class Faq
-  def self.init
-    @@faq ||= JSON.parse(File.read(Rails.configuration.x.constants['faq_path'])).map { |b| Hashie::Mash.new(b) }
-    @@faq.each.with_index { |f, i| f[:i] = i }
-    raise 'create Faq' if @@faq.empty?
-  end
-
-  def self.list
-    init
-    @@faq
+  class << self
+    def list
+      @faq ||= JSON.parse(File.read(Rails.configuration.x.constants['faq_path'])).map { |b| Hashie::Mash.new(b) }
+    end
   end
 end
