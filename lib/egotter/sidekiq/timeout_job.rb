72462d7991426fdd8621a1202d2fa0101a4c82db
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-02 02:02:40 +0900

    wip

--- lib/egotter/sidekiq/timeout_job.rb
+++ lib/egotter/sidekiq/timeout_job.rb
@@ -5,9 +5,21 @@ module Egotter
 
       def call(worker, msg, queue, &block)
         if worker.respond_to?(:timeout_in)
+          define_timeout_related_methods(worker, msg)
           yield_with_timeout(worker, msg['args'], worker.timeout_in, &block)
         else
           yield
+        end
+      end
+
+      def define_timeout_related_methods(worker, msg)
+        timeout_started_at = Time.zone.now
+        worker.define_singleton_method(:timeout?) do
+          Time.zone.now - timeout_started_at > worker.timeout_in
+        end
+
+        worker.define_singleton_method(:timeout!) do
+          raise 'Time is up!'
         end
       end
 
@@ -22,6 +34,17 @@ module Egotter
         perform_callback(worker, :after_timeout, args)
 
         nil
+      rescue => e
+        if e.message == 'Time is up!'
+          worker.logger.info "#{e.class}: #{e.message} #{timeout_in} #{args.inspect.truncate(100)}"
+          worker.logger.info e.backtrace.join("\n")
+
+          perform_callback(worker, :after_timeout, args)
+
+          nil
+        else
+          raise
+        end
       end
     end
   end
