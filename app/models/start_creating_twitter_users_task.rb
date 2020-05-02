67fa14a4eba43e79d486d9e20589959b0ef86b43
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-02 11:53:22 +0900

    StartCreatingTwitterUsersTask#initialize receives user_ids or start_date

--- app/models/start_creating_twitter_users_task.rb
+++ app/models/start_creating_twitter_users_task.rb
@@ -1,16 +1,21 @@
 # Perform a request and log an error
 class StartCreatingTwitterUsersTask
 
-  def initialize
+  def initialize(user_ids: nil, start_date: nil)
+    if user_ids.present?
+      @user_ids = user_ids
+    else
+      start_date = CreatePeriodicReportRequest::PERIOD_DURATION.ago unless start_date
+      @user_ids = AccessDay.where('created_at > ?', start_date).select(:user_id).distinct.map(&:user_id)
+    end
   end
 
   def start!
     last_request = CreateTwitterUserRequest.order(created_at: :desc).first
     last_request = CreateTwitterUserRequest.new(created_at: 1.second.ago) unless last_request
 
-    user_ids = AccessDay.where('created_at > ?', CreatePeriodicReportRequest::PERIOD_DURATION.ago).select(:user_id).distinct.map(&:user_id)
     requests = []
-    User.where(id: user_ids).find_each do |user|
+    User.where(id: @user_ids).find_each do |user|
       requests << CreateTwitterUserRequest.new(
           requested_by: 'batch',
           user_id: user.id,
--- app/models/start_sending_periodic_reports_task.rb
+++ app/models/start_sending_periodic_reports_task.rb
@@ -1,15 +1,20 @@
 # Perform a request and log an error
 class StartSendingPeriodicReportsTask
 
-  def initialize
+  def initialize(user_ids: nil, start_date: nil)
+    if user_ids.present?
+      @user_ids = user_ids
+    else
+      start_date = CreatePeriodicReportRequest::PERIOD_DURATION.ago unless start_date
+      @user_ids = AccessDay.where('created_at > ?', start_date).select(:user_id).distinct.map(&:user_id)
+    end
   end
 
   def start!
     last_request = CreatePeriodicReportRequest.order(created_at: :desc).first
     last_request = CreatePeriodicReportRequest.new(created_at: 1.second.ago) unless last_request
 
-    user_ids = AccessDay.where('created_at > ?', CreatePeriodicReportRequest::PERIOD_DURATION.ago).select(:user_id).distinct.map(&:user_id)
-    requests = user_ids.map { |user_id| CreatePeriodicReportRequest.new(user_id: user_id) }
+    requests = @user_ids.map { |user_id| CreatePeriodicReportRequest.new(user_id: user_id) }
     CreatePeriodicReportRequest.import requests, validate: false
 
     CreatePeriodicReportRequest.where('created_at > ?', last_request.created_at).find_each do |request|
