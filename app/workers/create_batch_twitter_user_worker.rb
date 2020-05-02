4746734a86b68be0577343534d1bade1bdd8cc40
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-02 09:05:21 +0900

    Implement CreateBatchTwitterUserWorker#expire_in

--- app/workers/create_batch_twitter_user_worker.rb
+++ app/workers/create_batch_twitter_user_worker.rb
@@ -2,4 +2,8 @@
 class CreateBatchTwitterUserWorker < CreateTwitterUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: self, retry: 0, backtrace: false
+
+  def expire_in
+    1.day
+  end
 end
