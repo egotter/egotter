0a5c78fb4b5eb9ce4b816e4baa08a82a699eb531
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-02 20:58:46 +0900

    wip

--- lib/egotter/sidekiq/timeout_job.rb
+++ lib/egotter/sidekiq/timeout_job.rb
@@ -5,17 +5,19 @@ module Egotter
 
       def call(worker, msg, queue, &block)
         if worker.respond_to?(:timeout_in)
-          define_timeout_related_methods(worker, msg)
           yield_with_timeout(worker, msg['args'], worker.timeout_in, &block)
+        elsif worker.respond_to?(:timeout_in_v2)
+          define_timeout_related_methods(worker, msg)
+          yield_with_timeout_v2(worker, msg['args'], worker.timeout_in, &block)
         else
           yield
         end
       end
 
       def define_timeout_related_methods(worker, msg)
-        timeout_started_at = Time.zone.now
+        timeout_started_at = Time.now
         worker.define_singleton_method(:timeout?) do
-          Time.zone.now - timeout_started_at > worker.timeout_in
+          Time.now - timeout_started_at > worker.timeout_in
         end
 
         worker.define_singleton_method(:timeout!) do
@@ -24,6 +26,19 @@ module Egotter
       end
 
       def yield_with_timeout(worker, args, timeout_in, &block)
+        Timeout.timeout(timeout_in) do
+          yield
+        end
+      rescue Timeout::Error => e
+        worker.logger.info "#{e.class}: #{e.message} #{timeout_in} #{args.inspect.truncate(100)}"
+        worker.logger.info e.backtrace.join("\n")
+
+        perform_callback(worker, :after_timeout, args)
+
+        nil
+      end
+
+      def yield_with_timeout_v2(worker, args, timeout_in, &block)
         Timeout.timeout(timeout_in) do
           yield
         end
