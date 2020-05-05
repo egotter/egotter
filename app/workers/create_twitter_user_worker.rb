3bb7d105ba7b048ca66971e84cd43312d6a28524
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-06 08:36:32 +0900

    Perform ImportTwitterUserRequest in CreateTwitterUserWorker

--- app/workers/create_twitter_user_worker.rb
+++ app/workers/create_twitter_user_worker.rb
@@ -30,9 +30,9 @@ class CreateTwitterUserWorker
     request = CreateTwitterUserRequest.find(request_id)
     task = CreateTwitterUserTask.new(request)
     task.start!
-    twitter_user = task.twitter_user
 
-    enqueue_next_jobs(request.user_id, request.uid, twitter_user)
+    UpdateUsageStatWorker.perform_async(request.uid, user_id: request.user_id, location: self.class)
+    UpdateAudienceInsightWorker.perform_async(request.uid, location: self.class)
     notify(request.user, request.uid)
 
     # Saved values and relations At this point:
@@ -40,17 +40,15 @@ class CreateTwitterUserWorker
     #   friendships(efs+s3), followerships(efs+s3)
     #   statuses, mentions, favorites
 
+    import_request = ImportTwitterUserRequest.create!(user_id: request.user_id, twitter_user: task.twitter_user)
+    import_request.perform!
+    import_request.finished!
+
   rescue CreateTwitterUserRequest::Error => e
   rescue => e
     logger.warn "#{e.class} #{e.message} #{request_id} #{options.inspect}"
     logger.info "Caused by #{e.cause.inspect}" if e.cause
     logger.info e.backtrace.join("\n")
-  end
-
-  def enqueue_next_jobs(user_id, uid, twitter_user)
-    ImportTwitterUserRelationsWorker.perform_async(user_id, uid, twitter_user_id: twitter_user.id)
-    UpdateUsageStatWorker.perform_async(uid, user_id: user_id, location: self.class)
-    UpdateAudienceInsightWorker.perform_async(uid, location: self.class, twitter_user_id: twitter_user.id)
   end
 
   private
