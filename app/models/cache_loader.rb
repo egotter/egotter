c0001fd7f2d223e7c85c189161b0033ddcd666b3
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-05 19:39:36 +0900

    Dont use Parallel if concurrency is 1

--- app/models/cache_loader.rb
+++ app/models/cache_loader.rb
@@ -12,10 +12,18 @@ class CacheLoader
     @start = Time.zone.now
     q = Queue.new
 
-    Parallel.each(@records, in_threads: @concurrency) do |record|
-      raise Parallel::Break if timeout?
-      @block.call(record)
-      q.push(true)
+    if @concurrency > 1
+      Parallel.each(@records, in_threads: @concurrency) do |record|
+        raise Parallel::Break if timeout?
+        @block.call(record)
+        q.push(true)
+      end
+    else
+      @records.each do |record|
+        break if timeout?
+        @block.call(record)
+        q.push(true)
+      end
     end
 
     unless q.size == @records.size
