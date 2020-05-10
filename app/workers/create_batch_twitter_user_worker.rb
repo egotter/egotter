6106b1c7769dd1390cff7a2c35e5ff1ea2953eab
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-11 05:48:02 +0900

    Remove StartCreatingTwitterUsersTask

--- app/workers/create_batch_twitter_user_worker.rb
+++
@@ -1,9 +0,0 @@
-# I want to print this class name to sidekiq.log.
-class CreateBatchTwitterUserWorker < CreateTwitterUserWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: self, retry: 0, backtrace: false
-
-  def expire_in
-    1.day
-  end
-end
