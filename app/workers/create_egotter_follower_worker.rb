bb05ee3f042537770552a4e1e29c89afcf6aa6e8
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-15 20:34:52 +0900

    Update error handling of CreateEgotterFollowerWorker

--- app/workers/create_egotter_follower_worker.rb
+++ app/workers/create_egotter_follower_worker.rb
@@ -10,6 +10,10 @@ class CreateEgotterFollowerWorker
     1.minute
   end
 
+  def sleep_in
+    rand(3) + 1.second
+  end
+
   def perform(user_id, options = {})
     user = User.find(user_id)
     if EgotterFollower.exists?(uid: user.uid)
@@ -18,16 +22,18 @@ class CreateEgotterFollowerWorker
       EgotterFollower.create!(uid: user.uid, screen_name: user.screen_name)
     end
   rescue ActiveRecord::RecordNotUnique => e
+    logger.info "Record is not unique #{e.inspect} user_id=#{user_id} options=#{options.inspect}"
   rescue ActiveRecord::Deadlocked => e
     # Mysql2::Error: Deadlock found when trying to get lock; try restarting transaction:
     @retry_count ||= 0
     if @retry_count < 3
+      sleep(sleep_in)
       @retry_count += 1
       retry
     else
-      raise
+      logger.warn "Retry exhausted #{e.inspect} retry_count=#{@retry_count} user_id=#{user_id} options=#{options.inspect}"
     end
   rescue => e
-    logger.warn "#{e.class}: #{e.message} #{user_id} #{options.inspect}"
+    logger.warn "#{e.inspect} user_id=#{user_id} options=#{options.inspect}"
   end
 end
