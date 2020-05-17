2a5cbc4c1aad1bca083791f2a8b2aeba6f6a1c35
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-17 10:20:44 +0900

    Explicitly call the class object in preload.rb

--- config/initializers/preload.rb
+++ config/initializers/preload.rb
@@ -1,20 +1,25 @@
-S3
+S3.class
+S3::Friendship.class
+S3::Followership.class
+S3::Profile.class
 S3::Friendship.client
 S3::Followership.client
 S3::Profile.client
 
-DynamoDB
-DynamoDB::Util
-DynamoDB::TwitterUser
+DynamoDB.class
+DynamoDB::Util.class
+DynamoDB::TwitterUser.class
+DynamoDB::Client.class
 DynamoDB::Client.dynamo_db
 
-Efs
+Efs.class
+Efs::TwitterUser.class
 Efs::TwitterUser.cache_client
-Efs::Tweet
-Efs::StatusTweet
-Efs::FavoriteTweet
-Efs::MentionTweet
+Efs::Tweet.class
+Efs::StatusTweet.class
+Efs::FavoriteTweet.class
+Efs::MentionTweet.class
 
-InMemory
-InMemory::Tweet
-InMemory::StatusTweet
+InMemory.class
+InMemory::Tweet.class
+InMemory::StatusTweet.class
