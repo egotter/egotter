9da3b4e9ee27a373d2f4a444d8616456f2b22329
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-21 18:07:18 +0900

    Refactor #diff

--- app/models/concerns/twitter_user/dirty.rb
+++ app/models/concerns/twitter_user/dirty.rb
@@ -11,21 +11,43 @@ module Concerns::TwitterUser::Dirty
 
   # return:
   # {
-  #    friends_count: [before ,after],
-  #    followers_count: [before, after],
-  #    friend_uids: [[before], [after]],
-  #    follower_uids: [[before], [after]]
+  #    friends_count:   [111, 222]
+  #    followers_count: [333, 444]
+  #    friend_uids:     [[1, 2, 3], [3, 4, 5]]
+  #    follower_uids:   [[1, 2, 3], [3, 4, 5]]
   #  }
   def diff(newer)
     older = self
 
-    %i(friends_count followers_count friend_uids follower_uids).map do |attr|
-      values = [older.send(attr), newer.send(attr)]
-      values = values.map(&:sort) if attr.in?(%i(friend_uids follower_uids))
+    diff = Diff.from_record(older, newer)
 
-      logger.debug {"#{self.class}##{__method__} uid=#{uid} screen_name=#{screen_name} attr=#{attr} values[0]=#{values[0].inspect} values[1]=#{values[1].inspect}"}
+    if Rails.env.development?
+      logger.debug { "#{self.class}##{__method__} uid=#{uid} screen_name=#{screen_name} diff.keys=#{diff.keys}" }
+    end
 
-      [attr, values]
-    end.to_h.reject {|_, v| v[0] == v[1]}
+    diff
+  end
+
+  class Diff < Hash
+    def initialize(hash)
+      hash.each { |key, value| self[key] = value }
+    end
+
+    class << self
+      def from_record(older, newer)
+        array = []
+
+        %i(friends_count followers_count friend_uids follower_uids).map do |attr|
+          values = [older.send(attr), newer.send(attr)]
+          values = values.map(&:sort) if %i(friend_uids follower_uids).include?(attr)
+          array << [attr, values]
+        end
+
+        hash = array.to_h
+        hash.reject! { |_, v| v[0] == v[1] }
+
+        new(hash)
+      end
+    end
   end
 end
--- spec/models/concerns/twitter_user/dirty_spec.rb
+++ spec/models/concerns/twitter_user/dirty_spec.rb
@@ -1,43 +1,116 @@
 require 'rails_helper'
 
 RSpec.describe Concerns::TwitterUser::Dirty do
-  let(:twitter_user) { create(:twitter_user, friends_count: 5, followers_count: 5) }
+  let(:friend_uids) { [1, 2, 3, 4, 5].shuffle }
+  let(:friends_count) { friend_uids.size }
+  let(:follower_uids) { [1, 2, 3, 4, 5, 6, 7, 8, 9].shuffle }
+  let(:followers_count) { follower_uids.size }
+  let(:twitter_user) { build(:twitter_user) }
+
+  shared_context 'copy record' do
+    let(:copied) { double('twitter_user') }
+    before do
+      allow(copied).to receive(:friends_count).and_return(new_friends_count)
+      allow(copied).to receive(:friend_uids).and_return(new_friend_uids)
+      allow(copied).to receive(:followers_count).and_return(new_followers_count)
+      allow(copied).to receive(:follower_uids).and_return(new_follower_uids)
+    end
+  end
 
   describe '#diff' do
     subject { twitter_user.diff(copied) }
 
-    context 'Same record is passed' do
-      let(:copied) do
-        build(:twitter_user, uid: twitter_user.uid, screen_name: twitter_user.screen_name)
-      end
+    before do
+      allow(twitter_user).to receive(:friends_count).and_return(friends_count)
+      allow(twitter_user).to receive(:friend_uids).and_return(friend_uids)
+      allow(twitter_user).to receive(:followers_count).and_return(followers_count)
+      allow(twitter_user).to receive(:follower_uids).and_return(follower_uids)
+    end
 
-      before do
-        allow(copied).to receive(:friends_count).and_return(twitter_user.friends_count)
-        allow(copied).to receive(:friend_uids).and_return(twitter_user.friend_uids)
-        allow(copied).to receive(:followers_count).and_return(twitter_user.followers_count)
-        allow(copied).to receive(:follower_uids).and_return(twitter_user.follower_uids)
-      end
+    context 'all values are the same' do
+      let(:new_friend_uids) { friend_uids }
+      let(:new_friends_count) { friend_uids.size }
+      let(:new_follower_uids) { follower_uids }
+      let(:new_followers_count) { follower_uids.size }
+
+      include_context 'copy record'
 
       it { is_expected.to be_empty }
     end
 
-    context 'Different record is passed' do
-      let(:copied) do
-        build(:twitter_user, uid: twitter_user.uid, screen_name: twitter_user.screen_name)
+    context 'all values are different' do
+      let(:new_friend_uids) { [2, 3, 4].shuffle }
+      let(:new_friends_count) { new_friend_uids.size }
+      let(:new_follower_uids) { [6, 7, 8].shuffle }
+      let(:new_followers_count) { new_follower_uids.size }
+
+      include_context 'copy record'
+
+      specify 'friend_uids is sorted' do
+        is_expected.to satisfy do |result|
+          result[:friend_uids][0] == friend_uids.sort &&
+              result[:friend_uids][1] == new_friend_uids.sort
+        end
       end
 
-      before do
-        allow(copied).to receive(:friends_count).and_return(1)
-        allow(copied).to receive(:friend_uids).and_return([2, 3, 4])
-        allow(copied).to receive(:followers_count).and_return(5)
-        allow(copied).to receive(:follower_uids).and_return([6, 7])
+      specify 'follower_uids is sorted' do
+        is_expected.to satisfy do |result|
+          result[:follower_uids][0] == follower_uids.sort &&
+              result[:follower_uids][1] == new_follower_uids.sort
+        end
       end
 
-      it do
-        is_expected.to match(friends_count: [twitter_user.friends_count, 1],
-                             friend_uids: [twitter_user.friend_uids.sort, [2, 3, 4]],
-                             follower_uids: [twitter_user.follower_uids.sort, [6, 7]])
+      specify 'result includes 4 keys' do
+        is_expected.to satisfy do |result|
+          result.keys.size == 4
+        end
+      end
+
+      specify 'result is correct' do
+        is_expected.to satisfy do |result|
+          result[:friends_count] == [friends_count, new_friends_count] &&
+              result[:friend_uids] == [friend_uids.sort, new_friend_uids.sort] &&
+              result[:followers_count] == [followers_count, new_followers_count] &&
+              result[:follower_uids] == [follower_uids.sort, new_follower_uids.sort]
+        end
       end
     end
+
+    context 'friend_uids is different' do
+      let(:new_friend_uids) { [2, 3, 4].shuffle }
+      let(:new_friends_count) { new_friend_uids.size }
+      let(:new_follower_uids) { follower_uids }
+      let(:new_followers_count) { followers_count }
+
+      include_context 'copy record'
+
+      specify 'friend_uids is sorted' do
+        is_expected.to satisfy do |result|
+          result[:friend_uids][0] == friend_uids.sort &&
+              result[:friend_uids][1] == new_friend_uids.sort
+        end
+      end
+
+      specify "result doesn't have :followers_count and follower_uids" do
+        is_expected.to satisfy do |result|
+          !result.has_key?(:followers_count) &&
+              !result.has_key?(:follower_uids)
+        end
+      end
+
+      specify 'result includes 2 keys' do
+        is_expected.to satisfy do |result|
+          result.keys.size == 2
+        end
+      end
+
+      specify 'result is correct' do
+        is_expected.to satisfy do |result|
+          result[:friends_count] == [friends_count, new_friends_count] &&
+              result[:friend_uids] == [friend_uids.sort, new_friend_uids.sort]
+        end
+      end
+    end
+
   end
 end
