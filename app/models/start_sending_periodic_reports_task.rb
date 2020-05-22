f10390846c89ced26c22080e7a710a944235b67c
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-22 17:43:40 +0900

    #start_reminding! uses @delay

--- app/models/start_sending_periodic_reports_task.rb
+++ app/models/start_sending_periodic_reports_task.rb
@@ -54,7 +54,11 @@ class StartSendingPeriodicReportsTask
     RemindPeriodicReportRequest.import requests, validate: false
 
     user_ids.each do |user_id|
-      CreatePeriodicReportMessageWorker.perform_async(user_id, allotted_messages_will_expire: true)
+      if @delay
+        CreatePeriodicReportMessageWorker.perform_in(@dekay, user_id, allotted_messages_will_expire: true)
+      else
+        CreatePeriodicReportMessageWorker.perform_async(user_id, allotted_messages_will_expire: true)
+      end
     end
   end
 
