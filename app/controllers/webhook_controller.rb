e96d029b48d904c9f65ded962898248ff7fe7961
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-22 11:32:43 +0900

    Remove #process_direct_message_event from WebhookController

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,7 +2,6 @@ require 'openssl'
 require 'base64'
 
 class WebhookController < ApplicationController
-  include Concerns::PeriodicReportConcern
 
   skip_before_action :verify_authenticity_token, only: :twitter
 
@@ -29,74 +28,6 @@ class WebhookController < ApplicationController
 
   def direct_message_event_for_egotter?
     params[:for_user_id].to_i == User::EGOTTER_UID && params[:direct_message_events]
-  end
-
-  def process_direct_message_event(event)
-    return unless event['type'] == 'message_create'
-
-    dm = DirectMessage.new(event: event.deep_symbolize_keys)
-
-    if sent_from_user?(dm)
-      process_message_from_user(dm)
-    elsif sent_from_egotter?(dm)
-      process_message_from_egotter(dm)
-    end
-
-    if message_from_user?(dm)
-      GlobalTotalDirectMessageReceivedFlag.new.received(dm.sender_id)
-    elsif message_from_egotter?(dm)
-      GlobalTotalDirectMessageSentFlag.new.received(dm.recipient_id)
-    end
-  end
-
-  def process_message_from_user(dm)
-    GlobalDirectMessageReceivedFlag.new.received(dm.sender_id)
-    GlobalSendDirectMessageCountByUser.new.clear(dm.sender_id)
-
-    if restart_requested?(dm)
-      enqueue_user_requested_restarting_periodic_report(dm)
-      enqueue_user_requested_periodic_report(dm, fuzzy: true)
-    elsif send_now_requested?(dm)
-      enqueue_user_requested_periodic_report(dm)
-    elsif continue_requested?(dm)
-      enqueue_user_requested_periodic_report(dm, fuzzy: true)
-    elsif stop_now_requested?(dm)
-      enqueue_user_requested_stopping_periodic_report(dm)
-    elsif report_received?(dm)
-      enqueue_user_received_periodic_report(dm)
-    else
-      logger.info { "#{__method__} dm is ignored #{dm.text}" }
-    end
-
-    SendReceivedMessageWorker.perform_async(dm.sender_id, dm_id: dm.id, text: dm.text)
-  end
-
-  def process_message_from_egotter(dm)
-    GlobalDirectMessageSentFlag.new.received(dm.recipient_id)
-
-    if send_now_requested?(dm)
-      enqueue_egotter_requested_periodic_report(dm)
-    end
-
-    SendSentMessageWorker.perform_async(dm.recipient_id, dm_id: dm.id, text: dm.text)
-  end
-
-  # TODO Rename to message_from_user_by_hand?
-  def sent_from_user?(dm)
-    dm.text.exclude?('#egotter') && dm.sender_id != User::EGOTTER_UID
-  end
-
-  # TODO Rename to message_from_egotter_by_hand?
-  def sent_from_egotter?(dm)
-    dm.text.exclude?('#egotter') && dm.sender_id == User::EGOTTER_UID
-  end
-
-  def message_from_user?(dm)
-    dm.sender_id != User::EGOTTER_UID
-  end
-
-  def message_from_egotter?(dm)
-    dm.sender_id == User::EGOTTER_UID
   end
 
   def crc_response
