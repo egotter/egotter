3f2065c69ec555f85f593e5c713490c6657472ba
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-25 10:39:10 +0900

    Apply #uniq to the sum of friend_uids and follower_uids

--- app/models/create_twitter_user_task.rb
+++ app/models/create_twitter_user_task.rb
@@ -13,17 +13,13 @@ class CreateTwitterUserTask
   def start!(context = nil)
     @log = CreateTwitterUserLog.create_by(request: request)
 
-    # Regardless of whether or not the TwitterUser record is created, the TwitterDB::User record is updated.
-    CreateTwitterDBUserWorker.perform_async([request.uid], user_id: request.user_id, force_update: true, enqueued_by: 'CreateTwitterUserTask request.uid')
+    update_target_user(request)
 
     @twitter_user = request.perform!(context)
     request.finished!
     @log.update(status: true)
 
-    ([@twitter_user.uid] + @twitter_user.friend_uids + @twitter_user.follower_uids).each_slice(100) do |uids|
-      options = {user_id: request.user_id, compressed: true, enqueued_by: 'CreateTwitterUserTask friends and followers'}
-      CreateTwitterDBUserWorker.perform_async(CreateTwitterDBUserWorker.compress(uids), options)
-    end
+    update_friends_and_followers(@twitter_user)
 
     self
   rescue Twitter::Error::TooManyRequests => e
@@ -39,5 +35,21 @@ class CreateTwitterUserTask
   rescue => e
     @log.update(error_class: e.class, error_message: e.message)
     raise
+  end
+
+  private
+
+  def update_target_user(request)
+    # Regardless of whether or not the TwitterUser record is created, the TwitterDB::User record is updated.
+    CreateTwitterDBUserWorker.perform_async([request.uid], user_id: request.user_id, force_update: true, enqueued_by: "#{self.class}##{__method__}")
+  end
+
+  def update_friends_and_followers(twitter_user)
+    uids = ([twitter_user.uid] + twitter_user.friend_uids + twitter_user.follower_uids).uniq
+    options = {user_id: request.user_id, compressed: true, enqueued_by: "#{self.class}##{__method__}"}
+
+    uids.each_slice(100) do |uids_array|
+      CreateTwitterDBUserWorker.perform_async(CreateTwitterDBUserWorker.compress(uids_array), options)
+    end
   end
 end
--- spec/models/create_twitter_user_task_spec.rb
+++ spec/models/create_twitter_user_task_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe CreateTwitterUserTask, type: :model do
   let(:user) { create(:user) }
   let(:request) { create(:create_twitter_user_request, user_id: user.id, uid: 1) }
-  let(:task) { CreateTwitterUserTask.new(request) }
+  let(:task) { described_class.new(request) }
 
   describe '#start!' do
     let(:context) { 'context' }
+    let(:twitter_user) { 'twitter_user' }
     subject { task.start!(context) }
+
+    it do
+      expect(task).to receive(:update_target_user).with(request)
+      expect(request).to receive(:perform!).with(context).and_return(twitter_user)
+      expect(request).to receive(:finished!)
+      expect(task).to receive(:update_friends_and_followers).with(twitter_user)
+      subject
+    end
 
     context 'request.perform! raises Twitter::Error::TooManyRequests' do
       before do
@@ -19,6 +28,33 @@ RSpec.describe CreateTwitterUserTask, type: :model do
         expect { subject }.to raise_error(CreateTwitterUserRequest::TooManyRequests).
             and change { TooManyRequestsUsers.new.size }.by(1)
       end
+    end
+  end
+
+  describe '#update_target_user' do
+    subject { task.send(:update_target_user, request) }
+    it do
+      expect(CreateTwitterDBUserWorker).to receive(:perform_async).
+          with([request.uid], user_id: request.user_id, force_update: true, enqueued_by: instance_of(String))
+      subject
+    end
+  end
+
+  describe '#update_friends_and_followers' do
+    let(:twitter_user) { instance_double(TwitterUser) }
+    subject { task.send(:update_friends_and_followers, twitter_user) }
+
+    before do
+      allow(twitter_user).to receive(:uid).and_return(1)
+      allow(twitter_user).to receive(:friend_uids).and_return([2, 3, 4])
+      allow(twitter_user).to receive(:follower_uids).and_return([3, 4, 5])
+    end
+
+    it do
+      expect(CreateTwitterDBUserWorker).to receive(:compress).with([1, 2, 3, 4, 5]).and_return('compressed')
+      expect(CreateTwitterDBUserWorker).to receive(:perform_async).
+          with('compressed', user_id: request.user_id, compressed: true, enqueued_by: instance_of(String))
+      subject
     end
   end
 end
