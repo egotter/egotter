a74635f996bf00b0adfd9c7d07a667448ef90822
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-26 21:29:33 +0900

    Rename from runner to runner.sh

--- bin/cron/runner.sh
+++ bin/cron/runner.sh
@@ -3,5 +3,6 @@
 log='/var/egotter/log/cron.log'
 cmd="/usr/local/bin/bundle exec rails runner $1"
 
-echo "$cmd started" >>$log 2>&1
+echo "`date` $cmd started" >>$log 2>&1
 cd /var/egotter && RAILS_ENV=production $cmd >>$log 2>&1
+echo "`date` $cmd finished" >>$log 2>&1
