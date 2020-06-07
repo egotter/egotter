afd4ffb920f7c6cc2f619afb5b316c6fc60f9fb8
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-08 01:06:01 +0900

    Ignore "Not authorized." in UpdateUsageStatWorker

--- app/workers/update_usage_stat_worker.rb
+++ app/workers/update_usage_stat_worker.rb
@@ -18,26 +18,25 @@ class UpdateUsageStatWorker
     return if stat&.fresh?
 
     twitter_user = TwitterUser.select(:uid, :screen_name, :created_at).latest_by(uid: uid)
-    statuses =
-        if twitter_user.status_tweets.any?
-          twitter_user.status_tweets
-        else
-          user = User.find_by(id: options['user_id'])
-          user = User.authorized.find_by(uid: uid) unless user
-          client = user ? user.api_client : Bot.api_client
-          client.user_timeline(uid.to_i).map { |s| TwitterDB::Status.build_by(twitter_user: twitter_user, status: s) }
-        end
+    statuses = twitter_user&.status_tweets
+
+    if statuses.blank?
+      user = User.find_by(id: options['user_id'])
+      user = User.authorized.find_by(uid: uid) unless user
+      client = user ? user.api_client : Bot.api_client
+      statuses = client.user_timeline(uid.to_i).map { |s| TwitterDB::Status.build_by(twitter_user: twitter_user, status: s) }
+    end
 
     if statuses.any?
       UsageStat.builder(uid).statuses(statuses).build.save!
     end
-  rescue Twitter::Error::Unauthorized => e
-    unless e.message == 'Invalid or expired token.'
-      logger.warn "#{e.class}: #{e.message} #{uid}"
+
+  rescue => e
+    if AccountStatus.invalid_or_expired_token?(e) || AccountStatus.not_authorized?(e)
+      # Do nothing
+    else
+      logger.warn "#{e.inspect} uid=#{uid} options=#{options.inspect}"
       logger.info e.backtrace.join("\n")
     end
-  rescue => e
-    logger.warn "#{e.class}: #{e.message} #{uid}"
-    logger.info e.backtrace.join("\n")
   end
 end
