4d4705e85dca2966af564dabcc96f0e5bdd9467e
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-14 02:44:24 +0900

    Print elapsed time in cron/rake.sh and cron/runner.sh

--- bin/cron/rake.sh
+++ bin/cron/rake.sh
@@ -3,6 +3,8 @@
 log='/var/egotter/log/cron.log'
 cmd="/usr/local/bin/bundle exec rake $1"
 
+SECONDS=0
+
 echo "`date` $cmd started" >>$log 2>&1
 cd /var/egotter && RAILS_ENV=production $cmd >>$log 2>&1
-echo "`date` $cmd finished" >>$log 2>&1
+echo "`date` $cmd finished elapsed=$SECONDS" >>$log 2>&1
