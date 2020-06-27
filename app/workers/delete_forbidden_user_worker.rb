768fd37f73cdce871d3b5c38e360a54c93332e16
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-27 14:14:40 +0900

    Add options param to DeleteForbiddenUserWorker and DeleteNotFoundUserWorker

--- app/workers/delete_forbidden_user_worker.rb
+++ app/workers/delete_forbidden_user_worker.rb
@@ -2,7 +2,7 @@ class DeleteForbiddenUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'deleting_low', retry: 0, backtrace: false
 
-  def unique_key(screen_name)
+  def unique_key(screen_name, options = {})
     screen_name
   end
 
@@ -10,7 +10,7 @@ class DeleteForbiddenUserWorker
     1.minute
   end
 
-  def perform(screen_name)
+  def perform(screen_name, options = {})
     ForbiddenUser.find_by(screen_name: screen_name)&.delete
   rescue ActiveRecord::RecordNotUnique => e
     # Do nothing
--- app/workers/delete_not_found_user_worker.rb
+++ app/workers/delete_not_found_user_worker.rb
@@ -2,7 +2,7 @@ class DeleteNotFoundUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'deleting_low', retry: 0, backtrace: false
 
-  def unique_key(screen_name)
+  def unique_key(screen_name, options = {})
     screen_name
   end
 
@@ -10,7 +10,7 @@ class DeleteNotFoundUserWorker
     1.minute
   end
 
-  def perform(screen_name)
+  def perform(screen_name, options = {})
     NotFoundUser.find_by(screen_name: screen_name)&.delete
   rescue ActiveRecord::RecordNotUnique => e
     # Do nothing
