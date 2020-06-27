95930b2d3b9e1694e8310499eaaf8c52cd5ad616
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-27 14:59:44 +0900

    Ignore invalid_credentials error in OmniauthLogger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,13 +5,23 @@ class OmniauthLogger < ::Logger
   end
 
   def error(*args)
-    if args[0].starts_with?('omniauth: (twitter) Authentication failure! session_expired:')
+    if session_expired?(args[0]) || invalid_credentials?(args[0])
       info(*args)
     else
       super
     end
   rescue => e
     super
+  end
+
+  private
+
+  def session_expired?(str)
+    str.include?('(twitter) Authentication failure! session_expired: OmniAuth::NoSessionError')
+  end
+
+  def invalid_credentials?(str)
+    str.include?('(twitter) Authentication failure! invalid_credentials: OAuth::Unauthorized')
   end
 end
 
