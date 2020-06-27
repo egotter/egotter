45b2f4dfc7c6ab112b0bfc6d5817c6cec3977153
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-27 19:15:44 +0900

    truncate(150) -> truncate(200) in ProcessWebhookEventWorker

--- app/workers/process_webhook_event_worker.rb
+++ app/workers/process_webhook_event_worker.rb
@@ -14,7 +14,7 @@ class ProcessWebhookEventWorker
   end
 
   def after_skip(*args)
-    logger.warn "The job of #{self.class} is skipped digest=#{digest(args[0])} args=#{args.inspect.truncate(150)}"
+    logger.warn "The job of #{self.class} is skipped digest=#{digest(args[0])} args=#{args.inspect.truncate(200)}"
   end
 
   def timeout_in
@@ -22,7 +22,7 @@ class ProcessWebhookEventWorker
   end
 
   def after_timeout(*args)
-    logger.warn "The job of #{self.class} timed out digest=#{digest(args[0])} args=#{args.inspect.truncate(150)}"
+    logger.warn "The job of #{self.class} timed out digest=#{digest(args[0])} args=#{args.inspect.truncate(200)}"
   end
 
   # options:
