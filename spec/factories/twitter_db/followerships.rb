e3fa3658e3bde504a716d341828166c79205b479
ts-3156 <ts_3156@yahoo.co.jp>
2020-06-28 15:28:13 +0900

    Remove TwitterDB::Friendship and TwitterDB::Followership

--- spec/factories/twitter_db/followerships.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :twitter_db_followership, class: TwitterDB::Followership do
-    user_uid {-1}
-    follower_uid {-1}
-  end
-end
--- spec/factories/twitter_db/friendships.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :twitter_db_friendship, class: TwitterDB::Friendship do
-    user_uid {-1}
-    friend_uid {-1}
-  end
-end
