a58aa8e99dba0ee117498d61f51e474e6b2be68d
ts-3156 <ts_3156@yahoo.co.jp>
2020-07-07 17:11:57 +0900

    Remove spec/support/hash.rb

--- spec/models/twitter_db/status_spec.rb
+++ spec/models/twitter_db/status_spec.rb
@@ -5,7 +5,27 @@ RSpec.describe TwitterDB::Status, type: :model do
   let(:twitter_user) { build(:twitter_user, uid: user.uid, screen_name: user.screen_name) }
 
   describe '.collect_raw_attrs' do
-    let(:method_name) { :collect_raw_attrs }
-    it_should_behave_like 'Accept any kind of keys'
+    let(:output) { {id: 1}.to_json }
+    subject { described_class.send(:collect_raw_attrs, input) }
+
+    context 'With symbol key' do
+      let(:input) { {id: 1} }
+      it { is_expected.to eq(output) }
+    end
+
+    context 'With string key' do
+      let(:input) { {'id' => 1} }
+      it { is_expected.to eq(output) }
+    end
+
+    context 'With Hashie::Mash with symbol key' do
+      let(:input) { Hashie::Mash.new(id: 1) }
+      it { is_expected.to eq(output) }
+    end
+
+    context 'With Hashie::Mash with string key' do
+      let(:input) { Hashie::Mash.new('id' => 1) }
+      it { is_expected.to eq(output) }
+    end
   end
 end
