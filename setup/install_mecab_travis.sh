dbdbbdad58a6fe22d50a0ec2ace557f414160b30
ts-3156 <ts_3156@yahoo.co.jp>
2020-07-07 16:02:42 +0900

    Use caches for mecab on travisci

--- setup/install_mecab_travis.sh
+++ setup/install_mecab_travis.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
+set -ex
+
+if ! command -v mecab &>/dev/null; then
+  echo "mecab found"
+  exit
+fi
+
+cur=$(pwd)
+
+dir=${pwd}/.mecab
+if [ ! -d $dir ]; then
+  mkdir $dir
+fi
+cd $dir
+
 wget -O mecab-0.996.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE"
 tar zxfv mecab-0.996.tar.gz
 cd mecab-0.996
-./configure --prefix=/usr/local --enable-utf8-only
+./configure --prefix=$dir --enable-utf8-only
 make && make check && sudo make install
 sudo ldconfig
 
 wget -O mecab-ipadic-2.7.0-20070801.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM"
 tar zxfv mecab-ipadic-2.7.0-20070801.tar.gz
 cd mecab-ipadic-2.7.0-20070801
-./configure --prefix=/usr/local --with-mecab-config=/usr/local/bin/mecab-config --with-charset=utf8
+./configure --prefix=$dir --with-mecab-config=${dir}/bin/mecab-config --with-charset=utf8
 make && sudo make install
 sudo ldconfig
+
+PATH=$PATH:${dir}/bin
+
+cd $cur
