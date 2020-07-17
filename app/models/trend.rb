0d501218e76f0f134a8c87d8de5cde7c654c137b
ts-3156 <ts_3156@yahoo.co.jp>
2020-07-17 20:35:57 +0900

    Save only a query to trends.properties

--- app/models/trend.rb
+++ app/models/trend.rb
@@ -24,7 +24,9 @@ class Trend < ApplicationRecord
 
       [WORLD_WOE_ID, JAPAN_WOE_ID].each do |woe_id|
         Bot.api_client.twitter.trends(woe_id).each.with_index do |trend, i|
-          create!(woe_id: woe_id, rank: i + 1, tweet_volume: trend.tweet_volume, name: trend.name, properties: trend, time: time)
+          prop = {query: trend.query}
+          prop[:promoted_content] = true if trend.promoted_content?
+          create!(woe_id: woe_id, rank: i + 1, tweet_volume: trend.tweet_volume, name: trend.name, properties: prop, time: time)
         end
       end
     end
--- spec/models/trend_spec.rb
+++ spec/models/trend_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Trend, type: :model do
   describe '.save_current_trends' do
-    let(:trends) { [build(:trend), build(:trend)] }
+    let(:raw_trends) { 2.times.map { double('trend', name: 'name', query: 'query', tweet_volume: 1, promoted_content?: false) } }
     subject { described_class.save_current_trends }
     before do
       allow(Bot).to receive_message_chain(:api_client, :twitter, :trends).
-          with(described_class::WORLD_WOE_ID).and_return(trends)
+          with(described_class::WORLD_WOE_ID).and_return(raw_trends)
 
       allow(Bot).to receive_message_chain(:api_client, :twitter, :trends).
-          with(described_class::JAPAN_WOE_ID).and_return(trends)
+          with(described_class::JAPAN_WOE_ID).and_return(raw_trends)
     end
     it { is_expected.to be_truthy }
   end
