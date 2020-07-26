ec66c43f59eadcaa42e1859e602e47582d771500
ts-3156 <ts_3156@yahoo.co.jp>
2020-07-26 10:00:00 +0900

    Sort tweet_clusters in ClustersController

--- app/controllers/clusters_controller.rb
+++ app/controllers/clusters_controller.rb
@@ -7,6 +7,7 @@ class ClustersController < ApplicationController
 
   def show
     if (@clusters = @twitter_user.usage_stat&.tweet_clusters)
+      @clusters = @clusters.sort_by { |_, c| -c }.to_h
       clusters = @clusters.take(3).map { |word, _| t('.cluster_name', name: word) }.join("\n")
       @tweet_text = t('clusters.show.tweet', user: @twitter_user.screen_name, clusters: clusters, url: cluster_url(@twitter_user))
       @cluster_names_str = @clusters.take(3).map(&:first).join(' ')
