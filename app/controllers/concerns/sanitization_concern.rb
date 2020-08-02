c13c21c58e62890c7eb6b17d66670db4f76b4443
ts-3156 <ts_3156@yahoo.co.jp>
2020-08-02 19:28:41 +0900

    Stop autocomplete

--- app/controllers/concerns/sanitization_concern.rb
+++ app/controllers/concerns/sanitization_concern.rb
@@ -3,10 +3,20 @@ require 'active_support/concern'
 module Concerns::SanitizationConcern
   extend ActiveSupport::Concern
 
-  included do
-  end
+  SAFE_CONTROLLERS = %w(
+      conversations
+      clusters searches
+      timelines
+      scores
+      tokimeki_unfollow
+      /delete_tweets
+      settings
+      not_found
+      /start
+      /personality_insights
+      /audience_insights
+  )
 
-  SAFE_CONTROLLERS = %w(conversations clusters searches timelines scores tokimeki_unfollow /delete_tweets settings not_found /start /personality_insights)
   SANITIZE_REDIRECT_PATH_REGEXP = Regexp.union(Search::API_V1_NAMES.map(&:to_s) + SAFE_CONTROLLERS)
 
   def sanitized_redirect_path(path)
--- app/helpers/searches_helper.rb
+++
@@ -1,9 +0,0 @@
-module SearchesHelper
-  def search_form_id
-    "search-form-#{SecureRandom.urlsafe_base64(10)}"
-  end
-
-  def search_input_id
-    "search-input-#{SecureRandom.urlsafe_base64(10)}"
-  end
-end
