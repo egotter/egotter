3cb9daab7c01340342232a12d6e580c01a98eb5d
ts-3156 <ts_3156@yahoo.co.jp>
2020-08-03 09:39:53 +0900

    Redirect to root path if AudienceInsight is not found

--- app/controllers/audience_insights_controller.rb
+++ app/controllers/audience_insights_controller.rb
@@ -1,8 +1,19 @@
 class AudienceInsightsController < ApplicationController
   include Concerns::SearchRequestConcern
 
+  before_action(only: :show) do
+    unless set_insight
+      redirect_to root_path, alert: t('.show.not_found', user: @twitter_user.screen_name, url: timeline_path(@twitter_user))
+    end
+  end
+
   def show
     @page_title = t('.page_title', user: @twitter_user.screen_name)
+  end
+
+  private
+
+  def set_insight
     @insight = @twitter_user.audience_insight
   end
 end
