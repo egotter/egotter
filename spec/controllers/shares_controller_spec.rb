acf556a766e5a6b8809285892a4146e944ba3f01
ts-3156 <ts_3156@yahoo.co.jp>
2020-08-21 21:36:25 +0900

    Write the number of unfollowers at the top of the notification

--- spec/controllers/shares_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SharesController, type: :controller do
-  describe '#egotter_share_url' do
-    subject { controller.send(:egotter_share_url) }
-    it { is_expected.to be_truthy }
-  end
-end
