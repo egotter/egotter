c21df429ea0e7d97ed28872cb7e7988de19d0d93
ts-3156 <ts_3156@yahoo.co.jp>
2020-09-11 14:57:26 +0900

    Remove instance methods from ServiceStatus

--- app/models/service_status.rb
+++ app/models/service_status.rb
@@ -1,46 +1,26 @@
 class ServiceStatus
-  def initialize(ex: nil)
-    @ex = ex
-  end
-
-  def connection_reset_by_peer?
-    # Errno::ECONNRESET
-    # HTTP::ConnectionError
-    # Twitter::Error
-    @ex && @ex.message.include?('Connection reset by peer')
-  end
-
-  def internal_server_error?
-    @ex && @ex.class == Twitter::Error::InternalServerError && ['Internal error', ''].include?(@ex.message)
-  end
-
-  def service_unavailable?
-    @ex && @ex.class == Twitter::Error::ServiceUnavailable && ['Over capacity', ''].include?(@ex.message)
-  end
-
-  def execution_expired?
-    @ex && @ex.class == Twitter::Error && @ex.message == 'execution expired'
-  end
-
-  def retryable_error?
-    connection_reset_by_peer? || internal_server_error? || service_unavailable? || execution_expired?
-  end
-
   class << self
     def connection_reset_by_peer?(ex)
-      new(ex: ex).connection_reset_by_peer?
+      # Errno::ECONNRESET
+      # HTTP::ConnectionError
+      # Twitter::Error
+      ex && ex.message.include?('Connection reset by peer')
     end
 
     def internal_server_error?(ex)
-      new(ex: ex).internal_server_error?
+      ex && ex.class == Twitter::Error::InternalServerError && ['Internal error', ''].include?(ex.message)
     end
 
     def service_unavailable?(ex)
-      new(ex: ex).service_unavailable?
+      ex && ex.class == Twitter::Error::ServiceUnavailable && ['Over capacity', ''].include?(ex.message)
+    end
+
+    def execution_expired?(ex)
+      ex && ex.class == Twitter::Error && ex.message == 'execution expired'
     end
 
     def retryable_error?(ex)
-      new(ex: ex).retryable_error?
+      connection_reset_by_peer?(ex) || internal_server_error?(ex) || service_unavailable?(ex) || execution_expired?(ex)
     end
   end
 end
--- spec/models/service_status_spec.rb
+++ spec/models/service_status_spec.rb
@@ -1,44 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ServiceStatus, type: :model do
-  describe '#connection_reset_by_peer?' do
-    let(:ex) { RuntimeError.new('Connection reset by peer') }
-    subject { described_class.new(ex: ex).connection_reset_by_peer? }
-    it { is_expected.to be_truthy }
-  end
-
-  describe '#internal_server_error?' do
-    let(:ex) { Twitter::Error::InternalServerError.new('Internal error') }
-    subject { described_class.new(ex: ex).internal_server_error? }
-    it { is_expected.to be_truthy }
-  end
-
-  describe '#service_unavailable?' do
-    let(:ex) { Twitter::Error::ServiceUnavailable.new('Over capacity') }
-    subject { described_class.new(ex: ex).service_unavailable? }
-    it { is_expected.to be_truthy }
-  end
-
-  describe '#execution_expired?' do
-    let(:ex) { Twitter::Error.new('execution expired') }
-    subject { described_class.new(ex: ex).execution_expired? }
-    it { is_expected.to be_truthy }
-  end
-
-  describe '#retryable_error?' do
-    let(:ex) { RuntimeError.new('Anything') }
-    let(:status) { described_class.new(ex: ex) }
-    subject { status.retryable_error? }
-
-    it do
-      expect(status).to receive(:connection_reset_by_peer?).with(no_args).and_return(false)
-      expect(status).to receive(:internal_server_error?).with(no_args).and_return(false)
-      expect(status).to receive(:service_unavailable?).with(no_args).and_return(false)
-      expect(status).to receive(:execution_expired?).with(no_args).and_return(true)
-      is_expected.to be_truthy
-    end
-  end
-
   describe '.connection_reset_by_peer?' do
     let(:ex) { RuntimeError.new('Connection reset by peer') }
     subject { described_class.connection_reset_by_peer?(ex) }
@@ -55,5 +17,24 @@ RSpec.describe ServiceStatus, type: :model do
     let(:ex) { Twitter::Error::ServiceUnavailable.new('Over capacity') }
     subject { described_class.service_unavailable?(ex) }
     it { is_expected.to be_truthy }
+  end
+
+  describe '.execution_expired?' do
+    let(:ex) { Twitter::Error.new('execution expired') }
+    subject { described_class.execution_expired?(ex) }
+    it { is_expected.to be_truthy }
+  end
+
+  describe '.retryable_error?' do
+    let(:ex) { RuntimeError.new('Anything') }
+    subject { described_class.retryable_error?(ex) }
+
+    it do
+      expect(described_class).to receive(:connection_reset_by_peer?).with(ex)
+      expect(described_class).to receive(:internal_server_error?).with(ex)
+      expect(described_class).to receive(:service_unavailable?).with(ex)
+      expect(described_class).to receive(:execution_expired?).with(ex)
+      subject
+    end
   end
 end
