771c75d99fe4eae0387d835e82e22271a9ffb7e2
ts-3156 <ts_3156@yahoo.co.jp>
2020-09-20 14:38:24 +0900

    Remove Api::V1::SummaryController

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
       get "follower_insights/locations_count", to: "follower_insights#locations_count"
       get "follower_insights/tweet_times", to: "follower_insights#tweet_times"
 
-      get "summary/summary", to: "summary#summary"
       get "timelines/profile", to: "timelines#profile"
       get "timelines/summary", to: "timelines#summary"
       get 'account_statuses', to: 'account_statuses#show'
