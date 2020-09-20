77bdf333db9892eac6c053a3f0ab7bf217576cc1
ts-3156 <ts_3156@yahoo.co.jp>
2020-09-20 16:57:57 +0900

    Fix test cases

---
+++ spec/controllers/api/v1/timelines_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TimelinesController, type: :controller do
+  let(:twitter_user) { create(:twitter_user) }
+
+  before do
+    allow(controller).to receive(:twitter_user_persisted?).with(any_args).and_return(true)
+    allow(controller).to receive(:twitter_db_user_persisted?).with(any_args).and_return(true)
+    allow(controller).to receive(:protected_search?).with(any_args).and_return(false)
+    allow(TwitterUser).to receive(:latest_by).with(any_args).and_return(twitter_user)
+  end
+
+  describe 'GET #summary' do
+    before do
+      allow(twitter_user).to receive_message_chain(:one_sided_friendships, :size).and_return(1)
+      allow(twitter_user).to receive_message_chain(:one_sided_followerships, :size).and_return(2)
+      allow(twitter_user).to receive_message_chain(:mutual_friendships, :size).and_return(3)
+      allow(twitter_user).to receive_message_chain(:unfriendships, :size).and_return(4)
+      allow(twitter_user).to receive_message_chain(:unfollowerships, :size).and_return(5)
+      allow(twitter_user).to receive_message_chain(:mutual_unfriendships, :size).and_return(6)
+    end
+
+    it do
+      get :summary, params: {uid: twitter_user.uid}
+      expect(JSON.parse(response.body)).to eq('one_sided_friends' => 1, 'one_sided_followers' => 2, 'mutual_friends' => 3, 'unfriends' => 4, 'unfollowers' => 5, 'blocking_or_blocked' => 6)
+    end
+  end
+
+  describe 'GET #profile' do
+    render_views
+
+    it do
+      get :profile, params: {uid: twitter_user.uid}
+      expect(JSON.parse(response.body).has_key?('html')).to be_truthy
+    end
+  end
+end
