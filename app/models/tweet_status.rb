8cfa862594ec30818c3a8ac1a11f468ab32f9811
ts-3156 <ts_3156@yahoo.co.jp>
2020-09-27 05:19:50 +0900

    Implement TweetStatus#that_page_does_not_exist?

--- app/models/tweet_status.rb
+++ app/models/tweet_status.rb
@@ -5,6 +5,10 @@ class TweetStatus
       ex.class == Twitter::Error::NotFound && ex.message == 'No status found with that ID.'
     end
 
+    def that_page_does_not_exist?(ex)
+      ex.class == Twitter::Error::NotFound && ex.message == 'Sorry, that page does not exist.'
+    end
+
     def not_authorized?(ex)
       ex.class == Twitter::Error::Forbidden && ex.message == 'Sorry, you are not authorized to see this status.'
     end
--- app/workers/delete_tweet_worker.rb
+++ app/workers/delete_tweet_worker.rb
@@ -28,7 +28,7 @@ class DeleteTweetWorker
   rescue => e
     if ServiceStatus.retryable_error?(e)
       retry
-    elsif TweetStatus.no_status_found?(e) || TweetStatus.not_authorized?(e)
+    elsif TweetStatus.no_status_found?(e) || TweetStatus.not_authorized?(e) || TweetStatus.that_page_does_not_exist?(e)
       # Do nothing
     else
       raise
