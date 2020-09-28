3ba501574de87fec059e0959667bdc760a50e110
ts-3156 <ts_3156@yahoo.co.jp>
2020-09-29 01:10:37 +0900

    Draw a watermark near the WordCloud

--- app/javascript/packs/word_cloud.js
+++ app/javascript/packs/word_cloud.js
@@ -4,13 +4,34 @@ class WordCloud {
   //   {word: 'w2', count: 2}
   // ]
   constructor(selector, data, width, height, smartphone) {
+    this.selector = selector;
+    this.width = width;
+    this.height = height;
+    this.watermarkOffset = 30;
+
     if (data.length >= 300) {
       data.sort(function (x, y) {
         return d3.descending(x.count, y.count);
       });
       data = data.splice(0, 300);
     }
+    this.data = data;
 
+    var rangeMax;
+    if (smartphone) {
+      rangeMax = data.length > 500 ? 100 : 50;
+    } else {
+      rangeMax = data.length > 500 ? 1000 : 100;
+    }
+    this.rangeMax = rangeMax;
+  }
+
+  draw() {
+    var selector = this.selector;
+    var width = this.width;
+    var height = this.height;
+    var watermarkOffset = this.watermarkOffset;
+    var data = this.data;
     var random = d3.random.irwinHall(2);
 
     function rotateWord() {
@@ -21,15 +42,7 @@ class WordCloud {
       return d.count;
     });
 
-    var rangeMax;
-    if (smartphone) {
-      rangeMax = data.length > 500 ? 100 : 50;
-    } else {
-      rangeMax = data.length > 500 ? 1000 : 100;
-    }
-    console.log('rangeMax', rangeMax);
-
-    var sizeScale = d3.scale.linear().domain([0, countMax]).range([10, rangeMax]);
+    var sizeScale = d3.scale.linear().domain([0, countMax]).range([10, this.rangeMax]);
     var colorScale = d3.scale.category20();
 
     var words = data.map(function (d) {
@@ -40,11 +53,12 @@ class WordCloud {
     });
 
     function draw(words) {
-      d3.select(selector).append("svg")
+      d3.select('#' + selector).append("svg")
+          .attr("id", selector + '-svg')
           .attr("width", width)
-          .attr("height", height)
+          .attr("height", height - watermarkOffset)
           .append("g")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+          .attr("transform", "translate(" + width / 2 + "," + (height - watermarkOffset) / 2 + ")")
           .selectAll("text")
           .data(words)
           .enter()
@@ -73,7 +87,7 @@ class WordCloud {
           });
     }
 
-    d3.layout.cloud().size([width, height])
+    d3.layout.cloud().size([width, height - watermarkOffset])
         .words(words)
         .rotate(rotateWord)
         .font("Impact")
@@ -82,7 +96,51 @@ class WordCloud {
         })
         .on("end", draw)
         .start();
+
+    this.drawWatermark();
+  }
+
+  drawWatermark() {
+    var svg = $('#' + this.selector).find('svg');
+    svg.attr('height', parseInt(svg.attr('height')) + this.watermarkOffset);
+    new Watermark(svg.attr('id'), svg.attr('width'), svg.attr('height')).draw();
   }
 }
 
 window.WordCloud = WordCloud;
+
+class Watermark {
+  constructor(id, x, y) {
+    this.elem = d3.select('#' + id);
+    this.x = parseInt(x);
+    this.y = parseInt(y) - 7;
+    this.name = '#ワードクラウド';
+    this.domain = 'egotter.com';
+    this.nameOffset = 14;
+  }
+
+  draw() {
+    this.drawName();
+    this.drawDomain();
+  }
+
+  drawName() {
+    this.elem.append("svg:text")
+        .attr("x", this.x)
+        .attr("y", this.y - this.nameOffset)
+        .attr("text-anchor", 'end')
+        .attr("font-size", 10)
+        .attr("style", 'fill: #EA2184;')
+        .text(this.name);
+  }
+
+  drawDomain() {
+    this.elem.append("svg:text")
+        .attr("x", this.x)
+        .attr("y", this.y)
+        .attr("text-anchor", 'end')
+        .attr("font-size", 12)
+        .attr("style", 'fill: #EA2184;')
+        .text(this.domain);
+  }
+}
