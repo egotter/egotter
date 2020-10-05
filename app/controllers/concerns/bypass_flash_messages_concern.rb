7b9b9429961498dd2591e62d092ced25c309c5c2
ts-3156 <ts_3156@yahoo.co.jp>
2020-10-06 08:00:13 +0900

    Use collapse function in current_periodic_report.ja.html.erb

--- app/controllers/concerns/bypass_flash_messages_concern.rb
+++ app/controllers/concerns/bypass_flash_messages_concern.rb
@@ -27,23 +27,21 @@ module BypassFlashMessagesConcern
   end
 
   def bypassed_notice_message
-    if bypassed_notice_message_found?
-      if session[:bypassed_notice_message] == 'after_sign_in'
-        @without_alert_container = true
-        after_sign_in_message('sign_in')
-      elsif session[:bypassed_notice_message] == 'after_sign_up'
-        @without_alert_container = true
-        after_sign_in_message('sign_up')
-      elsif session[:bypassed_notice_message] == 'search_limitation_soft_limited'
-        url = sign_in_path(via: current_via('search_limitation_soft_limited'))
-        search_limitation_soft_limited_message('user', url) # The user name can be anything
-      elsif session[:bypassed_notice_message] == 'too_many_searches'
-        @without_alert_container = true
-        too_many_searches_message
-      elsif start_page? && user_signed_in?
-        @without_alert_container = true
-        after_sign_in_message(nil)
-      end
+    if session[:bypassed_notice_message] == 'after_sign_in'
+      @without_alert_container = true
+      after_sign_in_message('sign_in')
+    elsif session[:bypassed_notice_message] == 'after_sign_up'
+      @without_alert_container = true
+      after_sign_in_message('sign_up')
+    elsif session[:bypassed_notice_message] == 'search_limitation_soft_limited'
+      url = sign_in_path(via: current_via('search_limitation_soft_limited'))
+      search_limitation_soft_limited_message('user', url) # The user name can be anything
+    elsif session[:bypassed_notice_message] == 'too_many_searches'
+      @without_alert_container = true
+      too_many_searches_message
+    elsif start_page? && user_signed_in?
+      @without_alert_container = true
+      after_sign_in_message(nil)
     end
   end
 
@@ -56,7 +54,6 @@ module BypassFlashMessagesConcern
         sample_image1_url: view_context.image_path('/egotter_screenshot_prompt_report_828x739.png'),
         sample_image2_url: view_context.image_path('/egotter_screenshot_prompt_report_1165x318.png'),
         sample_image3_url: view_context.image_path('/egotter_screenshot_prompt_report_1156x422.png'),
-        id_hash: SecureRandom.urlsafe_base64(10)
     }
     render_to_string(template: 'messages/current_periodic_report', layout: false, locals: values)
   end
