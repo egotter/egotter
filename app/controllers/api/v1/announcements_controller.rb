0658f9e76765d738cd8c991c3a24ee0cd46e1e35
ts-3156 <ts_3156@yahoo.co.jp>
2020-10-07 00:04:05 +0900

    Add _announcement_section.html.erb and _announcements.html.erb

--- app/controllers/api/v1/announcements_controller.rb
+++ app/controllers/api/v1/announcements_controller.rb
@@ -5,7 +5,8 @@ module Api
       before_action { self.access_log_disabled = true }
 
       def list
-        render json: {records: Announcement.list.map { |a| {date: a.date, message: a.message} }}
+        html = render_to_string(partial: 'shared/announcements', formats: [:html])
+        render json: {html: html}
       end
     end
   end
--- app/javascript/packs/announcement.js
+++ app/javascript/packs/announcement.js
@@ -8,16 +8,7 @@ class Announcement {
   load() {
     var url = this.url;
     var id = '#' + this.id;
-
-    new AsyncLoader(url, id, function (res) {
-      var container = $( id);
-      res.records.forEach(function (record) {
-        var row = $('<tr>');
-        row.append($('<td>').text(record.date));
-        row.append($('<td>').text(record.message));
-        container.append(row);
-      });
-    }).lazyload();
+    new AsyncLoader(url, id).lazyload();
   }
 }
 
