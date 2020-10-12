e163d6aaadd1eb9151c155418f4ff2dfec4fd9ef
ts-3156 <ts_3156@yahoo.co.jp>
2020-10-12 15:18:59 +0900

    Specify id to close button

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -15,7 +15,7 @@ module ModalHelper
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title">#{title}</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <button type="button" id="close-#{id}" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
