52b0dcd19b8040deeaf6a02be263f270af17c952
ts-3156 <ts_3156@yahoo.co.jp>
2020-10-23 09:13:56 +0900

    Check #blocked_search? in ApiRequestConcern

--- app/controllers/concerns/api_request_concern.rb
+++ app/controllers/concerns/api_request_concern.rb
@@ -10,9 +10,10 @@ module ApiRequestConcern
     before_action { head :forbidden unless request.headers['HTTP_X_CSRF_TOKEN'] }
 
     before_action -> { valid_uid?(params[:uid]) }
-    before_action -> { twitter_db_user_persisted?(params[:uid]) }
     before_action -> { twitter_user_persisted?(params[:uid]) }
+    before_action -> { twitter_db_user_persisted?(params[:uid]) }
     before_action -> { @twitter_user = TwitterUser.with_delay.latest_by(uid: params[:uid]) }
     before_action -> { !protected_search?(@twitter_user) }
+    before_action -> { !blocked_search?(@twitter_user) }
   end
 end
