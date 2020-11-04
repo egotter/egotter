798567aa3a3c95e31ab55405a8e8aaa03cb24411
ts-3156 <ts_3156@yahoo.co.jp>
2020-11-04 11:29:02 +0900

    Create CloseFriendsOgImage only if the user is signed in

--- app/controllers/api/v1/close_friends_controller.rb
+++ app/controllers/api/v1/close_friends_controller.rb
@@ -3,7 +3,7 @@ module Api
     class CloseFriendsController < ::Api::V1::Base
 
       def show
-        CreateCloseFriendsOgImageWorker.perform_async(@twitter_user.uid) unless from_crawler?
+        CreateCloseFriendsOgImageWorker.perform_async(@twitter_user.uid) if user_signed_in?
         summary
       end
 
