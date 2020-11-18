ec4228cf8d5fb9b1c9e29a092e2c0c2b42b295dd
ts-3156 <ts_3156@yahoo.co.jp>
2020-11-18 14:55:00 +0900

    Call #find instead of #find_by in TrendsController

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -6,12 +6,12 @@ class TrendsController < ApplicationController
   end
 
   def tweets
-    @trend = TrendDecorator.new(Trend.japan.latest_trends.top_n(3).find_by(id: params[:id]))
+    @trend = TrendDecorator.new(Trend.japan.latest_trends.top_n(3).find(params[:id]))
     @tweets = @trend.tweets.take(100)
   end
 
   def download_tweets
-    trend = Trend.japan.latest_trends.top_n(3).find_by(id: params[:id])
+    trend = Trend.japan.latest_trends.top_n(3).find(params[:id])
     data = data_for_download(trend, trend.tweets.take(limit_for_download))
     send_data data, filename: filename_for_download(trend), type: 'text/csv; charset=utf-8'
   end
