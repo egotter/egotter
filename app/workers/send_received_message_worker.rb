ba1b7918cc99c87d17a3528008b8061b6a9010b0
ts-3156 <ts_3156@yahoo.co.jp>
2020-11-22 08:33:01 +0900

    Update a regexp of spam message

--- app/workers/send_received_message_worker.rb
+++ app/workers/send_received_message_worker.rb
@@ -46,6 +46,7 @@ class SendReceivedMessageWorker
         BlockReportConcern::BlockReportProcessor.new(nil, text).restart_requested? ||
         BlockReportConcern::BlockReportProcessor.new(nil, text).received? ||
         BlockReportConcern::BlockReportProcessor.new(nil, text).send_requested? ||
+        SpamMessageConcern::SpamMessageProcessor.new(nil, text).received? ||
         QUICK_REPLIES.any? { |regexp| regexp.match?(text) } ||
         text.match?(/\Aリムられ通知(\s|　)*(送信|受信|継続|今すぐ)?\z/) ||
         text == 'リムられ' ||
