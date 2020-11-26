8306e0334f451ff861f22e34fccad71076597406
ts-3156 <ts_3156@yahoo.co.jp>
2020-11-26 13:46:27 +0900

    Move #words_count_chart to TrendDecorator

--- app/decorators/trend_decorator.rb
+++ app/decorators/trend_decorator.rb
@@ -17,4 +17,28 @@ class TrendDecorator < ApplicationDecorator
   def latest_tweet
     object.tweets.last
   end
+
+  def words_count_chart
+    (words_count || []).map { |w, c| {word: w, count: c} }
+  end
+
+  def times_count_chart(padding: true)
+    return [] if times_count.blank?
+
+    trend_time = object.time
+
+    if padding
+      time = trend_time - 1.day
+
+      while time < trend_time do
+        label_time = time.change(second: 0)
+        if times_count.none? { |t, _| t == label_time.to_i }
+          times_count << [label_time.to_i, 0]
+        end
+        time += 1.minute
+      end
+    end
+
+    times_count.sort_by { |t, _| t }.map { |t, c| [t * 1000, c] }
+  end
 end
