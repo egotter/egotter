6fe3791c1625d4f382f7c88cf0d2c261982870e4
ts-3156 <ts_3156@yahoo.co.jp>
2020-11-28 16:12:22 +0900

    Redirect outputs with exec command

--- bin/cron/ruby.sh
+++ bin/cron/ruby.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-log='/var/egotter/log/cron.log'
+exec >>/var/egotter/log/cron.log 2>&1
 cmd="/usr/local/bin/bundle exec ruby $1"
 
 SECONDS=0
 
-echo -e "$(date) \e[35m$cmd started\e[m" >>$log 2>&1
-cd /var/egotter && RAILS_ENV=production $cmd >>$log 2>&1
-echo -e "$(date) \e[35m$cmd finished elapsed=${SECONDS}\e[m" >>$log 2>&1
+echo -e "$(date) \e[35m$cmd started\e[m"
+cd /var/egotter && RAILS_ENV=production $cmd
+echo -e "$(date) \e[35m$cmd finished elapsed=${SECONDS}\e[m"
