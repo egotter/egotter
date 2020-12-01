c501405a62b2722d402dfa3b5672cb40f59049ab
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-02 04:11:49 +0900

    Redirect by using JS

--- app/controllers/og_images_controller.rb
+++ app/controllers/og_images_controller.rb
@@ -1,24 +1,11 @@
 class OgImagesController < ApplicationController
 
-  TEMPLATE = <<~HTML
-      <html>
-      <meta http-equiv="refresh" content="<%= count %>;URL=<%= url %>">
-      <head>
-      </head>
-    <body>
-      Redirecting...
-    </body>
-      </html>
-  HTML
-
-  RETRIES_REGEXP = /\A[1-3]\z/
-
   before_action { valid_uid?(params[:uid]) }
 
   before_action do
     @retries = params[:retries].to_s
 
-    if !@retries.empty? && !@retries.match?(RETRIES_REGEXP)
+    if !@retries.empty? && !@retries.match?(/\A[1-3]\z/)
       head :not_found
     end
   end
@@ -28,13 +15,14 @@ class OgImagesController < ApplicationController
     twitter_user = TwitterUser.latest_by(uid: uid)
 
     if (url = twitter_user&.close_friends_og_image&.cdn_url)
-      html = ERB.new(TEMPLATE).result_with_hash(count: 0, url: url)
-      render html: html.html_safe, status: :found
+      @count = 0
+      @url = url
+      render status: :found
     else
+      @count = 5
       retries = @retries.empty? ? 1 : @retries.to_i + 1
-      url = og_image_path(uid: uid, retries: retries)
-      html = ERB.new(TEMPLATE).result_with_hash(count: 5, url: url)
-      render html: html.html_safe, status: :found
+      @url = og_image_path(uid: uid, retries: retries)
+      render status: :found
     end
   rescue => e
     head :not_found
