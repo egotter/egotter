ef50e50f3aeaf259c20b854473831ee37b71a7d1
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-07 23:32:30 +0900

    Disable the end-trial (or cancel) button immediately after being clicked

--- app/javascript/packs/end_trial_modal.js
+++ app/javascript/packs/end_trial_modal.js
@@ -1,13 +1,26 @@
 class EndTrialModal {
-  constructor(id) {
+  constructor(id, i18n) {
     this.$el = $('#' + id);
+    this.i18n = i18n;
     this.init();
   }
 
   init() {
     var url = '/api/v1/orders/end_trial'; // api_v1_orders_end_trial_path
+    var button = null;
+    var i18n = this.i18n;
+
+    this.$el.on('show.bs.modal', function (e) {
+      button = e.relatedTarget;
+    });
 
     this.$el.find('.positive').on('click', function () {
+      if (button) {
+        $(button).addClass('disabled').attr('disabled', 'disabled').prop("disabled", true);
+      }
+
+      ToastMessage.info(i18n['processing']);
+
       $.post(url).done(function (res) {
         ToastMessage.info(res.message);
         setTimeout(function () {
--- app/javascript/packs/orders.js
+++ app/javascript/packs/orders.js
@@ -1,11 +1,15 @@
 class Order {
-  constructor(id) {
+  constructor(id, i18n) {
     this.id = id;
+    this.i18n = i18n;
   }
 
   cancel() {
     var url = '/api/v1/orders/cancel'; // api_v1_orders_cancel_path
     var id = this.id;
+    var i18n = this.i18n;
+
+    ToastMessage.info(i18n['processing']);
 
     $.post(url, {id: id}).done(function (res) {
       ToastMessage.info(res.message);
