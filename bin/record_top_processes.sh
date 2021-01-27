625dc2b15a4a99670b6f83d0b6b8dd5cef7c1d48
ts-3156 <ts_3156@yahoo.co.jp>
2021-01-28 08:32:16 +0900

    Update record_top_processes.sh

--- bin/record_top_processes.sh
+++ bin/record_top_processes.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+exec >>top_processes.log 2>&1
+
 for i in `seq 1 60`; do
   echo "-------- start `date`"
-  ps aux -L --sort=-%cpu | head -n 50 >>top_processes.log
+  ps aux -L --sort=-%cpu | head -n 50
   echo '-------- end'
   sleep 1
 done
