161375646848ba8062389622864f182d7c81a00c
ts-3156 <ts_3156@yahoo.co.jp>
2021-01-29 04:37:03 +0900

    Record top 5 records (was 50)

--- bin/record_top_processes.sh
+++ bin/record_top_processes.sh
@@ -2,9 +2,9 @@
 
 exec >>top_processes.log 2>&1
 
-for i in `seq 1 60`; do
+for i in `seq 1 59`; do
   echo "-------- start `date`"
-  ps aux -L --sort=-%cpu | head -n 50
+  ps aux -L --sort=-%cpu | head -n 5
   echo '-------- end'
   sleep 1
 done
