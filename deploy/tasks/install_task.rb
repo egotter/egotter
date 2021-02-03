266c236f7504e9818cfb847b06f6f728fb94d2a0
ts-3156 <ts_3156@yahoo.co.jp>
2021-02-04 00:50:37 +0900

    Add a line break to crontab

--- deploy/tasks/install_task.rb
+++ deploy/tasks/install_task.rb
@@ -140,6 +140,7 @@ module Tasks
 
         upload_file('./setup/var/spool/cron/ec2-user', '/var/spool/cron/ec2-user')
         backend('sudo chown ec2-user:ec2-user /var/spool/cron/ec2-user')
+        backend('sudo chmod 644 /var/spool/cron/ec2-user')
 
         self
       end
