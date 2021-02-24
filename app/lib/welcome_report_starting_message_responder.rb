5eb4c7906bfb8b01646ca4cc8fb79e6238a642c8
ts-3156 <ts_3156@yahoo.co.jp>
2021-02-24 09:55:48 +0900

    Add WelcomeReportStartingMessageResponder

---
+++ app/lib/welcome_report_starting_message_responder.rb
@@ -0,0 +1,22 @@
+class WelcomeReportStartingMessageResponder < AbstractMessageResponder
+
+  def processor_class
+    Processor
+  end
+
+  class Processor
+    include AbstractReportProcessor
+
+    def message_length
+      20
+    end
+
+    def received_regexp
+      /初期設定(\s|　)*開始/
+    end
+
+    def send_message
+      CreateWelcomeReportStartingMessageWorker.perform_async(@uid)
+    end
+  end
+end
---
+++ app/workers/create_welcome_report_starting_message_worker.rb
@@ -0,0 +1,26 @@
+class CreateWelcomeReportStartingMessageWorker
+  include Sidekiq::Worker
+  include ReportErrorHandler
+  sidekiq_options queue: 'messaging', retry: 0, backtrace: false
+
+  def unique_key(uid, options = {})
+    uid
+  end
+
+  def unique_in
+    30.seconds
+  end
+
+  # options:
+  def perform(uid, options = {})
+    unless (user = User.select(:id).find_by(uid: uid))
+      # TODO Send a message
+    end
+
+    CreateWelcomeMessageWorker.perform_async(user.id)
+  rescue => e
+    unless ignorable_report_error?(e)
+      logger.warn "#{e.inspect} uid=#{uid} options=#{options.inspect}"
+    end
+  end
+end
