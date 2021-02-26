a71aa9f2c551d0558e41c37dde0a969ff840761d
ts-3156 <ts_3156@yahoo.co.jp>
2021-02-26 10:52:57 +0900

    Show dm-confirmation announcement if new user is signed in

---
+++ app/helpers/welcome_reports_helper.rb
@@ -0,0 +1,7 @@
+module WelcomeReportsHelper
+  def show_dm_confirmation_announcement?
+    user_signed_in? &&
+        current_user.created_at >= 1.day.ago &&
+        !PeriodicReport.messages_allotted?(current_user)
+  end
+end
