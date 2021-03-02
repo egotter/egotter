285c4c8c7c6ffd6f11408d6f86af180559a73d0c
ts-3156 <ts_3156@yahoo.co.jp>
2021-03-02 13:18:42 +0900

    Show dm-confirmation announcement to visitors

--- app/helpers/welcome_reports_helper.rb
+++ app/helpers/welcome_reports_helper.rb
@@ -1,7 +1,5 @@
 module WelcomeReportsHelper
   def show_dm_confirmation_announcement?
-    user_signed_in? &&
-        current_user.created_at >= 1.day.ago &&
-        !PeriodicReport.messages_allotted?(current_user)
+    !user_signed_in? || (current_user.created_at >= 1.day.ago && !PeriodicReport.messages_allotted?(current_user))
   end
 end
