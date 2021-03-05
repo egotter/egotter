95c8a562006d61e38a93f4a91a3d08b846521955
ts-3156 <ts_3156@yahoo.co.jp>
2021-03-06 08:15:10 +0900

    Use new og-image

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -229,17 +229,6 @@ module PathsHelper
     elsif controller_name == 'close_friends' && @twitter_user&.close_friends_og_image
       @twitter_user.close_friends_og_image.lambda_url
     else
-      image_url('/egotter_usagi_840x450.jpg?loc=og_image')
-    end
-  end
-
-  # For debugging
-  def meta_og_image2_url
-    if controller_name == 'personality_insights'
-      image_url('/egotter_personality_insight_840x450.jpg?loc=og_image')
-    elsif controller_name == 'close_friends' && @twitter_user&.close_friends_og_image
-      @twitter_user.close_friends_og_image.lambda_url
-    else
       image_url('/egotter_like_app_store_840x450.png?loc=og_image')
     end
   end
