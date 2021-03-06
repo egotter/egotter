ef64099803daa7a6781e628fc895b5597f888a45
ts-3156 <ts_3156@yahoo.co.jp>
2021-03-06 20:59:02 +0900

    Update SendReceivedMessageWorker#dont_send_message?

--- app/workers/send_received_message_worker.rb
+++ app/workers/send_received_message_worker.rb
@@ -31,7 +31,8 @@ class SendReceivedMessageWorker
         WelcomeReportResponder::Processor.new(nil, text).received? ||
         SpamMessageConcern::SpamMessageProcessor.new(nil, text).received? ||
         QUICK_REPLIES.any? { |regexp| regexp.match?(text) } ||
-        text == '今すぐ' ||
+        text.include?('送信】をDMにコピペして送ってください') ||
+        text.include?('送ると、今すぐリムられ通知を受信することができます') ||
         text == 'あ' ||
         text == 'は' ||
         text == 'り'
