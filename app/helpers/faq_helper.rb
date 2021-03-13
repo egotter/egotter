abf78e3a3680513c4d9166bc0e2f0d613d83f2e7
ts-3156 <ts_3156@yahoo.co.jp>
2021-03-13 15:31:36 +0900

    Update FaqHelper

--- app/helpers/faq_helper.rb
+++ app/helpers/faq_helper.rb
@@ -1,31 +1,25 @@
 module FaqHelper
-  def faq_id(value)
-    (@faq_ids ||= []) << (value == :auto ? "faq-id-#{@faq_ids.size}" : value)
+  def faq_section(value, &block)
+    @faq_section = value
+    @faq_questions = []
+
+    yield
+
+    @faq_sections ||= {}
+    @faq_sections[value] = @faq_questions
   end
 
-  def faq_question(value)
-    (@faq_questions ||= []) << value
+  def faq_contents(id, value)
+    @faq_contents ||= Hash.new { |h, k| h[k] = [] }
+    @faq_contents[@faq_section] << {id: id, text: value}
   end
 
-  def faq_answer(&block)
-    (@faq_answers ||= []) << capture(&block)
+  def faq_generate_id(value)
+    value == :auto ? "faq-id-#{@faq_questions.size}" : value
   end
 
-  def faq_ids
-    @faq_ids
-  end
-
-  def faq_questions
-    @faq_questions
-  end
-
-  def faq_answers
-    @faq_answers
-  end
-
-  def flush_faq
-    @faq_ids = nil
-    @faq_questions = nil
-    @faq_answers = nil
+  def faq_question(value, id: :auto, &block)
+    @faq_questions << {id: faq_generate_id(id), text: value, answer: capture(&block)}
+    faq_contents(id, value)
   end
 end
