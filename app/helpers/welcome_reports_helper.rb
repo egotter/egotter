4192e6204e95c8bc66fb10d9bfd10af82ccf7458
ts-3156 <ts_3156@yahoo.co.jp>
2021-03-17 00:22:46 +0900

    DM confirmation message is only shown on settings page

--- app/helpers/welcome_reports_helper.rb
+++ app/helpers/welcome_reports_helper.rb
@@ -1,9 +1,6 @@
 module WelcomeReportsHelper
   def show_dm_confirmation_announcement?
-    top = controller_name == 'home' && action_name == 'new'
-
-    if !top && controller_name != 'access_confirmations'
-      !user_signed_in? || (current_user.created_at >= 1.day.ago && !PeriodicReport.messages_allotted?(current_user))
-    end
+    timeline_page? &&
+        (!user_signed_in? || (current_user.created_at >= 1.day.ago && !PeriodicReport.messages_allotted?(current_user)))
   end
 end
