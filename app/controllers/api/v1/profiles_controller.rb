c44279e78e9f439853fd0a00951f8ef9e9ce1af9
ts-3156 <ts_3156@yahoo.co.jp>
2021-04-13 18:25:42 +0900

    Refactor Api::V1::ProfilesController

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -10,13 +10,18 @@ module Api
       before_action { head :forbidden unless request.headers['HTTP_X_CSRF_TOKEN'] }
 
       before_action { valid_uid?(params[:uid]) }
+      before_action { set_user(params[:uid]) }
 
       def show
-        if (user = TwitterDB::User.find_by(uid: params[:uid]))
-          html = render_to_string(partial: 'twitter/profile', locals: {user: user, body_expanded: params['expanded'] == 'true'}, formats: [:html])
-          render json: {html: html}
-        else
-          render json: {error: 'not found'}, status: :not_found
+        html = render_to_string(partial: 'twitter/profile', locals: {user: @user, body_expanded: params['expanded'] == 'true'}, formats: [:html])
+        render json: {html: html}
+      end
+
+      private
+
+      def set_user(uid)
+        unless (@user = TwitterDB::User.find_by(uid: uid))
+          head :not_found
         end
       end
     end
