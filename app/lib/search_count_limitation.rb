938057ea2196e1b9cfab064ecef9e360727d42db
ts-3156 <ts_3156@yahoo.co.jp>
2021-04-15 14:00:44 +0900

    Show periodic_tweet_min and periodic_tweet_max

--- app/lib/search_count_limitation.rb
+++ app/lib/search_count_limitation.rb
@@ -4,6 +4,8 @@ class SearchCountLimitation
   SIGN_IN_BONUS = Rails.configuration.x.constants[:search_count_limitation][:sign_in_bonus]
   SHARING_BONUS = Rails.configuration.x.constants[:search_count_limitation][:sharing_bonus]
   PERIODIC_TWEET_BONUS = Rails.configuration.x.constants[:search_count_limitation][:periodic_tweet_bonus]
+  PERIODIC_TWEET_MIN = Rails.configuration.x.constants[:search_count_limitation][:periodic_tweet_min]
+  PERIODIC_TWEET_MAX = Rails.configuration.x.constants[:search_count_limitation][:periodic_tweet_max]
   CONTINUOUS_SIGN_IN_BONUS = Rails.configuration.x.constants[:search_count_limitation][:continuous_sign_in_bonus]
   ANONYMOUS = Rails.configuration.x.constants[:search_count_limitation][:anonymous]
   BASIC_PLAN = Rails.configuration.x.constants[:search_count_limitation][:basic_plan]
