ab77c90acff05747cd5677de8068d96c302429af
ts-3156 <ts_3156@yahoo.co.jp>
2021-04-16 20:19:17 +0900

    Skip :current_user_not_blocker? on PricingController and CheckoutSessionsController

--- app/controllers/api/v1/checkout_sessions_controller.rb
+++ app/controllers/api/v1/checkout_sessions_controller.rb
@@ -2,9 +2,12 @@ module Api
   module V1
     class CheckoutSessionsController < ApplicationController
 
+      skip_before_action :current_user_not_blocker?
+
       before_action :reject_crawler
       before_action :require_login!
       before_action :doesnt_have_valid_subscription!
+
       after_action { track_order_activity(checkout_session: {id: @session&.id, customer: @session&.customer, metadata: @session&.metadata}) }
 
       def create
--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -1,5 +1,7 @@
 class PricingController < ApplicationController
 
+  skip_before_action :current_user_not_blocker?
+
   after_action :track_order_activity
 
   def new
