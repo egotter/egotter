07aa19ec2697fff0f08febc410f0ec50715c0e2c
ts-3156 <ts_3156@yahoo.co.jp>
2021-05-11 13:58:00 +0900

    Display more polite messages when uploading

--- app/javascript/packs/delete_tweets_uploader.js
+++ app/javascript/packs/delete_tweets_uploader.js
@@ -4,54 +4,31 @@ class DeleteTweetsUploader {
     this.$input = $('#' + inputId);
     this.options = options;
     this.i18n = i18n;
-    var self = this;
+    this.errors = [];
 
-    this.$btn.on('click', function () {
-      self.$input.trigger('click');
-      return false;
-    });
-
-    this.$input.on('change', function () {
-      self.$btn.prop('disabled', true).addClass('disabled');
-      var file = self.$input[0].files[0];
-      self.file = file;
-
-      if (self.validate(file)) {
-        self.upload(file);
-      } else {
-        self.$input[0].value = '';
-        self.$btn.prop('disabled', false).removeClass('disabled');
-      }
-    });
+    this.$btn.on('click', this.openFileSelector.bind(this));
+    this.$input.on('change', this.fileSelected.bind(this));
   }
 
-  validate(file) {
-    var self = this;
-    var valid = true;
-    var error;
-
-    if (!file.name.match(/^twitter-20\d{2}-\d{2}-\d{2}-[a-z0-9-]+.zip$/i)) {
-      error = 'invalidFilename';
-      valid = false;
-    } else if (file.type.indexOf('zip') === -1 && file.type.indexOf('octet-stream' === -1)) {
-      error = 'invalidContentType';
-      valid = false;
-    } else if (file.size < 1000000) { // 1MB
-      error = 'filesizeTooSmall';
-      valid = false;
-    } else if (file.size > 30000000000) { // 30GB
-      error = 'filesizeTooLarge';
-      valid = false;
-    }
-
-    if (!valid) {
-      self.showMessage(self.i18n[error]);
-    }
-
-    return valid;
+  openFileSelector() {
+    this.$input.trigger('click');
+    return false;
   }
 
-  upload(file) {
+  fileSelected() {
+    this.$btn.prop('disabled', true).addClass('disabled');
+    var file = new ArchiveFile(this.$input[0].files[0], this.i18n);
+
+    if (file.validate()) {
+      this.startUploading(file);
+    } else {
+      this.$input[0].value = '';
+      this.$btn.prop('disabled', false).removeClass('disabled');
+      showMessage(file.errorMessage(), file.attrs());
+    }
+  }
+
+  startUploading(file) {
     var i18n = this.i18n;
     this.completed = false;
     var self = this;
@@ -62,18 +39,24 @@ class DeleteTweetsUploader {
       }
     };
 
-    this.showMessage(i18n['preparing']);
+    showMessage(i18n['preparing'], file.attrs());
 
-    this.uploadFile(file);
-
-    // this.createPresignedUrl(file, function (url) {
-    //   self.readFile(file, function (data) {
-    //     self.uploadChunk(url, data);
-    //   });
-    // });
+    setTimeout(function () {
+      self.managedUpload(file).then(
+          function () {
+            self.completed = true;
+            self.notifyUploadCompleted(self.metadata);
+            showMessage(i18n['success']);
+          },
+          function (err) {
+            logger.error(err.message);
+            showMessage(i18n['fail'], file.attrs());
+          }
+      );
+    }, 10000);
   }
 
-  uploadFile(file) {
+  managedUpload(file) {
     var i18n = this.i18n;
     var self = this;
 
@@ -87,7 +70,8 @@ class DeleteTweetsUploader {
         var curValue = parseInt((event.loaded * 100) / event.total);
         if (curValue > value) {
           value = curValue;
-          self.showMessage(i18n['uploading'].replace('{value}', value + '%'));
+          var options = Object.assign({value: value + '%'}, file.attrs());
+          showMessage(i18n['uploading'], options);
         }
       };
     })();
@@ -97,10 +81,12 @@ class DeleteTweetsUploader {
       IdentityPoolId: this.options.IdentityPoolId,
     });
 
+    var fileObj = file.getFile();
+
     self.metadata = {
-      filename: file.name,
-      filesize: '' + file.size,
-      filetype: file.type,
+      filename: fileObj.name,
+      filesize: '' + fileObj.size,
+      filetype: fileObj.type,
       since: $('#premium_since_date').val(),
       until: $('#premium_until_date').val()
     };
@@ -110,128 +96,70 @@ class DeleteTweetsUploader {
       params: {
         Bucket: this.options.bucket,
         Key: this.options.key,
-        Body: file,
+        Body: fileObj,
         ACL: 'private',
         Metadata: self.metadata
       }
     });
 
-    var promise = upload.on('httpUploadProgress', onProgress).promise();
-
-    promise.then(
-        function () {
-          self.completed = true;
-          self.notifyUploadCompleted(self.metadata);
-          self.showMessage(i18n['success']);
-        },
-        function (err) {
-          logger.error(err.message);
-          self.showMessage(i18n['fail']);
-        }
-    );
+    return upload.on('httpUploadProgress', onProgress).promise();
   }
-
-  // TODO Remove
-  // readFile(file, callback) {
-  //   var i18n = this.i18n;
-  //   var offset = 0;
-  //   var size = 10_000_000_000; // 10GB
-  //   var reader = new FileReader();
-  //
-  //   var readChunk = function () {
-  //     var slice = file.slice(offset, offset + size, file.type);
-  //     offset += size;
-  //     reader.readAsArrayBuffer(slice);
-  //   };
-  //
-  //   reader.onload = function (e) {
-  //     callback(e.target.result);
-  //     if (offset >= file.size) {
-  //       logger.log('#readFile completed');
-  //     } else {
-  //       readChunk();
-  //     }
-  //   };
-  //
-  //   reader.onerror = function (e) {
-  //     logger.error(e.target.error.name);
-  //     ToastMessage.warn(i18n['fail'], {autohide: false});
-  //   };
-  //
-  //   readChunk();
-  // }
-
-  // TODO Remove
-  // uploadChunk(url, data) {
-  //   var i18n = this.i18n;
-  //   var self = this;
-  //
-  //   var onProgress = (function () {
-  //     var value = 0;
-  //     return function (e) {
-  //       if (self.completed) {
-  //         return;
-  //       }
-  //
-  //       if (e.lengthComputable) {
-  //         value = Math.floor(100 * e.loaded / e.total);
-  //       } else {
-  //         value += Math.floor(Math.random() * 6); // 0-5
-  //         if (value > 99) {
-  //           value = 99;
-  //         }
-  //       }
-  //       ToastMessage.info(i18n['uploading'].replace('{value}', value + '%'), {autohide: false});
-  //     };
-  //   })();
-  //
-  //   $.ajax({
-  //     url: url,
-  //     data: data,
-  //     type: 'PUT',
-  //     xhr: function () {
-  //       var xhr = new window.XMLHttpRequest();
-  //       xhr.upload.addEventListener('progress', onProgress, false);
-  //       return xhr;
-  //     },
-  //     processData: false,
-  //     contentType: false
-  //   }).done(function () {
-  //     self.completed = true;
-  //     self.notifyUploadCompleted();
-  //     ToastMessage.info(i18n['success'], {autohide: false});
-  //   }).fail(function () {
-  //     ToastMessage.warn(i18n['fail'], {autohide: false});
-  //   });
-  // }
-
-  // TODO Remove
-  // createPresignedUrl(file, callback) {
-  //   var url = '/api/v1/delete_tweets_presigned_urls'; //  api_v1_delete_tweets_presigned_urls_path
-  //   $.post(url, {filename: file.name, filesize: file.size}).done(function (res) {
-  //     callback(res.url);
-  //   }).fail(showErrorMessage);
-  // }
 
   notifyUploadCompleted(metadata) {
     var url = '/api/v1/delete_tweets_notifications'; //  api_v1_delete_tweets_notifications_path
     $.post(url, metadata).done(function () {
     }).fail(showErrorMessage);
   }
+}
 
-  showMessage(message) {
-    if (this.file) {
-      try {
-        message = message
-            .replace('{name}', this.escapeHtml(this.file.name))
-            .replace('{type}', this.escapeHtml(this.file.type))
-            .replace('{size}', this.escapeHtml('' + this.file.size));
-      } catch (e) {
-        console.error(e);
-      }
+window.DeleteTweetsUploader = DeleteTweetsUploader;
+
+class ArchiveFile {
+  constructor(file, i18n) {
+    this.file = file;
+    this.i18n = i18n;
+    this.errors = [];
+  }
+
+  validate() {
+    var file = this.file;
+    this.errors = [];
+
+    if (!file.name.match(/^twitter-20\d{2}-\d{2}-\d{2}-[a-z0-9-]+.zip$/i)) {
+      this.errors = ['invalidFilename'];
+    } else if (file.type.indexOf('zip') === -1 && file.type.indexOf('octet-stream' === -1)) {
+      this.errors = ['invalidContentType'];
+    } else if (file.size < 1000000) { // 1MB
+      this.errors = ['filesizeTooSmall'];
+    } else if (file.size > 30000000000) { // 30GB
+      this.errors = ['filesizeTooLarge'];
     }
 
-    ToastMessage.info(message, {autohide: false});
+    return this.errors.length === 0;
+  }
+
+  errorMessage() {
+    if (this.errors.length === 0) {
+      return;
+    }
+
+    var key = this.errors[0];
+    var message = this.i18n[key];
+
+    if (!message) {
+      logger.warn('Message not found', key, message, this.errors);
+      return key;
+    }
+
+    return message;
+  }
+
+  getFile() {
+    return this.file;
+  }
+
+  attrs() {
+    return {name: this.file.name, type: this.file.type, size: this.file.size};
   }
 
   escapeHtml(str) {
@@ -252,4 +180,29 @@ class DeleteTweetsUploader {
   }
 }
 
-window.DeleteTweetsUploader = DeleteTweetsUploader;
+
+function showMessage(message, options) {
+  if (options) {
+    Object.keys(options).forEach(function (key) {
+      message = message.replace('{' + key + '}', escapeHtml('' + options[key]));
+    });
+  }
+  ToastMessage.info(message, {autohide: false});
+}
+
+function escapeHtml(str) {
+  if (str.length >= 30) {
+    str = str.substr(0, 30) + '...';
+  }
+
+  var type = '';
+  try {
+    type = str.replace(/<\/?[^>]+(>|$)/g, '');
+    if (!type) {
+      type = 'empty';
+    }
+  } catch (e) {
+    type = 'error';
+  }
+  return type;
+}
