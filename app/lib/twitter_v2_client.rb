f62df65962b7c27b4bf1137139506a46d693cf0e
ts-3156 <ts_3156@yahoo.co.jp>
2021-05-24 03:18:41 +0900

    Implement #unlike_tweet

--- app/lib/twitter_v2_client.rb
+++ app/lib/twitter_v2_client.rb
@@ -1,15 +1,23 @@
 class TwitterV2Client
-  def initialize(client = nil, user_id: nil)
-    @client = client || User.find(user_id).api_client.twitter
+  def initialize(uid: nil, screen_name: nil)
+    if uid
+      user = User.find_by(uid: uid)
+    else
+      user = User.find_by(screen_name: screen_name)
+    end
+
+    @uid = user.uid
+    @client = user.api_client.twitter
   end
 
-  def liked_tweets(user, options = {})
-    max_results = options[:count] || 100
+  def liked_tweets(user = nil, count: 100)
+    user ||= @uid
+    max_results = count
     collection = []
     next_token = nil
 
     50.times do
-      res = fetch(user, pagination_token: next_token)
+      res = fetch_liked_tweets(user, pagination_token: next_token)
       tweets = res[:data]
       break if tweets.nil? || tweets.empty?
 
@@ -24,11 +32,13 @@ class TwitterV2Client
     collection.take(max_results)
   end
 
+  def unlike_tweet(tweet_id)
+    http_delete("/2/users/#{@uid}/likes/#{tweet_id}")[:data]
+  end
+
   private
 
-  def fetch(user, query_params)
-    retries ||= 0
-
+  def fetch_liked_tweets(user, query_params)
     path = "/2/users/#{user}/liked_tweets?" + query_params.compact.merge(
         expansions: 'author_id',
         'tweet.fields' => ['created_at'].join(','),
@@ -36,7 +46,20 @@ class TwitterV2Client
         max_results: 100,
     ).to_query
 
-    request = ::Twitter::REST::Request.new(@client, :get, path)
+    http_get(path)
+  end
+
+  def http_get(path)
+    http_request(:get, path)
+  end
+
+  def http_delete(path)
+    http_request(:delete, path)
+  end
+
+  def http_request(method, path)
+    retries ||= 0
+    request = ::Twitter::REST::Request.new(@client, method, path)
     request.perform
   rescue => e
     if ServiceStatus.retryable_error?(e)
