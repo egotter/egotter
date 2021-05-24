5200637d339d747f0ce517cdaf26dfc7d6ed6553
ts-3156 <ts_3156@yahoo.co.jp>
2021-05-24 09:59:14 +0900

    Implement TwitterV2Client#tweets

--- app/lib/twitter_v2_client.rb
+++ app/lib/twitter_v2_client.rb
@@ -10,6 +10,11 @@ class TwitterV2Client
     @client = user.api_client.twitter
   end
 
+  def tweets(tweet_ids)
+    res = fetch_tweets(tweet_ids)
+    build_tweets_from_response(res)
+  end
+
   def liked_tweets(user = nil, count: 100)
     user ||= @uid
     max_results = count
@@ -18,11 +23,9 @@ class TwitterV2Client
 
     50.times do
       res = fetch_liked_tweets(user, pagination_token: next_token)
-      tweets = res[:data]
-      break if tweets.nil? || tweets.empty?
+      break if res[:data].nil? || res[:data].empty?
 
-      users = res.dig(:includes, :users).index_by { |u| u[:id] }
-      tweets.each { |t| t[:user] = users[t[:author_id]] }
+      tweets = build_tweets_from_response(res)
       collection.concat(tweets)
 
       break if collection.size >= max_results
@@ -38,6 +41,18 @@ class TwitterV2Client
 
   private
 
+  def fetch_tweets(tweet_ids)
+    path = "/2/tweets?" + {
+        ids: tweet_ids.join(','),
+        expansions: ['author_id', 'attachments.media_keys'].join(','),
+        'tweet.fields' => ['created_at', 'public_metrics', 'non_public_metrics', 'organic_metrics'].join(','),
+        'user.fields' => ['id', 'name'].join(','),
+        'media.fields' => ['non_public_metrics', 'organic_metrics'].join(','),
+    }.to_query
+
+    http_get(path)
+  end
+
   def fetch_liked_tweets(user, query_params)
     path = "/2/users/#{user}/liked_tweets?" + query_params.compact.merge(
         expansions: 'author_id',
@@ -47,6 +62,12 @@ class TwitterV2Client
     ).to_query
 
     http_get(path)
+  end
+
+  def build_tweets_from_response(res)
+    users = res.dig(:includes, :users).index_by { |u| u[:id] }
+    res[:data].each { |t| t[:user] = users[t[:author_id]] }
+    res[:data]
   end
 
   def http_get(path)
