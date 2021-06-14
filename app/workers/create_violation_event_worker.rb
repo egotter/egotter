24330a4ad06826f118c8595f742528c5fb47828a
ts-3156 <ts_3156@yahoo.co.jp>
2021-06-15 07:50:40 +0900

    Add CreateViolationEventWorker 2

---
+++ app/workers/create_violation_event_worker.rb
@@ -0,0 +1,20 @@
+class CreateViolationEventWorker
+  include Sidekiq::Worker
+  include WorkerErrorHandler
+  sidekiq_options queue: 'creating_low', retry: 0, backtrace: false
+
+  def unique_key(user_id, options = {})
+    user_id
+  end
+
+  def unique_in
+    1.minute
+  end
+
+  # options:
+  def perform(user_id, name, options = {})
+    ViolationEvent.create(user_id: user_id, name: name)
+  rescue => e
+    handle_worker_error(e, user_id: user_id, name: name, **options)
+  end
+end
