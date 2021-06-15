d42d97382d7cbff23b7b620dbc87f4807a41c9a1
ts-3156 <ts_3156@yahoo.co.jp>
2021-06-15 11:01:13 +0900

    Create ViolationEvent in SpamMessageConcern

--- app/controllers/concerns/spam_message_concern.rb
+++ app/controllers/concerns/spam_message_concern.rb
@@ -3,6 +3,9 @@ module SpamMessageConcern
     processor = SpamMessageProcessor.new(dm.sender_id, dm.text)
 
     if processor.received?
+      if (user = User.find_by(uid: dm.sender_id))
+        CreateViolationEventWorker.perform_async(user.id, 'Spam message')
+      end
       processor.send_message
       return true
     end
@@ -17,8 +20,10 @@ module SpamMessageConcern
       10
     end
 
+    REGEXP = /死ね|殺す|黙れ|馬鹿|役立たず|無能|やくたたず|きいてんのか|いいかげんに|うるせーよ|うるせ[えぇ]|うっせえ|気持ち悪い|キモい|きもい|きしょい|うるせえよ|うるさい|(^(しね|くず|うざ|くそ|ころす|きえろ|ばか|だまれ|かえれ|こら|おい|きも|ごみ|ゴミ|バカ|ちんぽ|ちんこ|まんこ|うんち|うんこ)$)/
+
     def received_regexp
-      /死ね|殺す|黙れ|馬鹿|役立たず|無能|やくたたず|きいてんのか|いいかげんに|うるせーよ|うるせ[えぇ]|うっせえ|気持ち悪い|キモい|きもい|きしょい|うるせえよ|うるさい|(^(しね|くず|うざ|くそ|ころす|きえろ|ばか|だまれ|かえれ|こら|おい|きも|ごみ|ゴミ|バカ|ちんぽ|ちんこ|まんこ|うんち|うんこ)$)/
+      REGEXP
     end
 
     def send_message
