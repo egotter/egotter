a007ffaf1989c0df3aef33e28f5bad8409aa7018
ts-3156 <ts_3156@yahoo.co.jp>
2021-06-22 18:28:05 +0900

    Fix TypeError

--- bin/notify_spot_instance_interruption.rb
+++ bin/notify_spot_instance_interruption.rb
@@ -5,15 +5,6 @@ require 'uri'
 require 'json'
 require 'dotenv/load'
 
-class Slack
-  class << self
-    def post(text)
-      uri = URI.parse(ENV['SLACK_NOTIFY_INTERRUPTION_URL'])
-      puts Net::HTTP.post_form(uri, payload: {text: text}.to_json).body
-    end
-  end
-end
-
 class App
   def marked_to_be_terminated?
     resp = `curl -s -H "X-aws-ec2-metadata-token: #{token}" http://169.254.169.254/latest/meta-data/spot/instance-action`
@@ -42,38 +33,47 @@ class App
   def token
     @token ||= `curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 3600"`
   end
-end
 
-class Process
-  def initialize(process_name)
-    @process_name = process_name
-  end
-
-  def stop
-    if `sudo status #{@process_name}`.include?('start/running')
-      `sudo stop #{@process_name}`
+  class Slack
+    class << self
+      def post(text)
+        uri = URI.parse(ENV['SLACK_NOTIFY_INTERRUPTION_URL'])
+        puts Net::HTTP.post_form(uri, payload: {text: text}.to_json).body
+      end
     end
   end
-end
 
-class WebProcess
-  def initialize(instance_id)
-    @instance_id = instance_id
+  class Process
+    def initialize(process_name)
+      @process_name = process_name
+    end
+
+    def stop
+      if `sudo status #{@process_name}`.include?('start/running')
+        `sudo stop #{@process_name}`
+      end
+    end
   end
 
-  def stop
-    deregister
-    Process.new('puma').stop
-  end
+  class WebProcess
+    def initialize(instance_id)
+      @instance_id = instance_id
+    end
 
-  private
+    def stop
+      deregister
+      Process.new('puma').stop
+    end
 
-  def deregister
-    `aws elbv2 deregister-targets --target-group-arn #{target_group} --targets Id=#{@instance_id} --region ap-northeast-1`
-  end
+    private
 
-  def target_group
-    `aws elbv2 describe-target-groups --region ap-northeast-1 --names egotter --query="TargetGroups[0].TargetGroupArn"`.gsub(/["\n]/, '')
+    def deregister
+      `aws elbv2 deregister-targets --target-group-arn #{target_group} --targets Id=#{@instance_id} --region ap-northeast-1`
+    end
+
+    def target_group
+      `aws elbv2 describe-target-groups --region ap-northeast-1 --names egotter --query="TargetGroups[0].TargetGroupArn"`.gsub(/["\n]/, '')
+    end
   end
 end
 
@@ -81,12 +81,12 @@ def main
   app = App.new
   return unless app.marked_to_be_terminated?
 
-  Slack.post("This instance is marked to be terminated app=#{app}")
+  App::Slack.post("This instance is marked to be terminated app=#{app}")
 
   app.stop
-  Slack.post("The app is stopped app=#{app}")
+  App::Slack.post("The app is stopped app=#{app}")
 rescue => e
-  Slack.post("An error occurred exception=#{e.inspect}")
+  App::Slack.post("An error occurred exception=#{e.inspect}")
 end
 
 if __FILE__ == $0
