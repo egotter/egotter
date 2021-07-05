75d2de8ee4d7701cefbe53b4ae73234127de6c54
ts-3156 <ts_3156@yahoo.co.jp>
2021-07-06 06:31:14 +0900

    Draw an stacked chart on app_stats page

--- app/controllers/app_stats_controller.rb
+++ app/controllers/app_stats_controller.rb
@@ -10,12 +10,14 @@ class AppStatsController < ApplicationController
 
   def generate_chart_data
     time = Time.zone.now.in_time_zone('Asia/Tokyo') - 1.day
-    previous_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time')
+    previous_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time').map(&:cnt)
+    previous_data = previous_data.map.with_index { |_, i| previous_data.take(i + 1).sum }
 
     time = Time.zone.now.in_time_zone('Asia/Tokyo')
-    current_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time')
+    current_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time').map(&:cnt)
+    current_data = current_data.map.with_index { |_, i| current_data.take(i + 1).sum }
 
-    [previous_data.map(&:cnt), current_data.map(&:cnt)]
+    [previous_data, current_data]
   rescue => e
     [[], []]
   end
