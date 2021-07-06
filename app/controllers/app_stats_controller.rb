79abdbfd7b618c1225ca212737ecfaba86638729
ts-3156 <ts_3156@yahoo.co.jp>
2021-07-07 03:56:26 +0900

    Simple chart on app_stats page

--- app/controllers/app_stats_controller.rb
+++ app/controllers/app_stats_controller.rb
@@ -9,16 +9,34 @@ class AppStatsController < ApplicationController
   private
 
   def generate_chart_data
-    time = Time.zone.now.in_time_zone('Asia/Tokyo') - 1.day
-    previous_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time').map(&:cnt)
-    previous_data = previous_data.map.with_index { |_, i| previous_data.take(i + 1).sum }
+    time = Time.zone.now.in_time_zone('Asia/Tokyo').beginning_of_day
+    x_times = 24.times.map { |n| time + n.hours }
+    x_labels = x_times.map.with_index do |t, i|
+      t = t.change(min: 59) if i == x_times.size - 1
+      t.strftime('%H:%M')
+    end
 
-    time = Time.zone.now.in_time_zone('Asia/Tokyo')
-    current_data = User.select('gd_hour(created_at, "UTC") time, count(*) cnt').where(created_at: time.beginning_of_day..time.end_of_day).group('time').map(&:cnt)
-    current_data = current_data.map.with_index { |_, i| current_data.take(i + 1).sum }
-
-    [previous_data, current_data]
+    {
+        categories: x_labels,
+        series: [time_to_points(time - 24.hours), time_to_points(time)]
+    }
   rescue => e
-    [[], []]
+    logger.warn "#{__method__}: #{e.inspect}"
+    logger.info e.backtrace.join("\n")
+    {error: e.inspect}
+  end
+
+  def time_to_points(time)
+    now = Time.zone.now
+    values = 24.times.map do |n|
+      start_time = time + n.hours
+      end_time = start_time + 1.hour - 1.second
+      if now < start_time
+        nil
+      else
+        SearchLog.where(created_at: start_time..end_time).size
+      end
+    end
+    values.map.with_index { |v, i| v ? values.take(i + 1).sum : nil }
   end
 end
