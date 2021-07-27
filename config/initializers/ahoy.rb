37a3f53e2e8229bab77d5a87e01869c54b8b2a0b
ts-3156 <ts_3156@yahoo.co.jp>
2021-07-27 19:40:03 +0900

    Remove Ahoy.exclude_method

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -12,16 +12,6 @@ Ahoy.user_agent_parser = :device_detector
 
 Ahoy.track_bots = true
 
-Ahoy.exclude_method = lambda do |controller, request|
-  # After signing in, Devise calls callbacks without controller.
-  unless controller.nil?
-    controller.from_crawler?
-  end
-rescue => e
-  Rails.logger.warn "in ahoy.rb something happened. #{e.class} #{e.message} #{controller.class} #{request.fullpath} #{request.referer}"
-  raise
-end
-
 Ahoy.visit_duration = 30.minutes
 
 Ahoy.geocode = false
