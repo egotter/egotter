457f5decab6de41e8e80ed61a4ea0a03eca384c0
ts-3156 <ts_3156@yahoo.co.jp>
2021-08-31 05:22:10 +0900

    Improve #invited_user_ids

--- app/lib/click_id_generator.rb
+++ app/lib/click_id_generator.rb
@@ -17,11 +17,17 @@ class ClickIdGenerator
 
     def invited_user_ids(user)
       regexp = "^invitation-\\d+-#{user ? user.uid : '\\d+'}$"
-      Ahoy::Event.select('distinct user_id').
+
+      query = Ahoy::Event.select('distinct user_id').
           where('time > ?', 2.weeks.ago).
           where(name: 'Sign up').
-          where("properties->>'$.click_id' regexp ?", regexp).
-          map(&:user_id)
+          where("properties->>'$.click_id' regexp ?", regexp)
+
+      if (min_user = User.select(:id).where('created_at > ?', 2.weeks.ago).order(created_at: :asc)[0])
+        query = query.where('user_id >= ?', min_user.id)
+      end
+
+      query.map(&:user_id)
     end
 
     def invitation_bonus
