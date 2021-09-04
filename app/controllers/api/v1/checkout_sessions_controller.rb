5934f7a9c28699962a83c4bdd1bd2c53df70b8a1
ts-3156 <ts_3156@yahoo.co.jp>
2021-09-04 19:55:42 +0900

    Track button id

--- app/controllers/api/v1/checkout_sessions_controller.rb
+++ app/controllers/api/v1/checkout_sessions_controller.rb
@@ -25,7 +25,7 @@ module Api
 
       # TODO Remove later
       def send_message(session_id)
-        message = "user_id=#{current_user.id} checkout_session_id=#{session_id} via=#{params[:via]} referer=#{request.referer.to_s.truncate(200)}"
+        message = "user_id=#{current_user.id} checkout_session_id=#{session_id} button_id=#{params[:button_id]} via=#{params[:via]} referer=#{request.referer.to_s.truncate(200)}"
         SlackMessage.create(channel: 'orders_cs_created', message: message)
         SendMessageToSlackWorker.perform_async(:orders_cs_created, "`#{Rails.env}` #{message}")
       end
