afabdd1781acd09b1277a6196e1a8e36139aef35
ts-3156 <ts_3156@yahoo.co.jp>
2021-09-04 19:50:13 +0900

    Track an event of pricing page if a user is signed in

--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -2,7 +2,7 @@ class PricingController < ApplicationController
 
   skip_before_action :current_user_not_blocker?
 
-  after_action :track_order_activity
+  after_action :track_order_activity, if: -> { user_signed_in? }
 
   def new
   end
