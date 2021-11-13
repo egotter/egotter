f8876c7e2a0d8384653ae5cfcab076c4f8f0e90c
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-13 11:09:37 +0900

    Don't update data when accessing remove related pages

--- app/controllers/mutual_unfriends_controller.rb
+++ app/controllers/mutual_unfriends_controller.rb
@@ -7,7 +7,6 @@ class MutualUnfriendsController < ApplicationController
   include JobQueueingConcern
 
   def show
-    @jid = enqueue_create_twitter_user_job_if_needed(@twitter_user.uid, user_id: current_user_id)
     @active_tab = 2
     render template: 'result_pages/show' unless performed?
   end
--- app/controllers/unfriends_controller.rb
+++ app/controllers/unfriends_controller.rb
@@ -10,7 +10,6 @@ class UnfriendsController < ApplicationController
   end
 
   def show
-    @jid = enqueue_create_twitter_user_job_if_needed(@twitter_user.uid, user_id: current_user_id)
     @active_tab = 0
     render template: 'result_pages/show' unless performed?
   end
