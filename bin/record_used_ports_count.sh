43e5e28e5f6f861622218f0758fbcb3a5b92d767
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-24 20:31:16 +0900

    Fix typo

--- bin/record_used_ports_count.sh
+++ bin/record_used_ports_count.sh
@@ -2,6 +2,3 @@
 
 counts=$(netstat -nt | awk "/^tcp/{print \$6}" | sort | uniq -c | sed -e 's/^ \+//' | sort -rV | tr '\n' ', ')
 echo "$(date +'%Y/%m/%d %H:%M') $counts">>/var/egotter/log/used_port_counts.log
-
-aaa
-
