1144c7da9554319ac440e02eb16321d966641770
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-24 19:10:32 +0900

    Remove spaces

--- bin/record_used_ports_count.sh
+++ bin/record_used_ports_count.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-counts=$(netstat -nt | awk "/^tcp/{print \$6}" | sort | uniq -c | sort -r | tr '\n' ' ')
+counts=$(netstat -nt | awk "/^tcp/{print \$6}" | sort | uniq -c | sed -e 's/^ \+//' | sort -rV | tr '\n' ', ')
 echo "$(date +'%Y/%m/%d %H:%M') $counts">>/var/egotter/log/used_port_counts.log
+
+aaa
+
