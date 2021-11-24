42ffa1b5224fdf2a2b6a4e405897dc688cccf2f9
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-25 07:43:26 +0900

    Override a logger of datadog tracer to suppress unnecessary error reporting

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -3,6 +3,9 @@ if Rails.env.production?
   require 'ddtrace'
 
   Datadog.configure do |c|
+    c.logger = ::Logger.new('log/datadog-tracer.log')
+    c.logger.level = ::Logger::INFO
+
     c.use :rails, service_name: 'egotter'
     c.use :sidekiq
     c.analytics_enabled = true
