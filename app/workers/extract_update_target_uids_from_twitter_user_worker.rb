e64d8d906304ee78840ff62036d47bae7f32074e
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-29 00:50:57 +0900

    Implement ExtractUpdateTargetUidsFromTwitterUserWorker

---
+++ app/workers/extract_update_target_uids_from_twitter_user_worker.rb
@@ -0,0 +1,30 @@
+class ExtractUpdateTargetUidsFromTwitterUserWorker
+  include Sidekiq::Worker
+  include WorkerErrorHandler
+  sidekiq_options queue: 'misc', retry: 0, backtrace: false
+
+  # options:
+  def perform(twitter_user_id, options = {})
+    twitter_user = TwitterUser.find(twitter_user_id)
+
+    if (missing_friend_uids = fetch_missing_uids(twitter_user.friend_uids)).any?
+      logger.info "#{self.class}: missing friend uids found twitter_user_id=#{twitter_user_id} size=#{missing_friend_uids.size}"
+    end
+
+    if (missing_follower_uids = fetch_missing_uids(twitter_user.follower_uids)).any?
+      logger.info "#{self.class}: missing follower uids found twitter_user_id=#{twitter_user_id} size=#{missing_follower_uids.size}"
+    end
+  rescue => e
+    handle_worker_error(e, twitter_user_id: twitter_user_id, **options)
+  end
+
+  private
+
+  def fetch_missing_uids(uids)
+    persisted_uids = []
+    uids.each_slice(1000) do |uids_array|
+      persisted_uids << TwitterDB::User.where(uid: uids_array).pluck(:uid)
+    end
+    uids - persisted_uids.flatten
+  end
+end
