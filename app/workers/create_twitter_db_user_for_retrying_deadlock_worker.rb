1edb5d9e1a08db4805b47cd12ea47eefc7066608
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-29 21:08:37 +0900

    Add SkippedCreateTwitterDBUserForRetryingDeadlockWorker

--- app/workers/create_twitter_db_user_for_retrying_deadlock_worker.rb
+++ app/workers/create_twitter_db_user_for_retrying_deadlock_worker.rb
@@ -2,4 +2,8 @@
 class CreateTwitterDBUserForRetryingDeadlockWorker < CreateTwitterDBUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: self, retry: 0, backtrace: false
+
+  def after_skip(uids, options = {})
+    SkippedCreateTwitterDBUserForRetryingDeadlockWorker.perform_async(uids, options)
+  end
 end
