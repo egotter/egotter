eb7ac9758a4297711ca65a8f257c6aac4aa76635
ts-3156 <ts_3156@yahoo.co.jp>
2021-11-30 15:57:04 +0900

    Add #restart_jobs to SkippedCreateTwitterDBUserWorker

--- app/workers/skipped_create_twitter_db_user_worker.rb
+++ app/workers/skipped_create_twitter_db_user_worker.rb
@@ -5,4 +5,34 @@ class SkippedCreateTwitterDBUserWorker
   def perform(*args)
     raise 'Do nothing'
   end
+
+  class << self
+    def restart_jobs(limit: 100, sync: true)
+      processed = 0
+
+      Sidekiq::Queue.new(self).each do |job|
+        if sync
+          CreateTwitterDBUserWorker.new.perform(*job.args)
+        else
+          CreateTwitterDBUserWorker.perform_async(*job.args)
+        end
+
+        job.delete
+        processed += 1
+
+        break if processed >= limit
+      end
+    end
+
+    def print_jobs(limit: 100)
+      processed = 0
+
+      Sidekiq::Queue.new(self).each do |job|
+        puts "#{self}: args=#{job.args}"
+
+        processed += 1
+        break if processed >= limit
+      end
+    end
+  end
 end
