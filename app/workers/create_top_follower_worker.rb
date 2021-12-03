9a2215eb486b81ae699a85d0caeef3ce5ad1cfe0
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-04 00:04:56 +0900

    Don't use grouping function in CreateTopFollowerWorker

--- app/workers/create_top_follower_worker.rb
+++ app/workers/create_top_follower_worker.rb
@@ -17,17 +17,23 @@ class CreateTopFollowerWorker
   # options:
   def perform(twitter_user_id, options = {})
     twitter_user = TwitterUser.find(twitter_user_id)
-    follower_uids = twitter_user.follower_uids
-    candidates = []
-
-    follower_uids.each_slice(1000) do |uids_array|
-      candidates << TwitterDB::User.where(uid: uids_array).group(:uid).select('uid, max(followers_count) followers_count').order(followers_count: :desc).first
+    if (top_follower = find_top_follower(twitter_user.follower_uids))
+      twitter_user.update(top_follower_uid: top_follower.uid)
     end
-
-    top_follower = candidates.compact.max_by { |user| user.followers_count }
-    twitter_user.update(top_follower_uid: top_follower.uid) if top_follower
   rescue => e
     logger.warn "#{e.inspect} twitter_user_id=#{twitter_user_id} options=#{options.inspect}"
     logger.info e.backtrace.join("\n")
+  end
+
+  private
+
+  def find_top_follower(uids)
+    candidates = []
+
+    uids.each_slice(1000) do |uids_array|
+      candidates << TwitterDB::User.where(uid: uids_array).select(:uid, :followers_count).order(followers_count: :desc).first
+    end
+
+    candidates.compact.max_by(&:followers_count)
   end
 end
