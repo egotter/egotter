b31c31833329ce6152b764c19cd778dd7d1ad924
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-03 20:17:30 +0900

    DeleteAhoyRecordsTask receives :loop_count option and :time_range option

--- app/lib/delete_ahoy_records_task.rb
+++ app/lib/delete_ahoy_records_task.rb
@@ -1,9 +1,14 @@
 class DeleteAhoyRecordsTask
+  # options:
+  #   loop_count
+  #   time_range
   def initialize(klass, start_time, end_time, options = {})
     @klass = klass
     @start_time = start_time
     @end_time = end_time
     @column = klass == Ahoy::Event ? :time : :started_at
+    @loop_count = options[:loop_count]
+    @time_range = options[:time_range]
   end
 
   def start
@@ -14,10 +19,21 @@ class DeleteAhoyRecordsTask
     sigint = Sigint.new.trap
     count = 0
     stopped = false
+    if total > 5_000_000
+      loop_count = @loop_count || 30 * 24 * 60
+      time_range = @time_range || 1.minute
+    else
+      loop_count = @loop_count || 30 * 24
+      time_range = @time_range || 1.hour
+    end
 
-    (24 * 30).times do |i|
-      start_time = @start_time + i.hours
-      end_time = @start_time + (i + 1).hours
+    puts "Time range is #{time_range}"
+    puts "Loop count is #{loop_count}"
+    progress = Progress.new(total: total * 2)
+
+    loop_count.times do |i|
+      start_time = @start_time + i * time_range
+      end_time = @start_time + (i + 1) * time_range
       if end_time > @end_time
         end_time = @end_time
         stopped = true
@@ -27,17 +43,36 @@ class DeleteAhoyRecordsTask
 
       query.find_in_batches do |records|
         ids_array << records.map(&:id)
+
+        progress.progress(count += records.size, '(collecting ids)')
+        break if sigint.trapped?
       end
 
       ids_array.each do |ids|
         @klass.where(id: ids).delete_all
-        count += ids.size
-        print "\r#{(100 * count.to_f / total).round(1)}%"
+
+        progress.progress(count += ids.size, '(deleting records)')
+        break if sigint.trapped?
       end
 
-      return if stopped || sigint.trapped?
+      break if stopped || sigint.trapped?
     end
 
     puts ''
+  end
+
+  class Progress
+    def initialize(total:)
+      @total = total
+      @output = $stdout
+    end
+
+    def progress(count, message = nil)
+      @output.print "\r#{(100 * count.to_f / @total).round(1)}% #{message}"
+    end
+
+    def finish
+      @output.puts '100.0%'
+    end
   end
 end
--- lib/tasks/ahoy/visits.rake
+++ lib/tasks/ahoy/visits.rake
@@ -3,8 +3,11 @@ namespace :ahoy do
     task delete: :environment do
       start_time = Time.zone.parse(ENV['SINCE'])
       end_time = Time.zone.parse(ENV['UNTIL'])
+      loop_count = ENV['LOOP']
+      time_range = ENV['RANGE']
+
       begin
-        DeleteAhoyRecordsTask.new(Ahoy::Visit, start_time, end_time).start
+        DeleteAhoyRecordsTask.new(Ahoy::Visit, start_time, end_time, loop_count: loop_count, time_range: time_range).start
       rescue => e
         puts e.inspect
       end
