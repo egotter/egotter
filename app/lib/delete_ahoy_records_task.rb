030768ab5575bcf983183355e5e95ce21f7a2888
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-03 14:15:36 +0900

    Implement DeleteAhoyRecordsTask

---
+++ app/lib/delete_ahoy_records_task.rb
@@ -0,0 +1,42 @@
+class DeleteAhoyRecordsTask
+  def initialize(klass, start_time, end_time, options = {})
+    @klass = klass
+    @start_time = start_time
+    @end_time = end_time
+    @column = klass == Ahoy::Event ? :time : :started_at
+  end
+
+  def start
+    total = @klass.where(@column => @start_time..@end_time).size
+    puts "Delete #{total} records"
+
+    sigint = Sigint.new.trap
+    count = 0
+    stopped = false
+
+    (24 * 30).times do |i|
+      start_time = @start_time + i.hours
+      end_time = @start_time + (i + 1).hours
+      if end_time > @end_time
+        end_time = @end_time
+        stopped = true
+      end
+      query = @klass.where(@column => start_time..end_time).select(:id)
+      ids_array = []
+
+      query.find_in_batches do |records|
+        ids_array << records.map(&:id)
+      end
+
+      ids_array.each do |ids|
+        @klass.where(id: ids).delete_all
+        count += ids.size
+        print "\r#{(100 * count.to_f / total).round(1)}%"
+      end
+
+      return if stopped || sigint.trapped?
+    end
+
+    puts ''
+  end
+end
--- lib/tasks/ahoy/visits.rake
+++ lib/tasks/ahoy/visits.rake
@@ -3,25 +3,11 @@ namespace :ahoy do
     task delete: :environment do
       start_time = Time.zone.parse(ENV['SINCE'])
       end_time = Time.zone.parse(ENV['UNTIL'])
-
-      query = Ahoy::Visit.where(started_at: start_time..end_time).select(:id)
-      total = query.size
-      puts "Delete #{total} records"
-
-      sigint = Sigint.new.trap
-      count = 0
-
-      query.find_in_batches do |records|
-        Ahoy::Visit.where(id: records.map(&:id)).delete_all
-        count += records.size
-        print "\r#{(100 * count.to_f / total).round(1)}%"
-
-        return if sigint.trapped?
+      begin
+        DeleteAhoyRecordsTask.new(Ahoy::Visit, start_time, end_time).start
+      rescue => e
+        puts e.inspect
       end
-
-      puts ''
-    rescue => e
-      puts e.inspect
     end
   end
 end
