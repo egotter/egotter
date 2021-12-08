72de33642d9eaa6b76dbc0f5d18272c4111e1fbd
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-09 04:07:58 +0900

    Fix "undefined local variable or method"

--- app/workers/create_search_request_worker.rb
+++ app/workers/create_search_request_worker.rb
@@ -4,10 +4,10 @@ class CreateSearchRequestWorker
   sidekiq_options queue: 'creating_high', retry: 0, backtrace: false
 
   # options:
-  def perform(request, options = {})
-    request = SearchRequest.find(request)
+  def perform(request_id, options = {})
+    request = SearchRequest.find(request_id)
     request.perform
   rescue => e
-    handle_worker_error(e, user_id: user_id, **options)
+    handle_worker_error(e, request_id: request_id, **options)
   end
 end
