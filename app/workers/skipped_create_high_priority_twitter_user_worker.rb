732b94ba5fd80f0417d52e9a71e6146990156755
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-10 04:44:55 +0900

    Remove CreateHighPriorityTwitterUserWorker

--- app/workers/skipped_create_high_priority_twitter_user_worker.rb
+++
@@ -1,8 +0,0 @@
-class SkippedCreateHighPriorityTwitterUserWorker
-  include Sidekiq::Worker
-  sidekiq_options queue: self, retry: 0, backtrace: false
-
-  def perform(*args)
-    raise 'Do nothing'
-  end
-end
