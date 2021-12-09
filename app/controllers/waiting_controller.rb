2dff29b25b557c6fdacdfd6afd44813c7ef7cc4c
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-09 23:02:28 +0900

    WaitingController can receive :screen_name too

--- app/controllers/waiting_controller.rb
+++ app/controllers/waiting_controller.rb
@@ -3,12 +3,50 @@ class WaitingController < ApplicationController
 
   before_action :reject_crawler
   before_action :reject_spam_access!
-  before_action { valid_uid?(params[:uid]) }
-  before_action { @twitter_user = build_twitter_user_by_uid(params[:uid]) }
+  before_action do
+    # TODO This is a temporary workaround. Only :screen_name is accepted in the future.
+    if params[:uid] && Validations::UidValidator::REGEXP.match?(params[:uid])
+      uid = params[:uid].to_i
+      request = SearchRequest.request_for(current_user&.id, uid: uid)
+      screen_name = request&.screen_name
+    elsif params[:screen_name] && Validations::ScreenNameValidator::REGEXP.match?(params[:screen_name])
+      screen_name = params[:screen_name]
+      request = SearchRequest.request_for(current_user&.id, screen_name: screen_name)
+      uid = request&.uid
+    else
+      uid = screen_name = nil
+    end
+
+    if uid && screen_name
+      @uid = uid
+      @screen_name = screen_name
+    else
+      redirect_to root_path(via: 'invalid_uid_or_screen_name')
+    end
+  end
+  before_action do
+    set_user(@uid)
+    set_redirect_path(@screen_name)
+  end
 
   def new
-    @redirect_path = sanitized_redirect_path(params[:redirect_path].presence || timeline_path(@twitter_user, via: current_via('waiting_redirect')))
-    @redirect_path.sub!(':screen_name', @twitter_user.screen_name) if @redirect_path.include?(':screen_name')
-    @jid = params[:jid]
+  end
+
+  private
+
+  def set_user(uid)
+    user = TwitterDB::User.find_by(uid: uid)
+    user = TwitterUser.latest_by(uid: uid) unless user
+    @user = user
+  end
+
+  def set_redirect_path(screen_name)
+    if params[:redirect_path]
+      path = sanitized_redirect_path(params[:redirect_path])
+      path.sub!(':screen_name', screen_name) if path.include?(':screen_name')
+    else
+      path = timeline_path(screen_name: screen_name, via: current_via('waiting_redirect'))
+    end
+    @redirect_path = path
   end
 end
