53c7d49e5aca6b4ec17da9d23bd73a25db2d262e
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-11 04:25:25 +0900

    Don't track page order activity on pricing page

--- app/controllers/pricing_controller.rb
+++ app/controllers/pricing_controller.rb
@@ -2,8 +2,6 @@ class PricingController < ApplicationController
 
   skip_before_action :current_user_not_blocker?
 
-  after_action :track_page_order_activity, if: -> { user_signed_in? }
-
   def index
   end
 end
