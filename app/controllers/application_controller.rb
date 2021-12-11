085caa83ae5ecb6964c3c6bdcaa58a0bc795c5d1
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-11 19:27:54 +0900

    Refactor #set_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,12 +25,15 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  # I18n.available_locales.map(&:to_s)
+  AVAILABLE_LOCALES = [:ja, :en]
+
   def set_locale
-    I18n.locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
+    I18n.locale = AVAILABLE_LOCALES.include?(params[:locale]) ? params[:locale] : I18n.default_locale
   end
 
   def default_url_options(options = {})
-    {locale: I18n.locale}.merge options
+    {locale: I18n.locale}.merge(options)
   end
 
   # Prevent CSRF attacks by raising an exception.
