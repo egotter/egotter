20c7f4f8fc296845ff956866df10b866b33864b0
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-12 06:36:46 +0900

    Save via param instead of button_id

--- app/controllers/api/v1/checkout_sessions_controller.rb
+++ app/controllers/api/v1/checkout_sessions_controller.rb
@@ -8,7 +8,7 @@ module Api
       before_action :require_login!
       before_action :doesnt_have_valid_subscription!
 
-      after_action { track_page_order_activity(stripe_session_id: @stripe_session&.id, button_id: params[:button_id]) }
+      after_action { track_page_order_activity(stripe_session_id: @stripe_session&.id) }
       after_action { send_message(@stripe_session&.id) }
 
       def create
@@ -21,7 +21,7 @@ module Api
       def create_session(user)
         attrs = CheckoutSessionBuilder.build(user)
         stripe_session = Stripe::Checkout::Session.create(attrs)
-        CheckoutSession.create!(user_id: user.id, stripe_checkout_session_id: stripe_session.id, properties: {button_id: params[:button_id]})
+        CheckoutSession.create!(user_id: user.id, stripe_checkout_session_id: stripe_session.id, properties: {via: params[:via]})
         stripe_session
       end
 
@@ -34,7 +34,6 @@ module Api
       def tracking_params
         {
             user_id: current_user.id,
-            button_id: params[:button_id],
             via: params[:via],
             referer: request.referer.to_s.truncate(200),
         }
