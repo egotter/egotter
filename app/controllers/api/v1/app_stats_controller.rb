6fdc2ccb2fec5d1531f3ece2324d6e3b753f45ea
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-16 17:55:24 +0900

    Add xxx_previous_period on app_stats page

--- app/controllers/api/v1/app_stats_controller.rb
+++ app/controllers/api/v1/app_stats_controller.rb
@@ -48,12 +48,20 @@ module Api
           ApiClientCacheStore.instance.redis.info['total_system_memory_human']
         when 'periodic_reports'
           PeriodicReport.where('created_at > ?', 1.day.ago).size
+        when 'periodic_reports_previous_period'
+          PeriodicReport.where(created_at: 2.days.ago..1.day.ago).size
         when 'block_reports'
           BlockReport.where('created_at > ?', 1.day.ago).size
+        when 'block_reports_previous_period'
+          BlockReport.where(created_at: 2.days.ago..1.day.ago).size
         when 'mute_reports'
           MuteReport.where('created_at > ?', 1.day.ago).size
+        when 'mute_reports_previous_period'
+          MuteReport.where(created_at: 2.days.ago..1.day.ago).size
         when 'search_reports'
           SearchReport.where('created_at > ?', 1.day.ago).size
+        when 'search_reports_previous_period'
+          SearchReport.where(created_at: 2.days.ago..1.day.ago).size
         when 'user_timeline'
           TwitterApiLog.where('created_at > ?', 1.day.ago).where(name: 'user_timeline').size
         when 'follow'
