b178bb038a8eef388431ecfd4b43211763677978
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-18 08:04:49 +0900

    Install rails-erb-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
+const erb = require('./loaders/erb')
+environment.loaders.prepend('erb', erb)
+
 const eslint = require('./loaders/eslint')
 environment.loaders.append('eslint', eslint)
 
