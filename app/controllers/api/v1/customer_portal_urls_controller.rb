474ec6280bde4294d8aa0184a2c3c62d4cdc822b
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-19 23:48:00 +0900

    Remove #current_user_has_order! from ValidationConcern

--- app/controllers/api/v1/customer_portal_urls_controller.rb
+++ app/controllers/api/v1/customer_portal_urls_controller.rb
@@ -4,12 +4,20 @@ module Api
 
       before_action :reject_crawler
       before_action :require_login!
-      before_action :current_user_has_order!
+      before_action :has_order!
 
       def create
         order = current_user.orders.find_by(id: params[:order_id])
         session = Stripe::BillingPortal::Session.create(customer: order.customer_id, return_url: settings_url(via: 'customer_portal'))
         render json: {url: session.url}
+      end
+
+      private
+
+      def has_order!
+        if current_user.orders.empty?
+          head :bad_request
+        end
       end
     end
   end
