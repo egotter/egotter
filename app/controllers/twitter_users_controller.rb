22963be8804986716d900674b5a5d8ebca668aaf
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-22 02:09:38 +0900

    Refactor TwitterUsersController

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -15,49 +15,48 @@ class TwitterUsersController < ApplicationController
   end
 
   def changes
-    render json: {text: ChangesTextBuilder.new(params[:uid]).build}
+    twitter_user = TwitterUser.with_delay.latest_by(uid: params[:uid])
+    render json: {text: changes_text(twitter_user)}
   end
 
   private
 
-  class ChangesTextBuilder
-    attr_reader :current_twitter_user, :previous_twitter_user, :screen_name
-
-    def initialize(uid)
-      @current_twitter_user = TwitterUser.latest_by(uid: uid)
-      @previous_twitter_user = TwitterUser.where.not(id: @current_twitter_user.id).latest_by(uid: uid)
-      @screen_name = current_twitter_user.screen_name
+  def changes_text(twitter_user)
+    unless twitter_user
+      return t('.changes.new_record_created', user: twitter_user.screen_name)
     end
 
-    def build
-      unless previous_twitter_user
-        return I18n.t('twitter_users.show.new_record_created', user: screen_name)
-      end
+    previous_version = twitter_user.previous_version
 
-      # To save time, only the latest differences are calculated.
-      new_unfollower_uids = UnfriendsBuilder::Util.unfollowers(previous_twitter_user, current_twitter_user)
-
-      # TODO At this point, the import batch may not be finished yet.
-
-      if new_unfollower_uids.any?
-        I18n.t('twitter_users.show.new_unfollowers_are_coming', user: screen_name, count: new_unfollower_uids.size)
-      else
-        if current_twitter_user.followers_count > previous_twitter_user.followers_count
-          I18n.t('twitter_users.show.followers_count_increased', user: screen_name, before: previous_twitter_user.followers_count, after: current_twitter_user.followers_count)
-        elsif current_twitter_user.followers_count < previous_twitter_user.followers_count
-          I18n.t('twitter_users.show.followers_count_decreased', user: screen_name, before: previous_twitter_user.followers_count, after: current_twitter_user.followers_count)
-        elsif current_twitter_user.friends_count > previous_twitter_user.friends_count
-          I18n.t('twitter_users.show.friends_count_increased', user: screen_name, before: previous_twitter_user.friends_count, after: current_twitter_user.friends_count)
-        elsif current_twitter_user.friends_count < previous_twitter_user.friends_count
-          I18n.t('twitter_users.show.friends_count_decreased', user: screen_name, before: previous_twitter_user.friends_count, after: current_twitter_user.friends_count)
-        else
-          I18n.t('twitter_users.show.update_is_coming', user: screen_name)
-        end
-      end
-    rescue => e
-      Airbag.warn "#{self.class}##{__method__} #{e.inspect} #{previous_twitter_user&.id} #{current_twitter_user.id}"
-      Airbag.info e.backtrace.join("\n")
-      I18n.t('twitter_users.show.update_is_coming_with_error', user: screen_name)
+    unless previous_version
+      return t('.changes.new_record_created', user: twitter_user.screen_name)
     end
+
+    if (uids = twitter_user.calc_new_unfollower_uids).any?
+      return t('.changes.unfollowers_changed', user: twitter_user.screen_name, count: uids.size)
+    end
+
+    options = {
+        user: twitter_user.screen_name,
+        previous_friends: previous_version.friends_count,
+        current_friends: twitter_user.friends_count,
+        previous_followers: previous_version.followers_count,
+        current_followers: twitter_user.followers_count,
+    }
+
+    if twitter_user.followers_count > previous_version.followers_count
+      t('.changes.followers_increased', options)
+    elsif twitter_user.followers_count < previous_version.followers_count
+      t('.changes.followers_decreased', options)
+    elsif twitter_user.friends_count > previous_version.friends_count
+      t('.changes.friends_increased', options)
+    elsif twitter_user.friends_count < previous_version.friends_count
+      t('.changes.friends_decreased', options)
+    else
+      t('.changes.something_changed', options)
+    end
+  rescue => e
+    Airbag.warn "#{self.class}##{__method__} #{e.inspect} twitter_user_id=#{twitter_user.id}"
+    t('.changes.error', user: twitter_user.screen_name)
   end
 end
--- spec/controllers/twitter_users_controller_spec.rb
+++ spec/controllers/twitter_users_controller_spec.rb
@@ -1,4 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe TwitterUsersController, type: :controller do
+  describe '#changes_text' do
+    let(:previous_friends) { 1 }
+    let(:current_friends) { 1 }
+    let(:previous_followers) { 1 }
+    let(:current_followers) { 1 }
+    let(:previous_version) { double('twitter_user', id: 1, uid: 3, screen_name: 'name', friends_count: previous_friends, followers_count: previous_followers) }
+    let(:twitter_user) { double('twitter_user', id: 2, uid: 3, screen_name: 'name', friends_count: current_friends, followers_count: current_followers) }
+    let(:uids) { [] }
+    subject { controller.send(:changes_text, twitter_user) }
+
+    before do
+      allow(twitter_user).to receive(:previous_version).and_return(previous_version)
+      allow(twitter_user).to receive(:calc_new_unfollower_uids).and_return(uids)
+    end
+
+    context 'previous_version is nil' do
+      let(:previous_version) { nil }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'uids is not empty' do
+      let(:uids) { [1] }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'followers increased' do
+      let(:previous_followers) { 1 }
+      let(:current_followers) { 2 }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'followers decreased' do
+      let(:previous_followers) { 2 }
+      let(:current_followers) { 1 }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'friends increased' do
+      let(:previous_friends) { 1 }
+      let(:current_friends) { 2 }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'friends decreased' do
+      let(:previous_friends) { 2 }
+      let(:current_friends) { 1 }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'both friends and followers not changed' do
+      let(:previous_friends) { 1 }
+      let(:current_friends) { 1 }
+      let(:previous_followers) { 1 }
+      let(:current_followers) { 1 }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'error is raised' do
+      before { allow(twitter_user).to receive(:previous_version).and_raise('error') }
+      it { is_expected.to be_truthy }
+    end
+  end
 end
