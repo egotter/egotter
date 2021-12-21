3633fa6cf6086e417ee5b36ecb0349390c53eb68
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-21 13:08:19 +0900

    Implement NewUnfriendsCountPoint

---
+++ db/migrate/20211221040142_create_new_unfriends_count_points.rb
@@ -0,0 +1,13 @@
+class CreateNewUnfriendsCountPoints < ActiveRecord::Migration[6.1]
+  def change
+    create_table :new_unfriends_count_points do |t|
+      t.bigint :uid, null: false
+      t.integer :value, null: false
+      t.timestamp :created_at, null: false
+
+      t.index :uid
+      t.index :created_at
+      t.index %i(uid created_at)
+    end
+  end
+end
---
+++ spec/factories/new_unfriends_count_points.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :new_unfriends_count_point do
+    
+  end
+end
