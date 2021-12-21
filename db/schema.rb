b7009f140bd0dc242fa5c86b3c15319fc6bceaff
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-21 13:12:14 +0900

    Implement NewUnfollowersCountPoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_040142) do
+ActiveRecord::Schema.define(version: 2021_12_21_040841) do
 
   create_table "access_days", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -969,6 +969,15 @@ ActiveRecord::Schema.define(version: 2021_12_21_040142) do
     t.index ["created_at"], name: "index_new_friends_count_points_on_created_at"
     t.index ["uid", "created_at"], name: "index_new_friends_count_points_on_uid_and_created_at"
     t.index ["uid"], name: "index_new_friends_count_points_on_uid"
+  end
+
+  create_table "new_unfollowers_count_points", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "uid", null: false
+    t.integer "value", null: false
+    t.timestamp "created_at", null: false
+    t.index ["created_at"], name: "index_new_unfollowers_count_points_on_created_at"
+    t.index ["uid", "created_at"], name: "index_new_unfollowers_count_points_on_uid_and_created_at"
+    t.index ["uid"], name: "index_new_unfollowers_count_points_on_uid"
   end
 
   create_table "new_unfriends_count_points", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
