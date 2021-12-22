c4fa59d660c8e5ec00893ef2ac342aa75368c1b1
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-23 08:30:12 +0900

    Add sign-out link to blocker_detected page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
   include SearchHistoriesConcern
 
+  skip_before_action :current_user_not_blocker?
+
   def destroy
     update_search_histories_when_signing_out { super }
   end
