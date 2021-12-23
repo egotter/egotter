025f5c220e175caac595583824192a82a573ff8f
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-24 06:46:43 +0900

    Set an interval for InvalidateExpiredCredentialWorker

--- app/controllers/api/v1/bots_controller.rb
+++ app/controllers/api/v1/bots_controller.rb
@@ -5,8 +5,8 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def invalidate_expired_credentials
-        Bot.all.each do |bot|
-          InvalidateExpiredCredentialWorker.perform_async(bot.id)
+        Bot.all.each.with_index do |bot, i|
+          InvalidateExpiredCredentialWorker.perform_in((0.5 * i).floor, bot.id)
         end
         render json: {status: 'ok'}
       end
