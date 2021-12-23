846c9024a9d92c87ff5eca4473cea854e358228b
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-24 08:02:43 +0900

    Add a note with instructions on how to stop all reports

--- app/workers/create_all_reports_stopped_message_worker.rb
+++ app/workers/create_all_reports_stopped_message_worker.rb
@@ -3,7 +3,7 @@ class CreateAllReportsStoppedMessageWorker
   include ReportErrorHandler
   sidekiq_options queue: 'messaging', retry: 0, backtrace: false
 
-  MESSAGE = <<~TEXT
+  MESSAGE = <<~ERB
     全ての通知（リムられ通知、検索通知、ブロック通知、ミュート通知）を停止しました。自分で再開しない限りこれらの通知が届くことはありません。
 
     ※ 全ての通知が止まっている間はデータの更新も止まります。
@@ -24,8 +24,12 @@ class CreateAllReportsStoppedMessageWorker
     【ミュート通知 再開】をDMで送信
     ↑ 定期的なミュート通知を再開します。
 
+    よくある質問 <%= support_url %>
+    設定ページ  <%= settings_url %>
+    問い合わせアカウント @egotter_cs
+
     #egotter
-  TEXT
+  ERB
 
   def unique_key(user_id, options = {})
     user_id
@@ -39,11 +43,30 @@ class CreateAllReportsStoppedMessageWorker
   def perform(user_id, options = {})
     user = User.find(user_id)
     replies = [PeriodicReport::QUICK_REPLY_RESTART, BlockReport::QUICK_REPLY_RESTART, MuteReport::QUICK_REPLY_RESTART]
-    event = DirectMessageEvent.build_with_replies(user.uid, MESSAGE, replies)
+    event = DirectMessageEvent.build_with_replies(user.uid, build_message, replies)
     User.egotter.api_client.create_direct_message_event(event: event)
   rescue => e
     unless ignorable_report_error?(e)
       Airbag.warn "#{e.inspect} user_id=#{user_id} options=#{options.inspect}"
+    end
+  end
+
+  private
+
+  def build_message
+    helper = UrlHelpers.new
+
+    ERB.new(MESSAGE).result_with_hash(
+        support_url: helper.support_url,
+        settings_url: helper.settings_url,
+    )
+  end
+
+  class UrlHelpers
+    include Rails.application.routes.url_helpers
+
+    def default_url_options
+      {og_tag: false}
     end
   end
 end
