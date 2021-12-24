b28c7cc5d717e38bd3d84aa245a01c6245d657bd
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-24 15:03:57 +0900

    Refactor DeleteForbiddenUsersWorker

--- app/workers/delete_forbidden_users_worker.rb
+++ app/workers/delete_forbidden_users_worker.rb
@@ -11,8 +11,14 @@ class DeleteForbiddenUsersWorker
   end
 
   def perform(options = {})
+    ids_array = []
+
     ForbiddenUser.where('created_at < ?', 15.minutes.ago).find_in_batches do |users|
-      ForbiddenUser.where(id: users.map(&:id)).delete_all
+      ids_array << users.map(&:id)
+    end
+
+    ids_array.each do |ids|
+      ForbiddenUser.where(id: ids).delete_all
     end
   rescue => e
     Airbag.warn "#{e.inspect} options=#{options.inspect}"
