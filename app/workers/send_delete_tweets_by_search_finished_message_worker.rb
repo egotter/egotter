4212a13123e99926a5512e7da76cd36094bec78c
ts-3156 <ts_3156@yahoo.co.jp>
2021-12-25 09:09:13 +0900

    Implement SendDeleteTweetsBySearchFinishedMessageWorker

---
+++ app/workers/send_delete_tweets_by_search_finished_message_worker.rb
@@ -0,0 +1,13 @@
+class SendDeleteTweetsBySearchFinishedMessageWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'misc', retry: 0, backtrace: false
+
+  # options:
+  def perform(request_id, options = {})
+    request = DeleteTweetsBySearchRequest.find(request_id)
+    request.finished!
+    SlackBotClient.channel('monit_delete_tweets').post_message("`Finished` #{request.to_message}")
+  rescue => e
+    Airbag.warn "#{e.inspect} request_id=#{request_id} options=#{options.inspect}"
+  end
+end
