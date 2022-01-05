239d54b16c9906cef2d80759462c8125a32cf1f1
ts-3156 <ts_3156@yahoo.co.jp>
2022-01-06 01:31:08 +0900

    Add shared/_confirmation_section.html.erb

--- app/helpers/confirmations_helper.rb
+++ app/helpers/confirmations_helper.rb
@@ -1,4 +1,9 @@
 module ConfirmationsHelper
+  def confirmation_path
+    redirect_path = send("#{controller_name}_path", share_dialog: 1, follow_dialog: 1, sign_in_dialog: 1, purchase_dialog: 1, user_token: params[:user_token], via: current_via)
+    sign_in_path(force_login: !user_signed_in?, via: current_via, redirect_path: redirect_path)
+  end
+
   def sign_in_as_confirmation_user?
     user_signed_in? && params[:user_token] && current_user.valid_user_token?(params[:user_token])
   end
