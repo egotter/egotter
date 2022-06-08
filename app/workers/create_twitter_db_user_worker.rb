48b0e9f657b40e8a8ba5c194d50058cddad34c06
ts-3156 <ts_3156@yahoo.co.jp>
2022-06-09 04:53:55 +0900

    Add debug options to SkippedCreateTwitterDBUserWorker

--- app/workers/create_twitter_db_user_worker.rb
+++ app/workers/create_twitter_db_user_worker.rb
@@ -5,16 +5,16 @@ class CreateTwitterDBUserWorker
   include WorkerErrorHandler
   sidekiq_options queue: self, retry: 0, backtrace: false
 
-  def unique_key(uids, options = {})
-    Digest::MD5.hexdigest(uids.to_s)
+  def unique_key(data, options = {})
+    Digest::MD5.hexdigest(data.to_s)
   end
 
   def unique_in
     10.seconds
   end
 
-  def after_skip(uids, options = {})
-    SkippedCreateTwitterDBUserWorker.perform_async(uids, options)
+  def after_skip(data, options = {})
+    SkippedCreateTwitterDBUserWorker.perform_async(data, options.merge(_size: (decompress(data).size rescue -1), _time: Time.zone.now))
   end
 
   def _timeout_in
@@ -25,40 +25,40 @@ class CreateTwitterDBUserWorker
   #   force_update
   #   user_id
   #   enqueued_by
-  def perform(uids, options = {})
-    target_uids = uids.is_a?(String) ? decompress(uids) : uids
+  def perform(data, options = {})
+    uids = decompress(data)
 
-    if target_uids.empty?
+    if uids.empty?
       Airbag.warn "the size of uids is 0 options=#{options.inspect}"
       return
     end
 
-    if target_uids.size > 100
+    if uids.size > 100
       Airbag.warn "the size of uids is greater than 100 options=#{options.inspect}"
     end
 
     user_id = (options['user_id'] && options['user_id'].to_i != -1) ? options['user_id'] : nil
 
-    task = CreateTwitterDBUsersTask.new(target_uids, user_id: user_id, force: options['force_update'])
+    task = CreateTwitterDBUsersTask.new(uids, user_id: user_id, force: options['force_update'])
     task.start
     Airbag.info "CreateTwitterDBUsersTask: DEBUG enqueued_by=#{options['enqueued_by']} user_id=#{user_id} #{task.debug_message}"
   rescue CreateTwitterDBUsersTask::RetryDeadlockExhausted => e
     Airbag.info "Retry deadlock error: #{e.inspect.truncate(200)}"
     delay = rand(20) + 15
-    CreateTwitterDBUserForRetryingDeadlockWorker.perform_in(delay, uids, options.merge(klass: self.class, error_class: e.class))
+    CreateTwitterDBUserForRetryingDeadlockWorker.perform_in(delay, data, options.merge(klass: self.class, error_class: e.class))
   rescue ApiClient::RetryExhausted => e
     Airbag.info "Retry retryable error: #{e.inspect.truncate(200)}"
     delay = rand(20) + 15
-    CreateTwitterDBUserForRetryableErrorWorker.perform_in(delay, uids, options.merge(klass: self.class, error_class: e.class))
+    CreateTwitterDBUserForRetryableErrorWorker.perform_in(delay, data, options.merge(klass: self.class, error_class: e.class))
   rescue ApiClient::ContainStrangeUid => e
-    if target_uids && target_uids.size > 1
-      slice_and_retry(target_uids, options)
+    if uids && uids.size > 1
+      slice_and_retry(uids, options)
     else
-      Airbag.info "#{e.message} uids=#{target_uids.inspect} options=#{options.inspect}"
+      Airbag.info "#{e.message} uids=#{uids.inspect} options=#{options.inspect}"
     end
   rescue => e
-    handle_worker_error(e, uids_size: target_uids.size, options: options)
-    FailedCreateTwitterDBUserWorker.perform_async(target_uids, options.merge(klass: self.class, error_class: e.class))
+    handle_worker_error(e, uids_size: uids.size, options: options)
+    FailedCreateTwitterDBUserWorker.perform_async(uids, options.merge(klass: self.class, error_class: e.class))
   end
 
   private
@@ -92,6 +92,6 @@ class CreateTwitterDBUserWorker
   end
 
   def decompress(data)
-    Zlib::Inflate.inflate(Base64.decode64(data)).split(',').map(&:to_i)
+    data.is_a?(String) ? Zlib::Inflate.inflate(Base64.decode64(data)).split(',').map(&:to_i) : data
   end
 end
