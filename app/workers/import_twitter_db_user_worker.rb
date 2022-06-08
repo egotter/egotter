11c0fe50b2ebadaa0012b7bb9f61170220766a99
ts-3156 <ts_3156@yahoo.co.jp>
2022-06-09 04:36:43 +0900

    Add debug options to SkippedImportTwitterDBUserWorker

--- app/workers/import_twitter_db_user_worker.rb
+++ app/workers/import_twitter_db_user_worker.rb
@@ -5,16 +5,16 @@ class ImportTwitterDBUserWorker
   include WorkerErrorHandler
   sidekiq_options queue: self, retry: 0, backtrace: false
 
-  def unique_key(users, options = {})
-    Digest::MD5.hexdigest(users.to_json)
+  def unique_key(data, options = {})
+    Digest::MD5.hexdigest(data.to_json)
   end
 
   def unique_in
     10.seconds
   end
 
-  def after_skip(users, options = {})
-    SkippedImportTwitterDBUserWorker.perform_async(users, options)
+  def after_skip(data, options = {})
+    SkippedImportTwitterDBUserWorker.perform_async(data, options.merge(_size: (decompress(data).size rescue -1), _time: Time.zone.now))
   end
 
   def _timeout_in
@@ -23,7 +23,7 @@ class ImportTwitterDBUserWorker
 
   # options:
   def perform(data, options = {})
-    users = data.is_a?(String) ? JSON.parse(Zlib::Inflate.inflate(Base64.decode64(data))) : data
+    users = decompress(data)
     import_users(users)
   rescue Deadlocked => e
     delay = rand(20) + 15
@@ -52,6 +52,10 @@ class ImportTwitterDBUserWorker
   # ActiveRecord::Deadlocked
   def deadlock_error?(e)
     e.message.include?('try restarting transaction')
+  end
+
+  def decompress(data)
+    data.is_a?(String) ? JSON.parse(Zlib::Inflate.inflate(Base64.decode64(data))) : data
   end
 
   class << self
