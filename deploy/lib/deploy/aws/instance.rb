e8818f3f912647392fc1b9fd4219c5c116daa0c0
ts-3156 <ts_3156@yahoo.co.jp>
2022-06-28 18:56:02 +0900

    Implement #instance_lifecycle

--- deploy/lib/deploy/aws/instance.rb
+++ deploy/lib/deploy/aws/instance.rb
@@ -3,7 +3,7 @@ require_relative './ec2'
 module Deploy
   module Aws
     class Instance
-      attr_reader :id, :name, :public_ip, :availability_zone, :launched_at, :instance_type
+      attr_reader :id, :name, :public_ip, :availability_zone, :launched_at
 
       def initialize(instance)
         @instance = instance
@@ -12,11 +12,18 @@ module Deploy
         @public_ip = instance.public_ip_address
         @availability_zone = instance.placement.availability_zone
         @launched_at = instance.launch_time
-        @instance_type = instance.instance_type
       end
 
       def host
         @name
+      end
+
+      def instance_type
+        @instance.instance_type
+      end
+
+      def instance_lifecycle
+        @instance.instance_lifecycle
       end
 
       def terminate
