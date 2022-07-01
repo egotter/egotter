7917a4a30ebcf2ccd6fb69c37acf166e6c602850
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-01 19:38:04 +0900

    Remove SlackClient and related keys

--- spec/lib/slack_client_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SlackClient, type: :model do
-  describe '.monitoring' do
-    subject { described_class.monitoring }
-    it { is_expected.to be_truthy }
-  end
-end
--- spec/support/slack_client.rb
+++
@@ -1,7 +0,0 @@
-module SendNothing
-  def send_message(*args)
-    'ok (send nothing in test environment)'
-  end
-end
-
-SlackClient.prepend SendNothing
