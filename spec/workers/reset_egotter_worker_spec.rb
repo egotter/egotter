017039bcf4dd3ac136d44fc86b1630ea7d3a9353
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-03 13:58:36 +0900

    Fix test cases

--- spec/workers/reset_egotter_worker_spec.rb
+++ spec/workers/reset_egotter_worker_spec.rb
@@ -4,16 +4,6 @@ RSpec.describe ResetEgotterWorker do
   let(:user) { create(:user) }
   let(:worker) { described_class.new }
 
-  describe '#after_timeout' do
-    let(:args) { [1, {}] }
-    let(:retry_in) { 1.second }
-    before { allow(worker).to receive(:retry_in).and_return(retry_in) }
-    it do
-      expect(described_class).to receive(:perform_in).with(retry_in, *args)
-      worker.after_timeout(*args)
-    end
-  end
-
   describe '#retry_in' do
     it { expect(worker.retry_in).to be >= worker.unique_in }
   end
