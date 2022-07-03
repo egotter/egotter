3ee47ac5d2e0ca2b00cac3057c244ff8207632c8
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-03 13:58:21 +0900

    Handle Slack::Web::Api::Errors::TooManyRequestsError

--- app/workers/send_message_to_slack_worker.rb
+++ app/workers/send_message_to_slack_worker.rb
@@ -5,6 +5,9 @@ class SendMessageToSlackWorker
   # options:
   def perform(channel, text, title = nil, options = {})
     SlackBotClient.channel(channel).post_message(text)
+  rescue Slack::Web::Api::Errors::TooManyRequestsError => e
+    # Don't use Airbag.warn to avoid infinite loop
+    logger.warn "#{e.class} channel=#{channel} text=#{text} options=#{options.inspect}"
   rescue => e
     # Don't use Airbag.warn to avoid infinite loop
     logger.warn "#{e.inspect} channel=#{channel} text=#{text} options=#{options.inspect}"
