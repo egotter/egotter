833eaa61fc332f99873c32bef6d79eb20d062c5f
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-08 15:06:32 +0900

    Update the schema information of sidekiq_logs table

--- app/models/sidekiq_log.rb
+++ app/models/sidekiq_log.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: sidekiq_logs
+#
+#  id         :bigint(8)        not null, primary key
+#  message    :text(65535)
+#  properties :json
+#  time       :datetime         not null
+#
+# Indexes
+#
+#  index_sidekiq_logs_on_time  (time)
+#
 class SidekiqLog < ApplicationRecord
 end
