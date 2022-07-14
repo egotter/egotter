bab3ea3bfe26cdf038f07b8b9b7822c4c42860d5
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-14 09:31:03 +0900

    Implement ImportTwitterDBSuspendedUserWorker

---
+++ app/workers/import_twitter_db_suspended_user_worker.rb
@@ -0,0 +1,28 @@
+require 'digest/md5'
+
+class ImportTwitterDBSuspendedUserWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'misc', retry: 0, backtrace: false
+
+  def unique_key(uids, options = {})
+    Digest::MD5.hexdigest(uids.to_json)
+  end
+
+  def unique_in
+    10.seconds
+  end
+
+  def _timeout_in
+    10.seconds
+  end
+
+  # options:
+  def perform(uids, options = {})
+    if (uids -= TwitterDB::User.where(uid: uids).pluck(:uid)).any?
+      users = uids.map { |uid| {id: uid, screen_name: 'suspended', description: ''} }
+      ImportTwitterDBUserWorker.perform_async(users)
+    end
+  rescue => e
+    Airbag.warn "#{e.inspect.truncate(200)} uids=#{uids}"
+  end
+end
