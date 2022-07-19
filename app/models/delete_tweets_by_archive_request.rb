7c8cdb86e1a63efa1f091d204f06330a3eadfb88
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-20 07:06:21 +0900

    Use Mutex in DeleteTweetsByArchiveRequest#delete_tweets

--- app/models/delete_tweets_by_archive_request.rb
+++ app/models/delete_tweets_by_archive_request.rb
@@ -41,37 +41,41 @@ class DeleteTweetsByArchiveRequest < ApplicationRecord
   private
 
   def delete_tweets(client, tweets, threads_count)
-    total_errors = Queue.new
-    consecutive_errors = Queue.new
+    errors = {total: [], consecutive: [], current: []}
+    lock = Mutex.new
 
     tweets.each_slice(threads_count) do |partial_tweets|
-      current_errors = Queue.new
+      errors[:current] = []
 
       partial_tweets.map do |tweet|
         Thread.new(client, tweet) do |clt, twt|
           clt.destroy_status(twt.id)
-          consecutive_errors = Queue.new if consecutive_errors.size > 0
         rescue => e
-          current_errors.push(e)
-          consecutive_errors.push(e)
-          total_errors.push(e)
-          puts "#{e.inspect} tweet_id=#{twt.id} consecutive=#{consecutive_errors.size} total=#{total_errors.size}"
+          lock.synchronize do
+            errors[:current] << e
+            errors[:consecutive] << e
+            errors[:total] << e
+            puts "#{e.inspect} tweet_id=#{twt.id} consecutive=#{errors[:consecutive].size} total=#{errors[:total].size}"
+          end
         end
       end.each(&:join)
 
-      increment!(:deletions_count, partial_tweets.size - current_errors.size)
-      increment!(:errors_count, current_errors.size) unless current_errors.empty?
+      if errors[:current].empty?
+        errors[:consecutive].clear
+      end
+
+      increment!(:deletions_count, partial_tweets.size - errors[:current].size)
+      increment!(:errors_count, errors[:current].size) if errors[:current].any?
 
       processed_count = deletions_count + errors_count
-      errors_any = current_errors.size.times.map { current_errors.pop }
 
       if processed_count % 1000 == 0 || tweets[-1] == partial_tweets[-1]
         puts progress(started_at, reservations_count, processed_count, deletions_count, errors_count)
       end
 
-      if errors_any.any? { |e| TwitterApiStatus.invalid_or_expired_token?(e) } ||
+      if errors[:current].any? { |e| TwitterApiStatus.invalid_or_expired_token?(e) } ||
           (errors_count > [processed_count, 1000].max / 10) ||
-          consecutive_errors.size > 100
+          errors[:consecutive].size > 100
         puts 'Stop processing'
         update(stopped_at: Time.zone.now)
       end
