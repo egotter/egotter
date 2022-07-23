9403c065a689ba82d9ac72e8d547cbe65cf675ef
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-24 06:03:35 +0900

    Update debug messages in TwitterUserAssociations

--- app/models/concerns/twitter_user_associations.rb
+++ app/models/concerns/twitter_user_associations.rb
@@ -52,7 +52,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::CloseFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       CloseFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -61,7 +61,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::FavoriteFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       FavoriteFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -70,7 +70,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::MutualFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       MutualFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -79,7 +79,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::OneSidedFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       OneSidedFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -88,7 +88,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::OneSidedFollowership.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       OneSidedFollowership.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -97,7 +97,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::InactiveFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       InactiveFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -106,7 +106,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::InactiveFollowership.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       InactiveFollowership.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -115,7 +115,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::InactiveMutualFriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       InactiveMutualFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -124,7 +124,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::MutualUnfriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       BlockFriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -133,7 +133,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::Unfriendship.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       Unfriendship.where(from_uid: uid).order(sequence: :asc)
     end
   end
@@ -142,7 +142,7 @@ module TwitterUserAssociations
     if (from_s3 = S3::Unfollowership.where(uid: uid))
       RelationshipProxy.new(from_s3)
     else
-      Airbag.info "Fetch records from outdated table method=#{__method__} twitter_user_id=#{id} uid=#{uid} created=#{created_at.to_s}", caller: caller
+      Airbag.info 'Fetch records from outdated table', method: __method__, twitter_user_id: id, uid: uid, created_at: created_at.to_s, caller: caller
       Unfollowership.where(from_uid: uid).order(sequence: :asc)
     end
   end
