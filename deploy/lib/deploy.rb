818e075d1fb0c76d56b6508728bc0d2a4bf61183
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-25 08:59:16 +0900

    Remove a lockfile only if the task is started and finished

--- deploy/lib/deploy.rb
+++ deploy/lib/deploy.rb
@@ -10,15 +10,18 @@ module Deploy
   end
 
   def with_lock(file, &block)
+    started = false
+
     if File.exist?(file)
       logger.info 'Another deployment is already running'
       return
     end
 
     File.write(file, Process.pid)
+    started = true
 
     yield
   ensure
-    File.delete(file) if File.exist?(file)
+    File.delete(file) if started && File.exist?(file)
   end
 end
