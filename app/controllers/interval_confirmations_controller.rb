e0aa842afa84b4d10d8c4e641051b04b6b729ca2
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-25 15:38:48 +0900

    Use uid instead of screen_name to fetch a record of TwitterDB::User

--- app/controllers/interval_confirmations_controller.rb
+++ app/controllers/interval_confirmations_controller.rb
@@ -1,8 +1,8 @@
 class IntervalConfirmationsController < ApplicationController
   def index
     if user_signed_in?
-      @user = TwitterDB::User.find_by(screen_name: current_user.screen_name)
-      @user = TwitterUser.latest_by(screen_name: current_user.screen_name) unless @user
+      @user = TwitterDB::User.find_by(uid: current_user.uid)
+      @user = TwitterUser.latest_by(uid: current_user.uid) unless @user
     end
   end
 end
