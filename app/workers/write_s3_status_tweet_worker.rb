986239c13731f3579886279f633404ef999c8695
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-27 15:12:35 +0900

    Implement WriteS3FavoriteTweetWorker, WriteS3MentionTweetWorker and WriteS3StatusTweetWorker

---
+++ app/workers/write_s3_status_tweet_worker.rb
@@ -0,0 +1,31 @@
+class WriteS3StatusTweetWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'import', retry: 0, backtrace: false
+
+  # options:
+  #   twitter_user_id
+  def perform(data, options = {})
+    hash = decompress(data)
+    S3::StatusTweet.import_from!(
+        hash['uid'],
+        hash['screen_name'],
+        hash['status_tweets']
+    )
+  rescue => e
+    Airbag.exception e, options: options
+  end
+
+  def decompress(data)
+    JSON.parse(Zlib::Inflate.inflate(Base64.decode64(data)))
+  end
+
+  class << self
+    def perform_async(hash, options = {})
+      super(compress(hash), options)
+    end
+
+    def compress(hash)
+      Base64.encode64(Zlib::Deflate.deflate(hash.to_json))
+    end
+  end
+end
