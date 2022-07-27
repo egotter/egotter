38dcb859a0485d054b547f7d5d237ee319971079
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-27 11:08:43 +0900

    Implement WriteS3FriendshipWorker, WriteS3FollowershipWorker and WriteS3ProfileWorker

---
+++ app/workers/write_s3_friendship_worker.rb
@@ -0,0 +1,34 @@
+class WriteS3FriendshipWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'import', retry: 0, backtrace: false
+
+  # options:
+  #   twitter_user_id
+  def perform(data, options = {})
+    hash = decompress(data)
+    S3::Friendship.import_from!(
+        hash['twitter_user_id'],
+        hash['uid'],
+        hash['screen_name'],
+        hash['friend_uids'],
+        async: false
+    )
+  rescue => e
+    self.class.perform_in(rand(10) + 10, data, options.merge(error_class: e.class))
+    Airbag.warn "Always retry #{e.inspect}", options: options
+  end
+
+  def decompress(data)
+    JSON.parse(Zlib::Inflate.inflate(Base64.decode64(data)))
+  end
+
+  class << self
+    def perform_async(hash, options = {})
+      super(compress(hash), options)
+    end
+
+    def compress(hash)
+      Base64.encode64(Zlib::Deflate.deflate(hash.to_json))
+    end
+  end
+end
