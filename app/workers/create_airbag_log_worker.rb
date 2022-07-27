2560680ab5ab7fb1e9972018ac4faa53754beb8c
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-27 13:36:43 +0900

    Do not print backtrace, cause_backtrace and caller in CreateAirbagLogWorker

--- app/workers/create_airbag_log_worker.rb
+++ app/workers/create_airbag_log_worker.rb
@@ -2,9 +2,13 @@ class CreateAirbagLogWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'logging', retry: 0, backtrace: false
 
-  def perform(severity, message, properties = nil, time = nil)
-    AirbagLog.create!(severity: severity, message: message, properties: properties.presence, time: time || Time.zone.now)
+  KEYS = %w(backtrace cause_backtrace caller)
+
+  def perform(severity, message, properties, time)
+    AirbagLog.create!(severity: severity, message: message, properties: properties, time: time)
+  rescue ActiveRecord::StatementInvalid => e
+    logger.warn "#{e.inspect.truncate(1000)} severity=#{severity} message=#{message.truncate(1000)} properties=#{properties.except(*KEYS)} time=#{time}"
   rescue => e
-    logger.error "#{e.inspect.truncate(1000)} severity=#{severity} message=#{message.truncate(1000)} properties=#{properties} time=#{time}"
+    logger.error "#{e.inspect.truncate(1000)} severity=#{severity} message=#{message.truncate(1000)} properties=#{properties.except(*KEYS)} time=#{time}"
   end
 end
