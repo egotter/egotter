aad295929eecf37b2a1723d47bc7e793c98663fc
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-28 10:24:45 +0900

    Update bin/send_metrics_to_cloudwatch.rb

--- bin/send_metrics_to_cloudwatch.rb
+++ bin/send_metrics_to_cloudwatch.rb
@@ -10,44 +10,46 @@ class Metrics
     @metrics = Hash.new { |hash, key| hash[key] = [] }
   end
 
-  def append(name, value, namespace:, dimensions: nil)
-    @metrics[namespace] << {
-        metric_name: name,
+  def namespace(value)
+    @namespace = value
+    self
+  end
+
+  def name(value)
+    @name = value
+    self
+  end
+
+  def append(value, dimensions)
+    @metrics[@namespace] << {
+        metric_name: @name,
         dimensions: dimensions,
         timestamp: Time.now,
         value: value,
         unit: 'Count'
     }
+    self
   end
 
   def upload
     client = Aws::CloudWatch::Client.new(region: 'ap-northeast-1')
 
     @metrics.each do |namespace, metric_data|
-      puts "send_metrics_to_cloudwatch.rb: Send #{metric_data.size} metrics to #{namespace}"
-
       metric_data.each_slice(20).each do |data|
-        params = {
-            namespace: namespace,
-            metric_data: data,
-        }
-        client.put_metric_data(params)
-      rescue => e
-        puts "send_metrics_to_cloudwatch.rb: #{e.inspect} data=#{data.inspect}"
+        client.put_metric_data(namespace: namespace, metric_data: data)
       end
     end
   end
 end
 
 def main
-  namespace = 'Google Analytics'
   ga = GoogleAnalyticsClient.new
 
-  metrics = Metrics.new
-  metrics.append('rt:activeUsers', ga.active_users, namespace: namespace, dimensions: [{name: 'rt:deviceCategory', value: 'TOTAL'}])
-  metrics.append('rt:activeUsers', ga.mobile_active_users, namespace: namespace, dimensions: [{name: 'rt:deviceCategory', value: 'MOBILE'}])
-  metrics.append('rt:activeUsers', ga.desktop_active_users, namespace: namespace, dimensions: [{name: 'rt:deviceCategory', value: 'DESKTOP'}])
-  metrics.upload
+  Metrics.new.namespace('Google Analytics').name('rt:activeUsers').
+      append(ga.active_users, [{name: 'rt:deviceCategory', value: 'TOTAL'}]).
+      append(ga.mobile_active_users, [{name: 'rt:deviceCategory', value: 'MOBILE'}]).
+      append(ga.desktop_active_users, [{name: 'rt:deviceCategory', value: 'DESKTOP'}]).
+      upload
 end
 
 if __FILE__ == $0
