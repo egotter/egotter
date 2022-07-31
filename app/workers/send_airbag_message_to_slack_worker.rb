011de48e69d83868676499972d7c9a12d158ffa5
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-31 17:42:34 +0900

    Implement JobConsumer and SendAirbagMessageToSlackWorker

---
+++ app/workers/send_airbag_message_to_slack_worker.rb
@@ -0,0 +1,14 @@
+class SendAirbagMessageToSlackWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'logging', retry: 0, backtrace: false
+
+  # options:
+  def perform(text, options = {})
+    channel = Rails.env.production? ? :airbag : :airbag_dev
+    SlackBotClient.channel(channel).post_message(text)
+  rescue Slack::Web::Api::Errors::TooManyRequestsError => e
+    logger.error "#{e.class} text=#{text.truncate(200)} options=#{options}"
+  rescue => e
+    logger.error "#{e.inspect.truncate(1000)} text=#{text.truncate(200)} options=#{options}"
+  end
+end
