4d5ba4500c08a0dfcb1de62cccb3b2d16b571760
ts-3156 <ts_3156@yahoo.co.jp>
2022-07-31 13:35:39 +0900

    Specify Safely.report_exception_method in initializers/ahoy.rb

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -15,3 +15,7 @@ Ahoy.track_bots = true
 Ahoy.visit_duration = 30.minutes
 
 Ahoy.geocode = false
+
+Safely.report_exception_method = Proc.new do |e, context|
+  Airbag.exception e, location: 'Safely.report_exception_method', context: context
+end
