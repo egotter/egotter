936a7e066b674535ec3a5efacb8a2f8b3b658fe7
ts-3156 <ts_3156@yahoo.co.jp>
2022-08-01 20:51:38 +0900

    Send upload-completed message to monit_delete_tweets_archive channel

--- app/controllers/api/v1/delete_tweets_notifications_controller.rb
+++ app/controllers/api/v1/delete_tweets_notifications_controller.rb
@@ -13,7 +13,7 @@ module Api
         else
           request = DeleteTweetsByArchiveRequest.create(user_id: current_user.id, archive_name: validated_archive, since_date: validated_since, until_date: validated_until)
           CreateDeleteTweetsUploadCompletedMessageWorker.perform_async(current_user.id, since: request.since_date, until: request.until_date)
-          SendMessageToSlackWorker.perform_async(:monit_delete_tweets, "`Upload completed` user_id=#{current_user.id} request_id=#{request.id}")
+          SendMessageToSlackWorker.perform_async(:monit_delete_tweets_archive, "Upload completed user_id=#{current_user.id} request_id=#{request.id}")
           render json: {status: 'ok'}
         end
       end
