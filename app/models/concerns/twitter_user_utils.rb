38cdb5f6624c461a612c377d07762b8aed418246
ts-3156 <ts_3156@yahoo.co.jp>
2022-08-05 08:13:04 +0900

    Update #where_mod

--- app/models/concerns/twitter_user_utils.rb
+++ app/models/concerns/twitter_user_utils.rb
@@ -50,7 +50,7 @@ module TwitterUserUtils
     end
 
     def where_mod(n1, n2)
-      num = 10 * n1 + n2
+      num = 100 * n1 + n2
       records = order(created_at: :desc).select(:id, :uid).where('created_at > ?', 1.day.ago)
       ids = records.uniq(&:uid).select { |r| r.uid % num == 0 }
       where(id: ids)
