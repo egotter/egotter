28204eb261f1e5ccbf0291f04ced246aac2c289f
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-06 11:27:01 +0900

    Send timeout messages to a new channel

--- app/workers/concerns/work_measurement.rb
+++ app/workers/concerns/work_measurement.rb
@@ -9,15 +9,17 @@ module WorkMeasurement
 
   def measure_time(*args)
     if respond_to?(:timeout_in)
+      log_props = {class: self.class, elapsed: sprintf("%.3f", elapsed_time), timeout: timeout_in}
+
       if timeout?(timeout_in)
         if respond_to?(:after_timeout)
           after_timeout(*args)
-        else
-          Airbag.warn 'Job timed out', class: self.class, timeout: timeout_in, elapsed: sprintf("%.3f", elapsed_time), args: args
         end
+        CreateSidekiqLogWorker.perform_async(nil, 'Job timed out', log_props.merge!(args: args), Time.zone.now)
+        SlackBotClient.channel(:job_timeout).post_message("Job timed out #{log_props}")
+      else
+        CreateSidekiqLogWorker.perform_async(nil, 'WorkMeasurement', log_props, Time.zone.now)
       end
-
-      CreateSidekiqLogWorker.perform_async(nil, 'WorkMeasurement', {elapsed: Time.zone.now - @start, class: self.class}, Time.zone.now)
     end
   end
 
--- spec/workers/concerns/work_measurement_spec.rb
+++ spec/workers/concerns/work_measurement_spec.rb
@@ -26,8 +26,7 @@ RSpec.describe WorkMeasurement do
     end
 
     it do
-      expect(Airbag).to receive(:warn).with(instance_of(String), anything)
-      expect(CreateSidekiqLogWorker).to receive(:perform_async).with(nil, 'WorkMeasurement', any_args)
+      expect(CreateSidekiqLogWorker).to receive(:perform_async).with(nil, 'Job timed out', any_args)
       subject
     end
 
