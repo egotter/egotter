4da5dd13684da75cfd85aaecb03ce46595c4c01a
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-08 02:16:46 +0900

    Fix "undefined method use"

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,15 +1,15 @@
-if Rails.env.production?
-  require 'datadog/statsd'
-  require 'ddtrace'
-
-  # Datadog APM
-  Datadog.configure do |c|
-    c.logger = ::Logger.new('log/datadog-tracer.log')
-    c.logger.level = ::Logger::INFO
-
-    c.use :rails, service_name: 'egotter'
-    c.use :sidekiq
-    c.analytics_enabled = true
-    c.runtime_metrics_enabled = true
-  end
-end
+# if Rails.env.production?
+#   require 'datadog/statsd'
+#   require 'ddtrace'
+#
+#   # Datadog APM
+#   Datadog.configure do |c|
+#     c.logger = ::Logger.new('log/datadog-tracer.log')
+#     c.logger.level = ::Logger::INFO
+#
+#     c.use :rails, service_name: 'egotter'
+#     c.use :sidekiq
+#     c.analytics_enabled = true
+#     c.runtime_metrics_enabled = true
+#   end
+# end
