013b399ecd71e8d8a8ff0211cf874cdc62491392
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-10 23:28:18 +0900

    Ignore ActionController::InvalidAuthenticityToken

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,13 +5,14 @@ class OmniauthLogger < ::Logger
   end
 
   def error(*args)
-    if session_expired?(args[0]) || invalid_credentials?(args[0])
+    if session_expired?(args[0]) || invalid_credentials?(args[0]) || invalid_token?(args[0])
       Airbag.info args[0]
       info(*args)
     else
       super
     end
   rescue => e
+    Airbag.exception e, message: args[0]
     super
   end
 
@@ -23,6 +24,10 @@ class OmniauthLogger < ::Logger
 
   def invalid_credentials?(str)
     str.include?('(twitter) Authentication failure! invalid_credentials: OAuth::Unauthorized')
+  end
+
+  def invalid_token?(str)
+    str.include?('(twitter) Authentication failure! ActionController::InvalidAuthenticityToken')
   end
 end
 
