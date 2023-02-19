ca9b1cf8a819c4a68a57c53ee79c3c95dbf9bf8c
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-19 19:56:48 +0900

    Send state_change_observer messages to :web_timeout

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,5 +1,6 @@
 Rack::Timeout.register_state_change_observer(:a_unique_name) do |env|
   if env['rack-timeout.info'].state == :timed_out
-    Airbag.warn 'Rack::Timeout.state_change_observer', state: :timed_out, REQUEST_URI: env&.fetch('REQUEST_URI', nil), HTTP_USER_AGENT: env&.fetch('HTTP_USER_AGENT', nil)
+    props = {state: :timed_out, REQUEST_URI: env&.fetch('REQUEST_URI', nil), HTTP_USER_AGENT: env&.fetch('HTTP_USER_AGENT', nil)}
+    SendMessageToSlackWorker.perform_async(:web_timeout, "Rack::Timeout.state_change_observer: #{props}") rescue nil
   end
 end
