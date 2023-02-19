00282497b23580a90eb7e76beb636f0fbfe64456
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-20 03:48:42 +0900

    Remove SkippedImportTwitterDBUserForRetryingDeadlockWorker

--- app/workers/import_twitter_db_user_for_retrying_deadlock_worker.rb
+++ app/workers/import_twitter_db_user_for_retrying_deadlock_worker.rb
@@ -1,8 +1,4 @@
 class ImportTwitterDBUserForRetryingDeadlockWorker < ImportTwitterDBUserWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'retry_low', retry: 0, backtrace: false
-
-  def after_skip(users, options = {})
-    SkippedImportTwitterDBUserForRetryingDeadlockWorker.perform_async(users, options)
-  end
 end
