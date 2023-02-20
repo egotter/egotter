2819b498a3ddc378f088443b315daf934f782655
ts-3156 <ts_3156@yahoo.co.jp>
2023-02-21 03:41:26 +0900

    Refactor SendReceivedMediaToSlackWorker

--- app/workers/send_received_media_to_slack_worker.rb
+++ app/workers/send_received_media_to_slack_worker.rb
@@ -5,19 +5,18 @@ class SendReceivedMediaToSlackWorker
   # options:
   def perform(json, options = {})
     dm = DirectMessageWrapper.from_json(json)
+    return unless dm.media_url
 
-    if dm.media_url
-      client = User.egotter.api_client.twitter
-      media = dm.retrieve_media(client)
-      user = TwitterDB::User.find_by(uid: dm.sender_id)
-      text = "uid=#{dm.sender_id} screen_name=#{user&.screen_name} text=#{dm.text}"
+    client = User.egotter.api_client.twitter
+    media = dm.retrieve_media(client)
+    user = TwitterDB::User.find_by(uid: dm.sender_id)
+    text = "uid=#{dm.sender_id} screen_name=#{user&.screen_name} text=#{dm.text}"
 
-      if media.present?
-        SlackBotClient.channel('general').upload_media(media, initial_comment: text)
-      else
-        text += " media=error"
-        SlackBotClient.channel('general').post_message(text)
-      end
+    if media.present?
+      SlackBotClient.channel('general').upload_media(media, initial_comment: text)
+    else
+      text += " media=error"
+      SlackBotClient.channel('general').post_message(text)
     end
   rescue => e
     Airbag.exception e, json: json
