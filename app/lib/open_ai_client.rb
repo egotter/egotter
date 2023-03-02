5a67fb035f72e603af6e34249d42917751dd1a49
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-02 23:29:46 +0900

    Exclude spam words from a response of chat messages

--- app/lib/open_ai_client.rb
+++ app/lib/open_ai_client.rb
@@ -5,7 +5,13 @@ class OpenAiClient
         {role: 'user', content: text},
     ]
     res = OpenAI::Client.new.chat(parameters: {model: 'gpt-3.5-turbo', messages: messages})
-    res['choices'][0]['message']['content']
+    message = res['choices'][0]['message']['content']
+
+    if SpamMessageResponder::Processor.new(nil, message).received?
+      I18n.t('ai.default_message')
+    else
+      message
+    end
   rescue => e
     Airbag.exception e, text: text
     TEXT.sample + Kaomoji::KAWAII.sample
--- app/lib/spam_message_responder.rb
+++ app/lib/spam_message_responder.rb
@@ -14,17 +14,24 @@ class SpamMessageResponder < AbstractMessageResponder
     def received?
       return false if @text.length > message_length
 
-      if @text.match?(received_regexp1) || @text.match?(received_regexp2)
+      if spam_received?(@text)
         @spam = true
       end
     end
 
+    REGEXP1 = /死ね|殺す|黙れ|馬鹿|役立たず|無能|やくたたず|きいてんのか|いいかげんに|うるせーよ|うるせ[えぇ]|うっせ[えぇ]|気持ち悪い|キモい|きもい|きしょい|うるせえよ|うるさい/
+    REGEXP2 = /^(しね|くず|うざ|くそ|ころす|きえろ|ばか|だまれ|かえれ|こら|きも|ごみ|ゴミ|バカ|ちんぽ|ちんこ|まんこ|うんち|うんこ)$/
+
+    def spam_received?(text)
+      text.match?(REGEXP1) || text.match?(REGEXP2)
+    end
+
     def received_regexp1
-      /死ね|殺す|黙れ|馬鹿|役立たず|無能|やくたたず|きいてんのか|いいかげんに|うるせーよ|うるせ[えぇ]|うっせ[えぇ]|気持ち悪い|キモい|きもい|きしょい|うるせえよ|うるさい/
+      REGEXP1
     end
 
     def received_regexp2
-      /^(しね|くず|うざ|くそ|ころす|きえろ|ばか|だまれ|かえれ|こら|きも|ごみ|ゴミ|バカ|ちんぽ|ちんこ|まんこ|うんち|うんこ)$/
+      REGEXP2
     end
 
     def send_message
