2ea740597bc0492ff8db86a06c67eab821ff03ce
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-11 05:40:03 +0900

    Fix "undefined method []"

--- app/lib/open_ai_client.rb
+++ app/lib/open_ai_client.rb
@@ -10,8 +10,8 @@ class OpenAiClient
         {role: 'system', content: I18n.t('ai.system_content')},
         {role: 'user', content: text},
     ]
-    res = OpenAI::Client.new.chat(parameters: {model: 'gpt-3.5-turbo', messages: messages})
-    message = res['choices'][0]['message']['content']
+    @res = OpenAI::Client.new.chat(parameters: {model: 'gpt-3.5-turbo', messages: messages})
+    message = @res['choices'][0]['message']['content']
 
     if SpamMessageResponder::Processor.new(nil, nil).spam_received?(text) ||
         I18n.t('ai.system_content').split(/[、。]/).any? { |content| message.include?(content) }
@@ -20,7 +20,7 @@ class OpenAiClient
 
     message
   rescue => e
-    Airbag.exception e, text: text
-    TEXT.sample + Kaomoji::KAWAII.sample
+    Airbag.exception e, text: text, res: @res
+    Kaomoji::KAWAII.sample
   end
 end
