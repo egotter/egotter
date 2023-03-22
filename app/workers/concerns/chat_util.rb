cef970671f4a3a321a0edb17caa45f121c7e8226
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-22 19:43:55 +0900

    Rename: chat -> chat_error

--- app/workers/concerns/chat_util.rb
+++ app/workers/concerns/chat_util.rb
@@ -2,7 +2,7 @@ module ChatUtil
   def generate_chat(text, options = {})
     res = OpenAiClient.new.chat!(text)
   rescue => e
-    SendMessageToSlackWorker.perform_async(:chat, {worker: self.class, exception: e, text: text, options: options}.inspect)
+    SendMessageToSlackWorker.perform_async(:chat_error, {worker: self.class, exception: e, text: text, options: options}.inspect)
     res = Kaomoji::KAWAII.sample
   ensure
     Airbag.info 'OpenAI response', input: text, output: res
