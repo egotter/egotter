c0650d73c951df24ddcf7266b7b07d5f9d0ecadb
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-24 00:04:25 +0900

    Add messages of CreateGreetingSorryMessageWorker

--- app/workers/create_greeting_sorry_message_worker.rb
+++ app/workers/create_greeting_sorry_message_worker.rb
@@ -4,10 +4,6 @@ class CreateGreetingSorryMessageWorker
   include ReportErrorHandler
   sidekiq_options queue: 'messaging', retry: 0, backtrace: false
 
-  MESSAGE = <<~TEXT
-    どうしたんですか？
-  TEXT
-
   def unique_key(uid, options = {})
     uid
   end
@@ -19,8 +15,7 @@ class CreateGreetingSorryMessageWorker
   # options:
   #   text
   def perform(uid, options = {})
-    message = generate_chat(options['text'], uid: uid)
-    User.egotter.api_client.create_direct_message(uid, message)
+    User.egotter.api_client.create_direct_message(uid, I18n.t('workers.chat_messages.sorry').sample)
   rescue => e
     unless ignorable_report_error?(e)
       Airbag.exception e, uid: uid, options: options
