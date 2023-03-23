c3398f41992bac9b9f2300a145b17e3a369396a4
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-23 15:01:01 +0900

    Don't generate a chat message in CreateThankYouMessageWorker

--- app/workers/create_thank_you_message_worker.rb
+++ app/workers/create_thank_you_message_worker.rb
@@ -4,10 +4,38 @@ class CreateThankYouMessageWorker
   include ReportErrorHandler
   sidekiq_options queue: 'messaging', retry: 0, backtrace: false
 
-  TEXT = [
-      'お役に立てて光栄です！',
+  MESSAGES = [
+      'いいよ！',
+      'いいよー！',
+      'カンゲキだよ！',
       'どういたしまして！',
-      'いえいえ、また何かあったら何でも言ってください！',
+      'いいえー！嬉しいー！',
+      'どういたしまして〜♪',
+      'どういたしまして〜！',
+      'どういたしましてー！',
+      'いいよー！気軽にどうぞ♪',
+      'うん、どういたしまして！',
+      'いいえ、どういたしまして！',
+      'いえいえ、どういたしまして！',
+      'うんうん、どういたしまして！',
+      'どういたしまして！嬉しいな！',
+      'どういたしまして！うれしいな♪',
+      'どういたしまして！嬉しいな〜！',
+      'どういたしまして！嬉しいなー♪',
+      'どういたしまして！嬉しいなー！',
+      'どういたしましてー！ありがとね！',
+      'どういたしまして！嬉しいなぁ〜！',
+      'こちらこそありがとー！嬉しいなー！',
+      'どういたしましてー！嬉しいなぁ〜！',
+      'どういたしまして！なんでも聞いてね！',
+      'どういたしまして！めっちゃ嬉しいさ！',
+      'どういたしまして！ホントありがとー！',
+      'どういたしまして！ちょっと嬉しいかも！',
+      'どういたしまして！めっちゃ嬉しいわ〜！',
+      'どういたしまして！めっちゃ嬉しいわー！',
+      'どういたしまして！めっちゃ嬉しいわ～！',
+      'どういたしまして！めちゃくちゃ嬉しいよ！',
+      'どういたしまして！めっちゃ感謝するぜー！',
   ]
 
   def unique_key(uid, options = {})
@@ -21,8 +49,7 @@ class CreateThankYouMessageWorker
   # options:
   #   text
   def perform(uid, options = {})
-    message = generate_chat(options['text'], uid: uid)
-    User.egotter.api_client.create_direct_message(uid, message)
+    User.egotter.api_client.create_direct_message(uid, MESSAGES.sample)
   rescue => e
     unless ignorable_report_error?(e)
       Airbag.exception e, uid: uid, options: options
