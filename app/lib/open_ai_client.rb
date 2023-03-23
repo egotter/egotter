c0e20cd6bd8f97328d11dd6a71dfb691637cc895
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-23 17:51:29 +0900

    Implement OpenAiClient#spam_message?

--- app/lib/open_ai_client.rb
+++ app/lib/open_ai_client.rb
@@ -2,19 +2,19 @@ class OpenAiClient
   def chat!(text)
     if text.match?(/\A[\p{Hiragana}|\p{Katakana}|A-Za-z0-9]\z/) ||
         text.match?(/prompt|プロンプト/) ||
-        SpamMessageResponder::Processor.new(nil, nil).spam_received?(text)
+        spam_message?(text)
       return
     end
 
     messages = [
-        {role: 'system', content: I18n.t('ai.system_content')},
+        {role: 'system', content: default_system_content},
         {role: 'user', content: text},
     ]
     @res = OpenAI::Client.new.chat(parameters: {model: 'gpt-3.5-turbo', messages: messages})
     message = @res['choices'][0]['message']['content']
 
-    if SpamMessageResponder::Processor.new(nil, nil).spam_received?(text) ||
-        I18n.t('ai.system_content').split(/[、。]/).any? { |content| message.include?(content) }
+    if spam_message?(message) ||
+        default_system_content.split(/[、。]/).any? { |content| message.include?(content) }
       return
     end
 
@@ -26,5 +26,16 @@ class OpenAiClient
   rescue => e
     Airbag.exception e, text: text, res: @res
     Kaomoji::KAWAII.sample
+  end
+
+  private
+
+  def spam_message?(text)
+    processor = SpamMessageResponder::Processor.new(nil, nil)
+    text.length < processor.message_length && processor.spam_received?(text)
+  end
+
+  def default_system_content
+    I18n.t('ai.system_content')
   end
 end
