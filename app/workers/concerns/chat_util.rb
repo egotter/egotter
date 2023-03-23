d3ca838b38dc307494f52860dc47521a61ea4c0d
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-23 15:01:28 +0900

    Log the worker class in ChatUtil

--- app/workers/concerns/chat_util.rb
+++ app/workers/concerns/chat_util.rb
@@ -5,6 +5,6 @@ module ChatUtil
     SendMessageToSlackWorker.perform_async(:chat_error, {worker: self.class, exception: e, text: text, options: options}.inspect)
     res = Kaomoji::KAWAII.sample
   ensure
-    Airbag.info 'OpenAI response', input: text, output: res
+    Airbag.info 'OpenAI response', input: text, output: res, worker: self.class
   end
 end
