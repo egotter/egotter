046d91dc92e17ada4c4ae91a3a15e6347e418b80
ts-3156 <ts_3156@yahoo.co.jp>
2023-03-27 11:17:43 +0900

    Check #stop_all_regexp before #stop_regexp

--- app/lib/stop_message_responder.rb
+++ app/lib/stop_message_responder.rb
@@ -14,30 +14,30 @@ class StopMessageResponder < AbstractMessageResponder
     def received?
       return false if @text.length > message_length
 
-      if @text.match?(stop_regexp)
-        @stop = true
-      elsif @text.match?(stop_all_regexp)
+      if @text.match?(stop_all_regexp)
         @stop_all = true
+      elsif @text.match?(stop_regexp)
+        @stop = true
       end
-    end
-
-    def stop_regexp
-      /停止|ストップ|オフ|勝手に|使わない|止めて|止まれ|止めろ|止めたい|送らないで|おくらないで|やめて|いらない|辞める|辞めたい|退会|解除|無効|終了/
     end
 
     def stop_all_regexp
       /(全て|すべて|全部|ぜんぶ)の?通知(\s|　)*停止/
     end
 
+    def stop_regexp
+      /停止|ストップ|オフ|勝手に|使わない|止めて|止まれ|止めろ|止めたい|送らないで|おくらないで|やめて|いらない|辞める|辞めたい|退会|解除|無効|終了/
+    end
+
     def send_message
       user = validate_report_status(@uid)
       return unless user
 
-      if @stop
-        CreatePeriodicReportHelpMessageWorker.perform_async(user.id)
-      elsif @stop_all
+      if @stop_all
         StopAllReportsWorker.perform_async(user.id)
         CreateAllReportsStoppedMessageWorker.perform_async(user.id)
+      elsif @stop
+        CreatePeriodicReportHelpMessageWorker.perform_async(user.id)
       end
     end
   end
