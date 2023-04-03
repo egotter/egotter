e5d972a539c434715316263dd5e492d6289c86de
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-04 01:13:18 +0900

    Check StopServiceFlag in CreateTweetsWorker and bots:invalidate_credentials task

--- app/workers/create_tweets_worker.rb
+++ app/workers/create_tweets_worker.rb
@@ -3,6 +3,11 @@ class CreateTweetsWorker
   sidekiq_options queue: 'misc', retry: 0, backtrace: false
 
   def perform(keyword)
+    if StopServiceFlag.on?
+      Airbag.info 'StopServiceFlag: CreateTweetsWorker is stopped'
+      return
+    end
+
     tweets = User.admin.api_client.search(keyword, count: 10).take(10)
     SearchedTweets.set(keyword, tweets.to_json)
   rescue => e
--- lib/tasks/bots.rake
+++ lib/tasks/bots.rake
@@ -2,6 +2,11 @@ namespace :bots do
   task invalidate_credentials: :environment do
     verbose = ENV['VERBOSE']
 
+    if StopServiceFlag.on?
+      Airbag.info 'StopServiceFlag: bots:invalidate_credentials is stopped'
+      break
+    end
+
     Bot.where(enabled: true).find_each do |bot|
       bot.invalidate_credentials
       print '.' if verbose
