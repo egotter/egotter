4da0de047481ac18bf998a7492defca290608045
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-10 17:59:37 +0900

    Check StopServiceFlag in SendCreateTweetStartedWorker and SendCreateTweetDeletedWorker

--- app/workers/send_create_tweet_deleted_worker.rb
+++ app/workers/send_create_tweet_deleted_worker.rb
@@ -4,6 +4,11 @@ class SendCreateTweetDeletedWorker
 
   # options:
   def perform(request_id, options = {})
+    if StopServiceFlag.on?
+      Airbag.info 'StopServiceFlag: SendCreateTweetDeletedWorker is stopped', request_id: request_id
+      return
+    end
+
     request = TweetRequest.find(request_id)
     SendMessageToSlackWorker.perform_async(:monit_tweet, "`Deleted` #{request.to_message}")
   rescue => e
--- app/workers/send_create_tweet_started_worker.rb
+++ app/workers/send_create_tweet_started_worker.rb
@@ -5,6 +5,11 @@ class SendCreateTweetStartedWorker
   # options:
   #   via
   def perform(request_id, options = {})
+    if StopServiceFlag.on?
+      Airbag.info 'StopServiceFlag: SendCreateTweetStartedWorker is stopped', request_id: request_id
+      return
+    end
+
     request = TweetRequest.find(request_id)
     SendMessageToSlackWorker.perform_async(:monit_tweet, "`Started` #{request.to_message(via: options['via'])}")
   rescue => e
