bc32b4a58ceafe9c2b6b4a8fa23aeb33464bf7f3
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-12 00:53:45 +0900

    Check StopServiceFlag in #show_dm_confirmation_announcement?

--- app/helpers/welcome_reports_helper.rb
+++ app/helpers/welcome_reports_helper.rb
@@ -1,5 +1,10 @@
 module WelcomeReportsHelper
   def show_dm_confirmation_announcement?
+    if StopServiceFlag.on?
+      Airbag.info 'StopServiceFlag: #show_dm_confirmation_announcement? is stopped'
+      return false
+    end
+
     timeline_page? &&
         (!user_signed_in? || (current_user.created_at >= 1.day.ago && !PeriodicReport.messages_allotted?(current_user)))
   end
